5f5c6606e0c2fa66f4e4bbc86ea66e23e7f1809c
Bjoern Buerger <b.buerger@pengutronix.de>
2004-03-15 09:48:36 +0000

    bbu: fix permissions script for /etc et all.
    git-svn-id: https://svn.pengutronix.de/svn/ptxdist/trunk@1207 33e552b5-05e3-0310-8538-816dae2090ed

--- misc/ptx-init-permissions.sh
+++ misc/ptx-init-permissions.sh
@@ -1,4 +1,31 @@
 #!/bin/sh
 # ptx initialize permissions
-echo "SET PERMISSIONS HERE ... [FIXME]"
 
+PERM_UNCONFIGURED="/etc/ptx_permissions.unconfigured"
+PERM_CONFIGURED="/etc/ptx_permissions"
+
+die(){
+echo "nothing to do"
+exit
+}
+
+fixperms(){
+ mount -o remount,rw /
+
+ while read file owner perms; do {
+ chown $owner $file
+ chmod $perms $file
+ } 
+ done < $PERM_UNCONFIGURED
+ 
+ mv $PERM_UNCONFIGURED $PERM_CONFIGURED;
+ mount -o remount,ro /
+ echo "done"
+}
+
+echo -n "checking owner / permissions ... "
+if [ -f $PERM_UNCONFIGURED ]; then 
+    fixperms
+else
+    die
+fi
