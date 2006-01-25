d1bb4b02db15abb3bba8812f498583d80348fe68
Sascha Hauer <s.hauer@pengutronix.de>
2006-01-25 16:57:53 +0000

    add script to apply a patchstack
    git-svn-id: https://svn.pengutronix.de/svn/ptxdist/trunks/ptxdist-0.9-trunk@4416 33e552b5-05e3-0310-8538-816dae2090ed

---
+++ scripts/apply_patch_series.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+#
+# apply_patch_series: apply a patch series to a directory
+#
+
+. `dirname $0`/libptxdist.sh
+
+usage() {
+	echo
+	[ -n "$1" ] && echo -e "${PREFIX} error: $1\n"
+	echo "usage: $0 <args>"
+	echo
+	echo " Arguments:"
+	echo
+	echo "  -s <file>      series file to use"
+	echo "  -d <directory> target directory"
+	exit 1
+}
+
+#
+# Option parser
+#
+while [ $# -gt 0 ]; do
+	case "$1" in
+		-s) SERIES=`ptxd_abspath $2`;	shift 2 ;;
+		-d) TARGET=`ptxd_abspath $2`;	shift 2 ;;
+		*) usage "unknown option $1" ;;
+	esac
+done
+
+#
+# Sanity checks
+#
+[ -z "$SERIES" ]       && usage "${PREFIX} error: specify a series file with -s"
+[ -z "$TARGET" ]       && usage "${PREFIX} error: specify a target directory with -d"
+
+pushd "$TARGET" || exit 1
+
+cat "$SERIES" | egrep -v "^[[:space:]]*#" | egrep -v "^[[:space:]]*$" | while read patchfile unused; do
+	abspatch=`dirname $SERIES`/"$patchfile"
+	if [ ! -e "$abspatch" ]; then
+		echo "patch $abspatch does not exist. aborting"
+		exit 1
+	fi
+	echo "applying $abspatch"
+	cat "$abspatch" | patch -p1 || exit 1
+done
+
+if [ "$?" != 0 ]; then
+	exit 1;
+fi
+
+popd
+
