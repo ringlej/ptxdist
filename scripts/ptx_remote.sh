a7b1f25f0bafb2319b8baa89ebdfc5865607ff6c
Marc Kleine-Budde <mkl@pengutronix.de>
2006-03-24 08:38:15 +0000

    git-svn-id: https://svn.pengutronix.de/svn/ptxdist/trunks/ptxdist-0.10-trunk@5253 33e552b5-05e3-0310-8538-816dae2090ed

---
+++ scripts/ptx_remote.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+host_ip=${1}
+target_ip=${2}
+target_port=${3}
+prog=${4}
+shift
+shift
+shift
+shift
+args=${*}
+
+rsh -l service ${target_ip} gdbserver ${host_ip}:${target_port} ${prog} ${args}
