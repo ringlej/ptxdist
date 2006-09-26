e9e9cd604aa81c20ea418736e0321d951daeb5d9
Juergen Beisert <j.beisert@pengutronix.de>
2006-09-26 09:43:45 +0000

    templates added for easier extension
    git-svn-id: https://svn.pengutronix.de/svn/ptxdist/trunks/ptxdist-trunk@6060 33e552b5-05e3-0310-8538-816dae2090ed

---
+++ rules/templates/kernel/module-template.c
@@ -0,0 +1,44 @@
+/*
+ * File: module-template.c
+ * Description: Small demo, how to write a Linux kernel module
+ */
+
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/init.h>
+
+/*
+ * template_init - function to insert this module into kernel space
+ *
+ * This is the first of two exported functions to handle inserting this
+ * code into a running kernel
+ *
+ * Returns 0 if successfull, otherwise -1
+ */
+
+static int __init template_init(void)
+{
+	printk("Hello World, here is your module speaking\n");
+	return(0);
+}
+
+/*
+ * template_cleanup - function to cleanup this module from kernel space
+ *
+ * This is the second of two exported functions to handle cleanup this
+ * code from a running kernel
+ */
+
+static void __exit template_cleanup(void)
+{
+	 printk("Short life for a small module...\n");
+}
+
+module_init(template_init);
+module_exit(template_cleanup);
+
+MODULE_DESCRIPTION( "Small module, demo only, not very usefull." );
+MODULE_LICENSE( "GPL" );
+
+/* end of file module-template.c */
+
