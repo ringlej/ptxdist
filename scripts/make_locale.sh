92c7c4215a7c0ace93648c4a25f9fb93db1adf3c
Luotao Fu <l.fu@pengutronix.de>
2007-01-10 09:31:16 +0000

    cleaned debugging and obsolete stuffs
    git-svn-id: https://svn.pengutronix.de/svn/ptxdist/trunks/ptxdist-trunk@6809 33e552b5-05e3-0310-8538-816dae2090ed

--- scripts/make_locale.sh
+++ scripts/make_locale.sh
@@ -22,20 +22,9 @@ usage() {
 	exit 1
 }
 
-_ptxd_get_sysroot_usr_by_sysroot() {
-    local sysroot
-
-    sysroot="`echo 'int main(void){return 0;}' | \
-	${CC} -x c -o /dev/null -v - 2>&1 | \
-	sed -ne \"/.*collect2.*/s,.*--sysroot=\([^[:space:]]*\).*,\1,p\"`"
-
-    test -n "${sysroot}" || return 1
-
-    echo "`ptxd_abspath ${sysroot}/usr`"
-}
-
 add_locale() {
 	local CHARMAP LOCALE_DEF PREF SYSROOT_USR LOCALE_NAME
+
 	while getopts "f:i:p:n::" opt; do
 		case "${opt}" in
 		    f)
@@ -55,16 +44,15 @@ add_locale() {
 			;;
 		esac
 	done
+
 	SYSROOT_USR=`ptxd_get_sysroot_usr`
 	[ ! -d ${SYSROOT_USR} ] && { echo "Toolchain sysroot dir not found"; exit 1; }
-	
 
 	if [ ! -d ${PREF}/usr/lib/locale ]; then
 		mkdir -p ${PREF}/usr/lib/locale 
 		[ $? -ne 0 ] && { echo "Could not create temporary locales directory ${PREF}/usr/lib/locale"; exit 1; }
 	fi
 
-	echo "${SYSROOT_USR} localedef -i $LOCALE_DEF -f $CHARMAP $LOCALE_NAME --prefix=${PREF}"
 	I18NPATH=${SYSROOT_USR}/share/i18n \
 	localedef -i $LOCALE_DEF -f $CHARMAP $LOCALE_NAME --prefix=${PREF} 
 
