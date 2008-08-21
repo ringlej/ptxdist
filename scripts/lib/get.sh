c44bef1fd574bb9475dd55c77d9f572cf3776362
Marc Kleine-Budde <mkl@pengutronix.de>
2008-08-21 20:48:55 +0000

    * lib, lib/get.sh: 	new
    git-svn-id: https://svn.pengutronix.de/svn/ptxdist/trunks/ptxdist-trunk@8752 33e552b5-05e3-0310-8538-816dae2090ed

---
+++ scripts/lib/get.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+ptxd_make_get() {
+	local orig_argv=( "${@}" )
+
+	if [ -z "${1}" ]; then
+		echo
+		echo "${PROMPT}error: empty parameter to '${FUNCNAME}'"
+		echo
+		exit 1
+	fi
+
+	# split URLs by spaces
+	set -- ${*}
+
+	while [ ${#} -ne 0 ]; do
+		local url="${1}"
+		shift
+
+		case "${url}" in
+		http://*|ftp://*)
+			wget --passive-ftp ${PTXDIST_QUIET:+--quiet} -P "${PTXDIST_SRCDIR}" "${url}" && return
+			;;
+		file*)
+			local thing="${url/file:\/\///}"
+			
+			if [ -f "$thing" ]; then
+				echo "local archive, copying"
+				cp -av "${thing}" "${PTXDIST_SRCDIR}" && return
+			elif [ -d "${thing}" ]; then
+				echo "local directory instead of tar file, skipping get"
+				return
+			else
+				thing="${url/file:\/\//./}"
+				if [ -d "${thing}" ]; then
+					echo "local project directory instead of tar file, skipping get"
+					return
+				fi
+			fi
+			;;
+		*)
+			echo
+			echo "Unknown URL Type!"
+			echo "URL: $url"
+			echo
+			exit 1
+			;;
+		esac
+	done
+
+	echo
+	echo "Could not download packet"
+	echo "URL: ${orig_argv[@]}"
+	echo
+	exit 1
+}
+
+export -f ptxd_make_get
