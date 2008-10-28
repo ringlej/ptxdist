17394a8a2b9a5211599f470907535085d77f4f1e
Marc Kleine-Budde <mkl@pengutronix.de>
2008-10-28 15:43:31 +0000

    * lib/ptxd_make_extract.sh, lib/ptxd_lib_kgen.sh 	new
    git-svn-id: https://svn.pengutronix.de/svn/ptxdist/trunks/ptxdist-trunk@8960 33e552b5-05e3-0310-8538-816dae2090ed

---
+++ scripts/lib/ptxd_lib_kgen.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+#
+# local defined vars
+#
+PTXDIST_OVERWRITE_DIRS_MSB=( "${PTXDIST_PLATFORMCONFIGDIR}" "${PTXDIST_WORKSPACE}" "${PTXDIST_TOPDIR}" )
+PTXDIST_OVERWRITE_DIRS_LSB=( "${PTXDIST_TOPDIR}" "${PTXDIST_WORKSPACE}" "${PTXDIST_PLATFORMCONFIGDIR}" )
+
+ptxd_kgen_awk()
+{
+    gawk '
+	BEGIN {
+	    FS = ":##[[:space:]]*SECTION=|[[:space:]]*$";
+	}
+
+	{
+	    file = $1;
+	    section = $2;
+	    pkg = gensub(/.*\//, "", "g", file);
+
+	    pkgs[section, pkg] = file;
+	}
+
+	END {
+	    n = asorti(pkgs, sorted);
+
+	    for (i = 1; i <= n; i++) {
+		split(sorted[i], sep, SUBSEP)
+
+		file = pkgs[sorted[i]];
+		section = sep[1];
+		pkg = sep[2];
+
+		output = "'"${PTXDIST_KGEN_DIR}"'" "/" section ".in";
+
+		print "source \"" file "\"" > output
+	    }
+	}
+	'
+}
+
+ptxd_kgen_generate_sections()
+{
+    local dir
+
+    (
+	for dir in "${PTXDIST_OVERWRITE_DIRS_LSB[@]}"; do
+	    if [ \! -d "${dir}/rules" ]; then
+		continue
+	    fi
+
+	    grep -R -e "^##[[:space:]]*SECTION=" "${dir}/rules/"*.in
+	done
+    ) | ptxd_kgen_awk
+}
+
+ptxd_kgen()
+{
+    if [ -e "${PTXDIST_KGEN_DIR}" ]; then
+	rm -rf "${PTXDIST_KGEN_DIR}"
+    fi
+
+    mkdir -p "${PTXDIST_KGEN_DIR}"
+
+    ptxd_kgen_generate_sections
+}
---
+++ scripts/lib/ptxd_make_extract.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+#
+# -d DEST
+# -s PACKET_SOURCE
+# -p PACKET_DIR
+# -u PACKET_URL
+#
+ptxd_make_extract() {
+    . ${PTXDIST_TOPDIR}/scripts/ptxdist_vars.sh
+
+    while getopts "s:p:u:d:" opt; do
+	case "${opt}" in
+	    s)
+		local packet_source="${OPTARG}"
+		;;
+ 	    p)
+		local packet_dir="${OPTARG}"
+		;;
+	    u)
+		local packet_url="${OPTARG}"
+		;;
+	    d)
+		local dest="${OPTARG}"
+		;;
+	    *)
+		exit 1
+		;;
+	esac
+    done
+
+    dest="${dest:-${BUILDDIR}}"
+
+    case "$packet_url" in
+	file://*)
+	    local thing="${packet_url/file:\/\///}"
+	    if [ -d "${thing}" ]; then
+		echo "local directory instead of tar file, linking build dir"
+		ln -sf "$(ptxd_abspath "${thing}")" "${packet_dir}"
+		exit $?
+	    fi
+	    ;;
+    esac
+
+    if [ -z "${packet_source}" ]; then
+	echo
+	echo "Error: empty parameter to 'extract()'"
+	echo
+	exit 1
+    fi
+
+    if [ \! -d "${dest}" ]; then
+	mkdir -p "${dest}"
+    fi
+
+    echo "extract: archive=${packet_source}"
+    echo "extract: dest=${dest}"
+
+    case "${packet_source}" in
+	*gz)
+	    local extract=gzip
+	    ;;
+	*bz2)
+	    local extract=bzip2
+	    ;;
+	*zip)
+	    echo "$(basename "${packet_source}")" >> "${STATEDIR}/packetlist"
+	    unzip -q "${packet_source}" -d "${dest}"
+	    exit $?
+	    ;;
+	*)
+	    echo
+	    echo "Unknown format, cannot extract!"
+	    echo
+	    exit 1
+	    ;;
+    esac
+
+    echo "$(basename "${packet_source}")" >> "${STATEDIR}/packetlist"
+    ${extract} -dc "${packet_source}" | tar -C "${dest}" -xf -
+
+    check_pipe_status
+}
+
+export -f ptxd_make_extract
