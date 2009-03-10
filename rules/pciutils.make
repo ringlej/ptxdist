ringlej/ptxdist

commit 5d038aa76f0d32904bee88da2bc4e65e2e8d4fd0

[pciutils] cleanups
Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>

git-svn-id: https://svn.pengutronix.de/svn/ptxdist/trunks/ptxdist-trunk@10038 33e552b5-05e3-0310-8538-816dae2090ed

rules/pciutils.make
@@ -25,103 +25,55 @@ PCIUTILS_URL		:= http://ftp.kernel.org/pub/software/utils/pciutils/$(PCIUTILS).$
 PCIUTILS_SOURCE		:= $(SRCDIR)/$(PCIUTILS).$(PCIUTILS_SUFFIX)
 PCIUTILS_DIR		:= $(BUILDDIR)/$(PCIUTILS)
 
-
 # ----------------------------------------------------------------------------
 # Get
 # ----------------------------------------------------------------------------
 
-pciutils_get: $(STATEDIR)/pciutils.get
-
-$(STATEDIR)/pciutils.get: $(pciutils_get_deps_default)
-	@$(call targetinfo, $@)
-	@$(call touch, $@)
-
 $(PCIUTILS_SOURCE):
-	@$(call targetinfo, $@)
+	@$(call targetinfo)
 	@$(call get, PCIUTILS)
-
-# ----------------------------------------------------------------------------
-# Extract
-# ----------------------------------------------------------------------------
-
-pciutils_extract: $(STATEDIR)/pciutils.extract
-
-$(STATEDIR)/pciutils.extract: $(pciutils_extract_deps_default)
-	@$(call targetinfo, $@)
-	@$(call clean, $(PCIUTILS_DIR))
-	@$(call extract, PCIUTILS)
-	@$(call patchin, PCIUTILS)
-	@$(call touch, $@)
 
 # ----------------------------------------------------------------------------
 # Prepare
 # ----------------------------------------------------------------------------
 
-pciutils_prepare: $(STATEDIR)/pciutils.prepare
+PCIUTILS_PATH	:= PATH=$(CROSS_PATH)
+PCIUTILS_MAKEVARS := \
+	CROSS_COMPILE=$(COMPILER_PREFIX) \
+	PREFIX=/usr \
+	SBINDIR='\$$(PREFIX)/bin' \
+	HOST=$(PTXCONF_ARCH_STRING)--linux \
+	RELEASE=$(PTXCONF_KERNEL_VERSION) \
+	DNS=no
 
-PCIUTILS_PATH	:=  PATH=$(CROSS_PATH)
-PCIUTILS_ENV 	:=  $(CROSS_ENV)
-
-PCIUTILS_ENV += PREFIX=/usr
-PCIUTILS_ENV += HOST=$(PTXCONF_ARCH_STRING)--linux
-PCIUTILS_ENV += RELEASE=$(PTXCONF_KERNEL_VERSION)
-# FIXME: Could be an option depending on libresolv
-PCIUTILS_ENV += DNS=no
 ifdef PTXCONF_PCIUTILS_COMPRESS
-PCIUTILS_ENV += ZLIB=yes
+PCIUTILS_MAKEVARS += ZLIB=yes
 else
-PCIUTILS_ENV += ZLIB=no
+PCIUTILS_MAKEVARS += ZLIB=no
 endif
-#
-# autoconf
-#
-PCIUTILS_AUTOCONF := $(CROSS_AUTOCONF_USR)
 
-$(STATEDIR)/pciutils.prepare: $(pciutils_prepare_deps_default)
-	@$(call targetinfo, $@)
-	@$(call touch, $@)
-
-# ----------------------------------------------------------------------------
-# Compile
-# ----------------------------------------------------------------------------
-
-pciutils_compile: $(STATEDIR)/pciutils.compile
-
-$(STATEDIR)/pciutils.compile: $(pciutils_compile_deps_default)
-	@$(call targetinfo, $@)
-	cd $(PCIUTILS_DIR) && $(PCIUTILS_PATH) $(MAKE) $(PCIUTILS_ENV)
-	@$(call touch, $@)
-
-# ----------------------------------------------------------------------------
-# Install
-# ----------------------------------------------------------------------------
-
-pciutils_install: $(STATEDIR)/pciutils.install
-
-$(STATEDIR)/pciutils.install: $(pciutils_install_deps_default)
-	@$(call targetinfo, $@)
-	@$(call touch, $@)
+$(STATEDIR)/pciutils.prepare:
+	@$(call targetinfo)
+	@$(call touch)
 
 # ----------------------------------------------------------------------------
 # Target-Install
 # ----------------------------------------------------------------------------
 
-pciutils_targetinstall: $(STATEDIR)/pciutils.targetinstall
-
-$(STATEDIR)/pciutils.targetinstall: $(pciutils_targetinstall_deps_default)
-	@$(call targetinfo, $@)
+$(STATEDIR)/pciutils.targetinstall:
+	@$(call targetinfo)
 
 	@$(call install_init, pciutils)
 	@$(call install_fixup,pciutils,PACKAGE,pciutils)
 	@$(call install_fixup,pciutils,PRIORITY,optional)
 	@$(call install_fixup,pciutils,VERSION,$(PCIUTILS_VERSION))
 	@$(call install_fixup,pciutils,SECTION,base)
-	@$(call install_fixup,pciutils,AUTHOR,"Robert Schwebel <r.schwebel\@pengutronix.de>")
+	@$(call install_fixup,pciutils,AUTHOR,"Robert Schwebel <r.schwebel@pengutronix.de>")
 	@$(call install_fixup,pciutils,DEPENDS,)
 	@$(call install_fixup,pciutils,DESCRIPTION,missing)
 
-	@$(call install_copy, pciutils, 0, 0, 0755, $(PCIUTILS_DIR)/lspci, /usr/bin/lspci)
-	@$(call install_copy, pciutils, 0, 0, 0755, $(PCIUTILS_DIR)/setpci, /usr/bin/setpci)
+	@$(call install_copy, pciutils, 0, 0, 0755, -, /usr/bin/lspci)
+	@$(call install_copy, pciutils, 0, 0, 0755, -, /usr/bin/setpci)
 ifdef PTXCONF_PCIUTILS_COMPRESS
 	[ -f $(PCIUTILS_DIR)/pci.ids.gz ] || gzip --best -c $(PCIUTILS_DIR)/pci.ids > $(PCIUTILS_DIR)/pci.ids.gz
 	@$(call install_copy, pciutils, 0, 0, 0644, $(PCIUTILS_DIR)/pci.ids.gz, /usr/share/pci.ids.gz, n)
@@ -131,7 +83,7 @@ endif
 
 	@$(call install_finish,pciutils)
 
-	@$(call touch, $@)
+	@$(call touch)
 
 # ----------------------------------------------------------------------------
 # Clean

commit 68b8e1f2ffccaa57363b37aa925c5eb7af0b27c9

[pciutils] add missing ENV
unbroke which was broken in 5d038aa76f0d32904bee88da2bc4e65e2e8d4fd0

spottet by jbe

Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>

git-svn-id: https://svn.pengutronix.de/svn/ptxdist/trunks/ptxdist-trunk@10047 33e552b5-05e3-0310-8538-816dae2090ed

rules/pciutils.make
@@ -38,6 +38,8 @@ $(PCIUTILS_SOURCE):
 # ----------------------------------------------------------------------------
 
 PCIUTILS_PATH	:= PATH=$(CROSS_PATH)
+PCIUTILS_COMPILE_ENV	:= $(CROSS_ENV)
+
 PCIUTILS_MAKEVARS := \
 	CROSS_COMPILE=$(COMPILER_PREFIX) \
 	PREFIX=/usr \