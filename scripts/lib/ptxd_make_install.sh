5ce5811e9cd7ca14eb20c9e9af473ab00f3ddfcd
Marc Kleine-Budde <mkl@pengutronix.de>
2009-03-12 00:07:39 +0000

    [ptxd_make_install] move ptxd_install_init into separate file
    from scripts/lib/ptxd_make_install.sh ->
    to   scripts/lib/ptxd_make_00-init.sh
    
    Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>
    
    git-svn-id: https://svn.pengutronix.de/svn/ptxdist/trunks/ptxdist-trunk@10065 33e552b5-05e3-0310-8538-816dae2090ed

--- scripts/lib/ptxd_make_install.sh
+++ scripts/lib/ptxd_make_install.sh
@@ -7,36 +7,6 @@ ptxd_dopermissions() {
 export -f ptxd_dopermissions
 
 
-ptxd_install_init() {
-    local ptx_arch="$(ptxd_get_ptxconf PTXCONF_ARCH_STRING)"
-    local ipkg_arch="${ptx_arch}"
-
-    case "${ptx_arch}" in
-	arm)
-	    local target="$(ptxd_get_ptxconf PTXCONF_GNU_TARGET)"
-
-	    case "${target}" in
-		armb*gnueabi|armeb*gnueabi)
-		    ipkg_arch=armeb
-		    ;;
-		arm-*gnueabi|armle*gnueabi|armel*gnueabi)
-		    ipkg_arch=armel
-		    ;;
-		*)
-		    ipkg_arch=arm
-		    ;;
-	    esac
-    esac
-
-    PTXDIST_IPKG_ARCH_STRING="${ipkg_arch}"
-
-    export PTXDIST_IPKG_ARCH_STRING
-}
-
-ptxd_install_init
-
-
-
 #
 # -p PACKET
 #
