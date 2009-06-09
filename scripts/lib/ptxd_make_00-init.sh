5e2ccada3900ec038f506dcf2612881bffc6c2c8
Marc Kleine-Budde <mkl@pengutronix.de>
2009-06-09 20:46:08 +0000

    [ptxd_make_00-init] add function to figure out toolchain's sysroot
    Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>
    
    git-svn-id: https://svn.pengutronix.de/svn/ptxdist/trunks/ptxdist-trunk@10670 33e552b5-05e3-0310-8538-816dae2090ed

--- scripts/lib/ptxd_make_00-init.sh
+++ scripts/lib/ptxd_make_00-init.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-ptxd_install_init() {
+#
+# figure out arch string for ipkgs
+#
+# out:	$PTXDIST_IPKG_ARCH_STRING
+#
+ptxd_init_arch() {
     local ptx_arch="$(ptxd_get_ptxconf PTXCONF_ARCH_STRING)"
     local ipkg_arch="${ptx_arch}"
 
@@ -26,4 +31,48 @@ ptxd_install_init() {
     export PTXDIST_IPKG_ARCH_STRING
 }
 
-ptxd_install_init
+
+#
+# figure out toolchain's sysroot
+#
+# link a programm and look at the compiler's output
+#
+# in:	$compiler_prefix
+# out:	$PTXDIST_SYSROOT_TOOLCHAIN
+#
+ptxd_init_sysroot_toolchain_by_sysroot() {
+    local sysroot
+
+    sysroot="$(echo 'int main(void){return 0;}' | \
+	${compiler_prefix}gcc -x c -o /dev/null -v - 2>&1 | \
+	sed -ne "/.*collect2.*/s,.*--sysroot=\([^[:space:]]*\).*,\1,p")"
+
+    test -n "${sysroot}" || return 1
+
+    PTXDIST_SYSROOT_TOOLCHAIN="$(ptxd_abspath "${sysroot}")"
+}
+
+
+#
+# figure out the toolchain's sysroot
+#
+# out:	PTXDIST_SYSROOT_TOOLCHAIN
+#
+ptxd_init_sysroot_toolchain() {
+    local compiler_prefix="$(ptxd_get_ptxconf PTXCONF_COMPILER_PREFIX)"
+    unset PTXDIST_SYSROOT_TOOLCHAIN
+
+    ptxd_init_sysroot_toolchain_by_sysroot
+
+    export PTXDIST_SYSROOT_TOOLCHAIN
+}
+
+
+#
+# initialize some vars needed in PTXdist's make
+#
+ptxd_make_init() {
+    ptxd_init_arch &&
+    ptxd_init_sysroot_toolchain || return
+}
+ptxd_make_init
