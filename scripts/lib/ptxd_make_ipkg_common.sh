eae2c84b174cba7c1bc29d059957dbcd0913b3dc
Marc Kleine-Budde <mkl@pengutronix.de>
2009-06-14 22:26:06 +0000

    [ptxd_make_ipkg_common] provide common function for ipkg packaging
    Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>
    
    git-svn-id: https://svn.pengutronix.de/svn/ptxdist/trunks/ptxdist-trunk@10781 33e552b5-05e3-0310-8538-816dae2090ed

---
+++ scripts/lib/ptxd_make_ipkg_common.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# Copyright (C) 2009 by Marc Kleine-Budde <mkl@pengutronix.de>
+#
+# See CREDITS for details about who has contributed to this project.
+#
+# For further information about the PTXdist project and license conditions
+# see the README file.
+#
+
+
+#
+# initialize variables needed to package ipkgs
+#
+ptxd_make_ipkg_init() {
+    pkg_ipkg_tmp="${pkg_xpkg_tmp}/ipkg"
+    pkg_ipkg_control_dir="${pkg_ipkg_tmp}/CONTROL"
+    pkg_ipkg_control="${pkg_ipkg_control_dir}/control"
+}
+export -f ptxd_make_ipkg_init
