145a4a53f83203dbd60682fc81089da405c09f51
Marc Kleine-Budde <mkl@pengutronix.de>
2009-06-15 22:30:15 +0000

    [ptxd_make_world_install] provide generic install function
    Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>
    
    git-svn-id: https://svn.pengutronix.de/svn/ptxdist/trunks/ptxdist-trunk@10795 33e552b5-05e3-0310-8538-816dae2090ed

---
+++ rules/post/ptxd_make_world_install.make
@@ -0,0 +1,37 @@
+# -*-makefile-*-
+#
+# Copyright (C) 2009 by Marc Kleine-Budde <mkl@pengutronix.de>
+
+# See CREDITS for details about who has contributed to this project.
+#
+# For further information about the PTXdist project and license conditions
+# see the README file.
+#
+
+$(STATEDIR)/%.install-deactivated:
+	@$(call targetinfo)
+	@$(call world/install, $(PTX_MAP_TO_PACKAGE_$(*)))
+	@$(call touch)
+
+#
+# world/install
+#
+world/install = \
+	$(call world/env, $1) \
+	ptxd_make_world_install
+
+#
+# Perform standard install actions
+#
+# $1: label of the packet
+# $2: optional: alternative directory
+# $3: optional: "h" = install as a host tool
+# $4: optional: args to pass to make install call
+#
+install-deactivated = \
+	pkg_deprecated_install_builddir="$(call ptx/escape, $(2))"	\
+	pkg_deprecated_install_hosttool="$(call ptx/escape, $(3))"	\
+	pkg_deprecated_install_opt="$(call ptx/escape, $(4))"		\
+	$(call world/install, $(1))
+
+# vim: syntax=make
---
+++ scripts/lib/ptxd_make_world_install.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+#
+# Copyright (C) 2009 by Marc Kleine-Budde <mkl@pengutronix.de>
+# See CREDITS for details about who has contributed to this project.
+#
+# For further information about the PTXdist project and license conditions
+# see the README file.
+#
+
+#
+# $1:	destdir
+#
+# in:
+# $echo		optional
+# $fakeroot	optional
+#
+# FIXME: kick ${pkg_install_env}
+#
+ptxd_make_world_install_pkg() {
+    "${echo:-echo}" \
+	"${pkg_path}" \
+	"${pkg_make_env}" \
+	"${pkg_install_env}" \
+	"${MAKE}" \
+	-C "${pkg_build_dir}" \
+	"${pkg_install_opt}" \
+	install -j1 \
+	DESTDIR="${1}" | "${fakeroot:-fakeroot}" --
+    check_pipe_status
+}
+export -f ptxd_make_world_install_pkg
+
+
+#
+# install
+#
+# Perform standard install actions
+#
+ptxd_make_world_install_target() {
+    rm -rf -- "${pkg_pkgdir}}" &&
+    mkdir -p -- "${pkg_pkgdir}"/{etc,{,usr/}{lib,{,s}bin,include,{,share/}man/man{1,2,3,4,5,6,7,8,9}}} &&
+
+    ptxd_make_world_install_pkg "${pkg_pkgdir}" || return
+
+    # remove empty dirs
+    find "${pkg_pkgdir}" -type d -print0 | xargs -r -0 -- \
+	rmdir --ignore-fail-on-non-empty -p -- &&
+    check_pipe_status &&
+
+    if [ \! -e "${pkg_pkgdir}" ]; then
+	ptxd_warning "PKG doesn't install anything to '${pkg_pkgdir}'"
+	return
+    fi &&
+
+    # prefix paths in la files with sysroot
+    find "${pkg_pkgdir}" -name "*.la" -print0 | xargs -r -0 -- \
+	sed -i -e "/^dependency_libs/s:\( \)\(/lib\|/usr/lib\):\1${PTXDIST_SYSROOT_TARGET}\2:g" \
+	-e "/^libdir=/s:\(libdir='\)\(/lib\|/usr/lib\):\1${PTXDIST_SYSROOT_TARGET}\2:g" &&
+    check_pipe_status &&
+
+    cp -dprf -- "${pkg_pkgdir}"/* "${PTXDIST_SYSROOT_TARGET}"
+}
+export -f ptxd_make_world_install_target
+
+
+#
+# for cross pkg
+#
+ptxd_make_world_install_cross() {
+    ptxd_make_world_install_pkg
+}
+export -f ptxd_make_world_install_cross
+
+
+#
+# for host pkgs
+#
+ptxd_make_world_install_host() {
+    #
+    # fakeroot is a host pkg and
+    # might not be available, yet
+    #
+    local echo="eval"
+    local fakeroot="cat"
+
+    ptxd_make_world_install_pkg
+}
+export -f ptxd_make_world_install_host
+
+
+#
+# generic "make install" function
+#
+ptxd_make_world_install() {
+    ptxd_make_world_init &&
+    ptxd_make_world_install_"${pkg_type}"
+}
+export -f ptxd_make_world_install
