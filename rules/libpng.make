ringlej/ptxdist

commit 05dc60908915b986629abcda160557c811412821

[libpng] version bump to 1.2.41
Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>

rules/libpng.make
@@ -18,7 +18,7 @@ PACKAGES-$(PTXCONF_LIBPNG) += libpng
 #
 # Paths and names
 #
-LIBPNG_VERSION	:= 1.2.39
+LIBPNG_VERSION	:= 1.2.41
 LIBPNG		:= libpng-$(LIBPNG_VERSION)
 LIBPNG_SUFFIX	:= tar.bz2
 LIBPNG_URL	:= $(PTXCONF_SETUP_SFMIRROR)/libpng/$(LIBPNG).$(LIBPNG_SUFFIX)
@@ -76,7 +76,7 @@ $(STATEDIR)/libpng.targetinstall:
 	@$(call install_fixup, libpng,DESCRIPTION,missing)
 
 	@$(call install_copy, libpng, 0, 0, 0644, -, \
-		/usr/lib/libpng12.so.0.39.0)
+		/usr/lib/libpng12.so.0.41.0)
 	@$(call install_link, libpng, libpng12.so.0.39.0, /usr/lib/libpng12.so.0)
 	@$(call install_link, libpng, libpng12.so.0.39.0, /usr/lib/libpng12.so)
 

patches/libpng-1.2.39/series
@@ -1,1 +1,0 @@
-libpng_config_sysroot.diff -p0

patches/libpng-1.2.41/libpng_config_sysroot.diff
@@ -7,11 +7,11 @@ Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>
  scripts/libpng-config.in |    6 +++---
  1 file changed, 3 insertions(+), 3 deletions(-)
 
-Index: scripts/libpng-config.in
+Index: libpng-1.2.41/scripts/libpng-config.in
 ===================================================================
---- scripts/libpng-config.in.orig
-+++ scripts/libpng-config.in
-@@ -15,8 +15,8 @@ libdir="@libdir@"
+--- libpng-1.2.41.orig/scripts/libpng-config.in
++++ libpng-1.2.41/scripts/libpng-config.in
+@@ -18,8 +18,8 @@ libdir="@libdir@"
  includedir="@includedir@/libpng@PNGLIB_MAJOR@@PNGLIB_MINOR@"
  libs="-lpng@PNGLIB_MAJOR@@PNGLIB_MINOR@"
  all_libs="-lpng@PNGLIB_MAJOR@@PNGLIB_MINOR@ @LIBS@"
@@ -22,7 +22,7 @@ Index: scripts/libpng-config.in
  R_opts=""
  cppflags=""
  ccopts="@LIBPNG_NO_MMX@"
-@@ -56,7 +56,7 @@ while test $# -gt 0; do
+@@ -59,7 +59,7 @@ while test $# -gt 0; do
      case "$1" in
  
      --prefix)

patches/libpng-1.2.41/series
@@ -1,0 +1,1 @@
+libpng_config_sysroot.diff

commit 72f83cde55449e75e4f07464e0582083855a4118

[libpng] fix links for libraries
Which was broken in 05dc60908915b986629abcda160557c811412821
due to a version bump.

Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>

rules/libpng.make
@@ -77,8 +77,8 @@ $(STATEDIR)/libpng.targetinstall:
 
 	@$(call install_copy, libpng, 0, 0, 0644, -, \
 		/usr/lib/libpng12.so.0.41.0)
-	@$(call install_link, libpng, libpng12.so.0.39.0, /usr/lib/libpng12.so.0)
-	@$(call install_link, libpng, libpng12.so.0.39.0, /usr/lib/libpng12.so)
+	@$(call install_link, libpng, libpng12.so.0.41.0, /usr/lib/libpng12.so.0)
+	@$(call install_link, libpng, libpng12.so.0.41.0, /usr/lib/libpng12.so)
 
 	@$(call install_finish, libpng)
 