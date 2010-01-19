ringlej/ptxdist

commit a1af64a2cdb964ab52752c1e8b111622de2cc6e0

[host-squashfs-tools] fix HOST_SQUASHFS_TOOLS_DIR
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/host-squashfs-tools.make
@@ -20,7 +20,8 @@ HOST_SQUASHFS_TOOLS		:= squashfs$(HOST_SQUASHFS_TOOLS_VERSION)
 HOST_SQUASHFS_TOOLS_SUFFIX	:= tar.gz
 HOST_SQUASHFS_TOOLS_URL		:= $(PTXCONF_SETUP_SFMIRROR)/squashfs/$(HOST_SQUASHFS_TOOLS).$(HOST_SQUASHFS_TOOLS_SUFFIX)
 HOST_SQUASHFS_TOOLS_SOURCE	:= $(SRCDIR)/$(HOST_SQUASHFS_TOOLS).$(HOST_SQUASHFS_TOOLS_SUFFIX)
-HOST_SQUASHFS_TOOLS_DIR		:= $(HOST_BUILDDIR)/$(HOST_SQUASHFS_TOOLS)/squashfs-tools
+HOST_SQUASHFS_TOOLS_DIR		:= $(HOST_BUILDDIR)/$(HOST_SQUASHFS_TOOLS)
+HOST_SQUASHFS_TOOLS_SUBDIR	:= squashfs-tools
 
 # ----------------------------------------------------------------------------
 # Get
@@ -31,29 +32,11 @@ $(HOST_SQUASHFS_TOOLS_SOURCE):
 	@$(call get, HOST_SQUASHFS_TOOLS)
 
 # ----------------------------------------------------------------------------
-# Prepare
-# ----------------------------------------------------------------------------
-
-HOST_SQUASHFS_TOOLS_COMPILE_ENV := \
-	$(HOST_ENV)
-
-HOST_SQUASHFS_TOOLS_MAKEVARS	:= \
-	INSTALL_DIR="$(PTXCONF_SYSROOT_HOST)/sbin"
-
-$(STATEDIR)/host-squashfs-tools.prepare:
-	@$(call targetinfo)
-	@$(call touch)
-
-# ----------------------------------------------------------------------------
 # Compile
 # ----------------------------------------------------------------------------
 
-$(STATEDIR)/host-squashfs-tools.compile:
-	@$(call targetinfo)
-	cd $(HOST_SQUASHFS_TOOLS_DIR) && \
-		$(HOST_SQUASHFS_TOOLS_PATH) \
-		$(HOST_SQUASHFS_TOOLS_COMPILE_ENV) \
-		$(MAKE) $(HOST_SQUASHFS_TOOLS_MAKEVARS) $(PARALLELMFLAGS_BROKEN)
-	@$(call touch)
+HOST_SQUASHFS_TOOLS_MAKE_ENV	:= $(HOST_ENV)
+HOST_SQUASHFS_TOOLS_MAKE_PAR	:= NO
+HOST_SQUASHFS_TOOLS_MAKE_OPT	:= INSTALL_DIR="$(PTXCONF_SYSROOT_HOST)/sbin"
 
 # vim: syntax=make

commit 7896c0be1c0fcf3424ee1c832e5a4342cec34721

[host-squashfs-tools] fix install stage
..which was broken in a1af64a2cdb964ab52752c1e8b111622de2cc6e0

Reported-by: on Ringle <jon@ringle.org>
Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>

rules/host-squashfs-tools.make
@@ -1,6 +1,7 @@
 # -*-makefile-*-
 #
 # Copyright (C) 2009 by Jon Ringle <jon@ringle.org>
+#               2010 by Marc Kleine-Budde <mkl@pengutronix.de>
 # See CREDITS for details about who has contributed to this project.
 #
 # For further information about the PTXdist project and license conditions
@@ -35,8 +36,8 @@ $(HOST_SQUASHFS_TOOLS_SOURCE):
 # Compile
 # ----------------------------------------------------------------------------
 
-HOST_SQUASHFS_TOOLS_MAKE_ENV	:= $(HOST_ENV)
-HOST_SQUASHFS_TOOLS_MAKE_PAR	:= NO
-HOST_SQUASHFS_TOOLS_MAKE_OPT	:= INSTALL_DIR="$(PTXCONF_SYSROOT_HOST)/sbin"
+HOST_SQUASHFS_TOOLS_MAKE_ENV := $(HOST_ENV)
+HOST_SQUASHFS_TOOLS_MAKE_PAR := NO
+HOST_SQUASHFS_TOOLS_INSTALL_OPT := install INSTALL_DIR="$(PTXCONF_SYSROOT_HOST)/sbin"
 
 # vim: syntax=make