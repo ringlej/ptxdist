54ce1718e7c90844cbd7280eb3a0a8b24bd8687a
Marc Kleine-Budde <mkl@pengutronix.de>
2010-04-03 18:53:36 +0200

    add usual headers to files
    Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>

--- scripts/lib/ptxd_make_fixpermissions.sh
+++ scripts/lib/ptxd_make_fixpermissions.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-
+#
+# Copyright (C) 2008, 2009 by Marc Kleine-Budde <mkl@pengutronix.de>
+#
+# See CREDITS for details about who has contributed to this project.
+#
+# For further information about the PTXdist project and license conditions
+# see the README file.
+#
 
 ptxd_make_fixpermissions_generate() {
     case "${kind}" in
