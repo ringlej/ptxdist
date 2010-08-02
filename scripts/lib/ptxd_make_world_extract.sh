ringlej/ptxdist

commit 0dbd7c1caad8dcc57e76caa05d5791f4fae238d4

[ptxd_make_world_extract] get rid of cmd line option -d
Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>

scripts/lib/ptxd_make_world_extract.sh
@@ -8,27 +8,12 @@
 #
 
 #
-# -d DEST
+# ptxd_make_extract
 #
 ptxd_make_extract() {
     ptxd_make_world_init || return
 
-    local opt
-
-    . ${PTXDIST_TOPDIR}/scripts/ptxdist_vars.sh || return 1
-
-    while getopts "s:p:u:d:" opt; do
-	case "${opt}" in
-	    d)
-		local dest="${OPTARG}"
-		;;
-	    *)
-		return 1
-		;;
-	esac
-    done
-
-    dest="${dest:-${BUILDDIR}}"
+    pkg_extract_dir="${pkg_deprecated_extract_dir:-${pkg_extract_dir}}"
 
     case "${pkg_url}" in
 	file://*)
@@ -63,9 +48,9 @@ ptxd_make_extract() {
     fi
 
     echo "extract: archive=${pkg_src}"
-    echo "extract: dest=${dest}"
+    echo "extract: dest=${pkg_extract_dir}"
 
-    ptxd_make_extract_archive "${pkg_src}" "${dest}"
+    ptxd_make_extract_archive "${pkg_src}" "${pkg_extract_dir}"
 }
 
 export -f ptxd_make_extract

rules/post/ptxd_make_world_extract.make
@@ -20,8 +20,8 @@
 #
 extract = \
 	$(call world/env, $(1)) \
-	ptxd_make_extract \
-		-d "$(strip $(2))"
+	pkg_deprecated_extract_dir="$(call ptx/escape, $(strip $(2)))" \
+	ptxd_make_extract
 
 
 ### --- for CROSS packages only ---

commit 4f1832d9a178062b23feec138e6c548665e66da3

[ptxd_make_world_extract] fix mkdir
This fixes an error introduced in commit
"0dbd7c1caad8dcc57e76caa05d5791f4fae238d4".

Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>

scripts/lib/ptxd_make_world_extract.sh
@@ -36,7 +36,7 @@ ptxd_make_world_extract() {
 	    ;;
     esac
 
-    mkdir -p "${dest}" || return
+    mkdir -p "${pkg_extract_dir}" || return
 
     echo "\
 extract: pkg_src=$(ptxd_print_path ${pkg_src})