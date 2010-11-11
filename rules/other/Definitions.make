ringlej/ptxdist

commit 9333a284047a6c5551fdd1bff006fd51b3948422

[rules] define and use PTXDIST_PATH_(PRE|POST)RULES
Don't explicitly use workspace and ptxdist rules dirs when including
pre and post makefiles. Use a path variable instead.

It's now possible to add pre and post makefiles in the platform.

Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/other/Toplevel.make
@@ -37,10 +37,9 @@ endif
 include $(PTX_MAP_ALL_MAKE)
 
 include $(RULESDIR)/other/Namespace.make
-include $(wildcard $(PRERULESDIR)/*.make)
 
 # might be non existent
-include $(wildcard $(PROJECTPRERULESDIR)/*.make)
+include $(wildcard $(addsuffix /*.make,$(subst :,$(space),$(PTXDIST_PATH_PRERULES))))
 
 include $(PTX_DGEN_DEPS_PRE)
 include $(PTX_DGEN_RULESFILES_MAKE)
@@ -96,8 +95,7 @@ PTX_PACKAGES_INSTALL	:= \
 	$(PACKAGES-b)
 
 # might be non existent
-include $(wildcard $(POSTRULESDIR)/*.make)
-include $(wildcard $(PROJECTPOSTRULESDIR)/*.make)
+include $(wildcard $(addsuffix /*.make,$(subst :,$(space),$(PTXDIST_PATH_POSTRULES))))
 # install_alternative and install_copy has some configuration defined
 # dependencies. include the files specifying these dependencies.
 include $(wildcard $(STATEDIR)/*.deps)

scripts/lib/ptxd_lib_00-init.sh
@@ -92,6 +92,12 @@ ptxd_init_ptxdist_path() {
     PTXDIST_PATH_RULES="${PTXDIST_PATH//://rules:}"
     export PTXDIST_PATH_RULES
 
+    PTXDIST_PATH_PRERULES="${PTXDIST_PATH_RULES//://pre:}"
+    export PTXDIST_PATH_PRERULES
+
+    PTXDIST_PATH_POSTRULES="${PTXDIST_PATH_RULES//://post:}"
+    export PTXDIST_PATH_POSTRULES
+
     ptxd_init_ptxdist_path_sysroot
 }
 

commit a1f643c5c863e940cd50ab278b51bb0a20fc25d1

[rules] reverse order when including pre/post rules
This fixes a bug introduced by 9333a284047a6c5551fdd1bff006fd51b3948422
${PTXDIST_TOPDIR}/rules/pre/ was included last makeing it impossible to
overwrite variables with files in ${PTXDIST_WORKSPACE}/rules/pre/

Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/other/Definitions.make
@@ -56,5 +56,9 @@ define tr_sh
 $(strip $(shell echo $(1) | sed 'y%*+%pp%;s%[^_abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789]%_%g'))
 endef
 
+define reverse
+$(if $(1),$(call reverse,$(wordlist 2,$(words $(1)),$(1)))) $(firstword $(1))
+endef
+
 # vim600:set foldmethod=marker:
 # vim600:set syntax=make:

rules/other/Toplevel.make
@@ -39,7 +39,7 @@ include $(PTX_MAP_ALL_MAKE)
 include $(RULESDIR)/other/Namespace.make
 
 # might be non existent
-include $(wildcard $(addsuffix /*.make,$(subst :,$(space),$(PTXDIST_PATH_PRERULES))))
+include $(wildcard $(addsuffix /*.make,$(call reverse,$(subst :,$(space),$(PTXDIST_PATH_PRERULES)))))
 
 include $(PTX_DGEN_DEPS_PRE)
 include $(PTX_DGEN_RULESFILES_MAKE)
@@ -95,7 +95,7 @@ PTX_PACKAGES_INSTALL	:= \
 	$(PACKAGES-b)
 
 # might be non existent
-include $(wildcard $(addsuffix /*.make,$(subst :,$(space),$(PTXDIST_PATH_POSTRULES))))
+include $(wildcard $(addsuffix /*.make,$(call reverse,$(subst :,$(space),$(PTXDIST_PATH_POSTRULES)))))
 # install_alternative and install_copy has some configuration defined
 # dependencies. include the files specifying these dependencies.
 include $(wildcard $(STATEDIR)/*.deps)