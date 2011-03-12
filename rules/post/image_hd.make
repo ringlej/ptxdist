886cf5cf7d2f75e5b8624a7b020c34c554b48fba
Michael Olbrich <m.olbrich@pengutronix.de>
2011-03-12 15:05:00 +0100

    image_hd: move adding the bootloader to a separate script
    Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

--- rules/post/image_hd.make
+++ rules/post/image_hd.make
@@ -22,15 +22,18 @@ endif
 ifdef PTXCONF_IMAGE_HD_PART4
 	GENHDIMARGS += -p $(PTXCONF_IMAGE_HD_PART4_START):$(PTXCONF_IMAGE_HD_PART4_END):$(PTXCONF_IMAGE_HD_PART4_TYPE):
 endif
-ifdef PTXCONF_GRUB
-	GENHDIMARGS += -m $(GRUB_DIR)/stage1/stage1
-	GENHDIMARGS += -n $(GRUB_DIR)/stage2/stage2
-endif
 
 $(IMAGEDIR)/hd.img: $(IMAGEDIR)/root.ext2
 	@echo "Creating hdimg from root.ext2";					\
 	PATH=$(PTXCONF_SYSROOT_HOST)/bin:$$PATH $(PTXDIST_TOPDIR)/scripts/genhdimg	\
 	-o $@ $(GENHDIMARGS)
+ifdef PTXCONF_GRUB
+	@echo
+	@echo "-----------------------------------"
+	@echo "Making the image bootable with grub"
+	@echo "-----------------------------------"
+	@ptxd_make_bootable $@ $(PTXCONF_IMAGE_HD_PART1_START) $(GRUB_DIR)/stage1/stage1 $(GRUB_DIR)/stage1/stage2
+endif
 	@echo "done."
 
 # vim: syntax=make
---
+++ scripts/lib/ptxd_make_bootable.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+#
+# Copyright (C) 2011 by Michael Olbrich <m.olbrich@pengutronix.de>
+#
+# See CREDITS for details about who has contributed to this project.
+#
+# For further information about the PTXdist project and license conditions
+# see the README file.
+#
+
+#
+# Make a disk image bootable. The bootloader is written to the MBR and
+# the following sectors without touching the partition table.
+#
+# This will fail if the bootloader is too large for the free sectors.
+#
+# $1	the image to modify
+# $2	number of free sectors at the start of the image
+# $3	first part of the bootloader for the code area of the MBR
+# $4	second part of the bootloader for sector 2..n
+#
+ptxd_make_bootable() {
+    local image="$1"
+    local bytes="$[$2 * 512]"
+    local stage1="$3"
+    local stage2="$4"
+    local opt="conv=notrunc"
+    local opt2
+
+    if [ ! -w "${image}" ]; then
+	ptxd_bailout "Cannot write image file '${stage1}'"
+    fi
+    if [ ! -r "${stage1}" ]; then
+	ptxd_bailout "Cannot open stage1 file '${stage1}'"
+    fi
+    if [ -z "${stage2}" ]; then
+	stage2="${stage1}"
+	opt2="${opt} seek=1 skip=1"
+    elif [ ! -r "${stage2}" ]; then
+	ptxd_bailout "Cannot open stage2 file '${stage1}'"
+    else
+	bytes="$[$bytes - 512]"
+    fi
+    local needed="$(stat --printf="%s" "${stage2}")"
+    if [ "${needed}" -gt "${bytes}" ]; then
+	ptxd_bailout "Not enough space to write stage2: available: ${bytes} needed: ${needed}"
+    fi
+
+    dd if="${stage1}" of="${image}" ${opt} bs=446 count=1 2>/dev/null &&
+    dd if="${stage2}" of="${image}" ${opt} ${opt2} bs=512 2>/dev/null
+}
+export -f ptxd_make_bootable
+
