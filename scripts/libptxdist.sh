ringlej/ptxdist

commit e449e28d4edfbf0ebc1fe9bfbbf0fa93e134e1c2

libptxdist: load scripts/lib/ptxd_make_* scripts from the BSP
this makes it possible to overwrite stuff exported during ptxd_make

Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

scripts/libptxdist.sh
@@ -337,8 +337,12 @@ export -f ptxd_kconfig
 # ("scripts/lib/ptxd_make_"*.sh)
 #
 ptxd_make() {
-	for lib in "${SCRIPTSDIR}/lib/ptxd_make_"*.sh; do
-		source "${lib}" || ptxd_bailout "failed to source lib: ${lib}"
+	local lib i
+	ptxd_in_path PTXDIST_PATH_SCRIPTS || return
+	for ((i=$((${#ptxd_reply[@]}-1)); i>=0; i--)) do
+		for lib in "${ptxd_reply[${i}]}/lib/ptxd_make_"*.sh; do
+			source "${lib}" || ptxd_bailout "failed to source lib: ${lib}"
+		done
 	done
 	${PTX_NICE:+nice -n ${PTX_NICE}} "${PTXCONF_SETUP_HOST_MAKE}" \
 	    "${PTX_MAKE_ARGS[@]}" "${PTXDIST_PARALLELMFLAGS_EXTERN}" \

scripts/lib/ptxd_lib_00-init.sh
@@ -101,6 +101,9 @@ ptxd_init_ptxdist_path() {
     PTXDIST_PATH_PLATFORMS="${PTXDIST_PATH//://platforms:}"
     export PTXDIST_PATH_PLATFORMS
 
+    PTXDIST_PATH_SCRIPTS="${PTXDIST_PATH//://scripts:}"
+    export PTXDIST_PATH_SCRIPTS
+
     ptxd_init_ptxdist_path_sysroot
 }
 

commit 05eef40ed8dc519509bac7717c1810d1eb99a66f

libptxdist: fix including scripts/lib/ptxd_make_*.sh
e449e28d4edfbf0ebc1fe9bfbbf0fa93e134e1c2 broke BSPs with a scripts/
directory but no file matching scripts/lib/ptxd_make_*.sh

Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

scripts/libptxdist.sh
@@ -338,9 +338,12 @@ export -f ptxd_kconfig
 #
 ptxd_make() {
 	local lib i
+	local -a dir
 	ptxd_in_path PTXDIST_PATH_SCRIPTS || return
-	for ((i=$((${#ptxd_reply[@]}-1)); i>=0; i--)) do
-		for lib in "${ptxd_reply[${i}]}/lib/ptxd_make_"*.sh; do
+	dir=( "${ptxd_reply[@]}" )
+	for ((i=$((${#dir[@]}-1)); i>=0; i--)) do
+		ptxd_get_path "${dir[${i}]}/lib/ptxd_make_"*.sh || continue
+		for lib in "${ptxd_reply[@]}"; do
 			source "${lib}" || ptxd_bailout "failed to source lib: ${lib}"
 		done
 	done