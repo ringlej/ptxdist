ringlej/ptxdist

commit c13c4ac2ddc6a5e2614073236c665ffe7e2e2c04

{os-release, rootfs}.make: Don't rely on GNU date
date -Iseconds is a GNU extension. Since that one is only used in two
places, don't check for GNU date but use the portable variant (format
specifiers).

Signed-off-by: Bernhard Walle <bernhard@bwalle.de>
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/rootfs.make
@@ -185,7 +185,7 @@ ifdef PTXCONF_ROOTFS_ISSUE
 	@$(call install_replace, rootfs, /etc/issue, \
 		@EXTRAVERSION@, $(PTXDIST_VERSION_SCM))
 	@$(call install_replace, rootfs, /etc/issue, \
-		@DATE@, $(shell date -Iseconds))
+		@DATE@, $(shell date +%Y-%M-%dT%H:%M:%S%z))
 	@$(call install_replace, rootfs, /etc/issue, \
 		@VENDOR@, $(PTXCONF_PROJECT_VENDOR))
 	@$(call install_replace, rootfs, /etc/issue, \

rules/os-release.make
@@ -46,7 +46,7 @@ $(STATEDIR)/os-release.targetinstall: $(PTXDIST_PTXCONFIG) $(PTXDIST_PLATFORMCON
 		@PTXDIST_VERSION@, $(PTXDIST_VERSION_FULL))
 
 	@$(call install_replace, os-release, /etc/os-release, \
-		@DATE@, $(shell date -Iseconds))
+		@DATE@, $(shell date +%Y-%M-%dT%H:%M:%S%z))
 
 
 	@$(call install_finish,os-release)

commit 2e0e1817b20691d55952a842a3b8b345a8416ad0

rules/rootfs.make: Fix date call
Apply the fix in 1139f0eb294834d54fbb4f1513e8696209839604 also to the
second invocation of date which my commit
c13c4ac2ddc6a5e2614073236c665ffe7e2e2c04 broke.

It fixes the problem that the date part of the timestamp contained
minutes (%M) instead of the month (%m).

Signed-off-by: Bernhard Walle <bernhard@bwalle.de>
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/rootfs.make
@@ -185,7 +185,7 @@ ifdef PTXCONF_ROOTFS_ISSUE
 	@$(call install_replace, rootfs, /etc/issue, \
 		@EXTRAVERSION@, $(PTXDIST_VERSION_SCM))
 	@$(call install_replace, rootfs, /etc/issue, \
-		@DATE@, $(shell date +%Y-%M-%dT%H:%M:%S%z))
+		@DATE@, $(shell date +%FT%T%z))
 	@$(call install_replace, rootfs, /etc/issue, \
 		@VENDOR@, $(PTXCONF_PROJECT_VENDOR))
 	@$(call install_replace, rootfs, /etc/issue, \