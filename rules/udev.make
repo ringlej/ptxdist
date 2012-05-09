ringlej/ptxdist

commit e6ad184d539042ae088b21f503c067d4afc55ab4

udev: version bump 162/172 -> 172/181
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/udev.make
@@ -1,6 +1,7 @@
 # -*-makefile-*-
 #
 # Copyright (C) 2005-2008 by Robert Schwebel
+#               2012 by Michael Olbrich <m.olbrich@pengutronix.de>
 #
 # See CREDITS for details about who has contributed to this project.
 #
@@ -16,29 +17,19 @@ PACKAGES-$(PTXCONF_UDEV) += udev
 #
 # Paths and names
 #
-ifndef PTXCONF_UDEV_EXPERIMENTAL
-UDEV_VERSION	:= 162
-UDEV_MD5	:= dd7099242e92084605e07db0c7299a02
-else
+ifdef PTXCONF_UDEV_LEGACY
 UDEV_VERSION	:= 172
 UDEV_MD5	:= bd122d04cf758441f498aad0169a454f
+else
+UDEV_VERSION	:= 181
+UDEV_MD5	:= 135c5acfd371febc5ed8326d48028922
 endif
 UDEV		:= udev-$(UDEV_VERSION)
 UDEV_SUFFIX	:= tar.bz2
+UDEV_URL	:= $(call ptx/mirror, KERNEL, utils/kernel/hotplug/$(UDEV).$(UDEV_SUFFIX))
 UDEV_SOURCE	:= $(SRCDIR)/$(UDEV).$(UDEV_SUFFIX)
 UDEV_DIR	:= $(BUILDDIR)/$(UDEV)
-
-UDEV_URL := \
-	$(call ptx/mirror, KERNEL, utils/kernel/hotplug/$(UDEV).$(UDEV_SUFFIX))
 UDEV_LICENSE	:= GPLv2
-
-# ----------------------------------------------------------------------------
-# Get
-# ----------------------------------------------------------------------------
-
-$(UDEV_SOURCE):
-	@$(call targetinfo)
-	@$(call get, UDEV)
 
 # ----------------------------------------------------------------------------
 # Prepare
@@ -46,115 +37,95 @@ $(UDEV_SOURCE):
 
 #
 # autoconf
-
+#
 UDEV_CONF_ENV := \
 	$(CROSS_ENV) \
 	CPPFLAGS="-I$(KERNEL_HEADERS_INCLUDE_DIR) $(CROSS_CPPFLAGS)"
 
-#
-# Note: explicit sbindir to avoid "${exec_prefix}/sbin/" on the target.
-UDEV_AUTOCONF := \
+UDEV_CONF_TOOL	:= autoconf
+UDEV_CONF_OPT	:= \
 	$(CROSS_AUTOCONF_ROOT) \
-	--libexecdir=/lib/udev \
-	--sbindir=/sbin \
-	\
+	$(GLOBAL_LARGE_FILE_OPTION) \
+	--disable-static \
+	--enable-shared \
+	--disable-gtk-doc \
+	--disable-gtk-doc-html \
+	--disable-gtk-doc-pdf \
+	--$(call ptx/endis,PTXCONF_UDEV_DEBUG)-debug \
+	--$(call ptx/endis,PTXCONF_UDEV_SYSLOG)-logging \
+	--$(call ptx/endis,PTXCONF_UDEV_LIBGUDEV)-gudev \
 	--disable-introspection \
-	--enable-shared
+	--$(call ptx/endis,PTXCONF_UDEV_KEYMAPS)-keymap \
+	--$(call ptx/endis,PTXCONF_UDEV_PERSISTENT_GENERATOR)-rule_generator \
+	--$(call ptx/endis,PTXCONF_UDEV_ACL)-udev_acl \
+	--disable-floppy \
+	--$(call ptx/endis,PTXCONF_UDEV_PERSISTENT_EDD)-edd \
+	--with-usb-ids-path=/usr/share/usb.ids \
+	--with-pci-ids-path=/usr/share/pci.ids$(call ptx/ifdef, PTXCONF_PCIUTILS_COMPRESS,.gz,) \
+	--without-selinux \
+	--with-systemdsystemunitdir=/lib/systemd/system
 
-ifdef PTXCONF_PCIUTILS_COMPRESS
-UDEV_AUTOCONF += --with-pci-ids-path=/usr/share/pci.ids.gz
+ifdef PTXCONF_UDEV_LEGACY
+UDEV_CONF_OPT += \
+	--libexecdir=/lib/udev \
+	--enable-hwdb
 else
-UDEV_AUTOCONF += --with-pci-ids-path=/usr/share/pci.ids
-endif
-
-ifdef PTXCONF_UDEV_DEBUG
-UDEV_AUTOCONF	+= --enable-debug
-else
-UDEV_AUTOCONF	+= --disable-debug
-endif
-
-ifdef PTXCONF_UDEV_LIBGUDEV
-UDEV_AUTOCONF	+= --enable-gudev
-else
-UDEV_AUTOCONF	+= --disable-gudev
-endif
-
-ifdef PTXCONF_UDEV_SYSTEMD
-UDEV_AUTOCONF	+= --with-systemdsystemunitdir=/lib/systemd/system
-else
-UDEV_AUTOCONF	+= --without-systemdsystemunitdir
+UDEV_CONF_OPT += \
+	--libexecdir=/lib \
+	--$(call ptx/endis,PTXCONF_UDEV_MTD_PROBE)-mtd_probe
 endif
 
-ifndef PTXCONF_UDEV_EXPERIMENTAL
-ifeq ($(PTXCONF_ARCH_ARM)-$(PTXCONF_UDEV_EXTRA_HID2HCI),-y)
-UDEV_AUTOCONF	+= --enable-bluetooth
-else
-UDEV_AUTOCONF	+= --disable-bluetooth
-endif
-endif
+UDEV_RULES-y := \
+	50-udev-default.rules \
+	60-persistent-alsa.rules \
+	60-persistent-input.rules \
+	60-persistent-serial.rules \
+	60-persistent-storage-tape.rules \
+	60-persistent-storage.rules \
+	75-net-description.rules \
+	75-tty-description.rules \
+	78-sound-card.rules \
+	95-udev-late.rules
 
-ifdef PTXCONF_UDEV_EXTRA_KEYMAP
-UDEV_AUTOCONF	+= --enable-keymap
+UDEV_RULES-$(PTXCONF_UDEV_ACCELEROMETER)	+= 61-accelerometer.rules
+ifdef PTXCONF_UDEV_LEGACY
+UDEV_RULES-$(PTXCONF_UDEV_ACL)			+= 70-acl.rules
 else
-UDEV_AUTOCONF	+= --disable-keymap
+UDEV_RULES-$(PTXCONF_UDEV_ACL)			+= 70-udev-acl.rules
 endif
+UDEV_RULES-$(PTXCONF_UDEV_DRIVERS_RULES)	+= 80-drivers.rules
+UDEV_RULES-$(PTXCONF_UDEV_KEYMAPS)		+= 95-keyboard-force-release.rules
+UDEV_RULES-$(PTXCONF_UDEV_KEYMAPS)		+= 95-keymap.rules
+UDEV_RULES-$(PTXCONF_UDEV_MTD_PROBE)		+= 75-probe_mtd.rules
+UDEV_RULES-$(PTXCONF_UDEV_PERSISTENT_CDROM)	+= 60-cdrom_id.rules
+UDEV_RULES-$(PTXCONF_UDEV_PERSISTENT_EDD)	+= 61-persistent-storage-edd.rules
+UDEV_RULES-$(PTXCONF_UDEV_PERSISTENT_GENERATOR)	+= 75-cd-aliases-generator.rules
+UDEV_RULES-$(PTXCONF_UDEV_PERSISTENT_GENERATOR)	+= 75-persistent-net-generator.rules
+UDEV_RULES-$(PTXCONF_UDEV_PERSISTENT_V4L)	+= 60-persistent-v4l.rules
 
-ifdef PTXCONF_UDEV_EXTRA_UDEV_ACL
-UDEV_AUTOCONF	+= --enable-udev_acl
-else
-UDEV_AUTOCONF	+= --disable-udev_acl
-endif
+UDEV_HELPER-$(PTXCONF_UDEV_ACCELEROMETER)		+= accelerometer
+UDEV_HELPER-$(PTXCONF_UDEV_PERSISTENT_ATA)		+= ata_id
+UDEV_HELPER-$(PTXCONF_UDEV_PERSISTENT_CDROM)		+= cdrom_id
+UDEV_HELPER-$(PTXCONF_UDEV_COLLECT)			+= collect
+UDEV_HELPER-$(PTXCONF_UDEV_PERSISTENT_EDD)		+= edd_id
+UDEV_HELPER-$(PTXCONF_UDEV_KEYMAPS)			+= findkeyboards
+UDEV_HELPER-$(PTXCONF_UDEV_KEYMAPS)			+= keyboard-force-release.sh
+UDEV_HELPER-$(PTXCONF_UDEV_KEYMAPS)			+= keymap
+UDEV_HELPER-$(PTXCONF_UDEV_MTD_PROBE)			+= mtd_probe
+UDEV_HELPER-$(PTXCONF_UDEV_PERSISTENT_GENERATOR)	+= rule_generator.functions
+UDEV_HELPER-$(PTXCONF_UDEV_PERSISTENT_SCSI)		+= scsi_id
+UDEV_HELPER-$(PTXCONF_UDEV_ACL)				+= udev-acl
+UDEV_HELPER-$(PTXCONF_UDEV_PERSISTENT_V4L)		+= v4l_id
+UDEV_HELPER-$(PTXCONF_UDEV_PERSISTENT_GENERATOR)	+= write_cd_rules
+UDEV_HELPER-$(PTXCONF_UDEV_PERSISTENT_GENERATOR)	+= write_net_rules
 
-ifndef PTXCONF_UDEV_EXPERIMENTAL
-ifdef PTXCONF_UDEV_EXTRA_USB_DB
-UDEV_AUTOCONF	+= --enable-usbdb
-else
-UDEV_AUTOCONF	+= --disable-usbdb
-endif
-
-ifdef PTXCONF_UDEV_EXTRA_PCI_DB
-UDEV_AUTOCONF	+= --enable-pcidb
-else
-UDEV_AUTOCONF	+= --disable-pcidb
-endif
-else
-ifneq ($(PTXCONF_UDEV_EXTRA_USB_DB)$(PTXCONF_UDEV_EXTRA_PCI_DB),)
-UDEV_AUTOCONF	+= --enable-hwdb
-else
-UDEV_AUTOCONF	+= --disable-hwdb
-endif
-
-ifdef PTXCONF_UDEV_EXTRA_FLOPPY
-UDEV_AUTOCONF	+= --enable-floppy
-else
-UDEV_AUTOCONF	+= --disable-floppy
-endif
-
-ifdef PTXCONF_UDEV_EXTRA_EDD_ID
-UDEV_AUTOCONF	+= --enable-edd
-else
-UDEV_AUTOCONF	+= --disable-edd
-endif
-endif
-
-ifndef PTXCONF_UDEV_EXPERIMENTAL
-ifdef PTXCONF_UDEV_EXTRA_MOBILE_ACTION_MODESWITCH
-UDEV_AUTOCONF	+= --enable-action_modeswitch
-else
-UDEV_AUTOCONF	+= --disable-action_modeswitch
-endif
-endif
-
-ifdef PTXCONF_UDEV_SELINUX
-UDEV_AUTOCONF	+= --with-selinux
-else
-UDEV_AUTOCONF	+= --without-selinux
-endif
-
-ifdef PTXCONF_UDEV_SYSLOG
-UDEV_AUTOCONF	+= --enable-logging
-else
-UDEV_AUTOCONF	+= --disable-logging
+ifdef PTXCONF_UDEV_LEGACY
+DEV_HELPER-$(PTXCONF_UDEV_LEGACY)			+= firmware
+DEV_HELPER-$(PTXCONF_UDEV_LEGACY)			+= input_id
+DEV_HELPER-$(PTXCONF_UDEV_LEGACY)			+= path_id
+DEV_HELPER-$(PTXCONF_UDEV_PERSISTENT_PCI)		+= pci-db
+DEV_HELPER-$(PTXCONF_UDEV_PERSISTENT_USB)		+= usb-db
+DEV_HELPER-$(PTXCONF_UDEV_PERSISTENT_USB)		+= usb_id
 endif
 
 # ----------------------------------------------------------------------------
@@ -170,64 +141,34 @@ $(STATEDIR)/udev.targetinstall:
 	@$(call install_fixup, udev,AUTHOR,"Robert Schwebel <r.schwebel@pengutronix.de>")
 	@$(call install_fixup, udev,DESCRIPTION,missing)
 
-#	#
-#	# binaries
-#	#
+ifdef PTXCONF_UDEV_ETC_CONF
+	@$(call install_alternative, udev, 0, 0, 0644, /etc/udev/udev.conf)
+endif
 
+ifdef PTXCONF_UDEV_LEGACY
 	@$(call install_copy, udev, 0, 0, 0755, -, /sbin/udevd)
 	@$(call install_copy, udev, 0, 0, 0755, -, /sbin/udevadm)
+else
+	@$(call install_copy, udev, 0, 0, 0755, -, /lib/udev/udevd)
+	@$(call install_copy, udev, 0, 0, 0755, -, /bin/udevadm)
+endif
 
-#	#
-#	# default rules
-#	#
-
-# install everything apart of drivers rule.
-ifdef PTXCONF_UDEV_DEFAULT_RULES
-	@for rule in \
-			50-udev-default.rules \
-			60-persistent-alsa.rules \
-			60-persistent-input.rules \
-			60-persistent-serial.rules \
-			60-persistent-storage-tape.rules \
-			60-persistent-storage.rules \
-			95-udev-late.rules; \
-			do \
+	@$(foreach rule, $(UDEV_RULES-y), \
 		$(call install_copy, udev, 0, 0, 0644, -, \
-			/lib/udev/rules.d/$$rule); \
-	done
-ifdef PTXCONF_UDEV_EXTRA_USB_DB
-ifdef PTXCONF_UDEV_EXTRA_PCI_DB
-	@for rule in \
-			75-net-description.rules \
-			75-tty-description.rules \
-			78-sound-card.rules; \
-			do \
-		$(call install_copy, udev, 0, 0, 0644, -, \
-			/lib/udev/rules.d/$$rule); \
-	done
-endif
-endif
-endif
+			/lib/udev/rules.d/$(rule));)
 
-# install drivers rules.
-ifdef PTXCONF_UDEV_DEFAULT_DRIVERS_RULES
-	@$(call install_copy, udev, 0, 0, 0644, -, \
-		/lib/udev/rules.d/80-drivers.rules)
-endif
-
-# install default keymaps.
-ifdef PTXCONF_UDEV_DEFAULT_KEYMAPS
-	@cd $(UDEV_PKGDIR)/lib/udev/keymaps; \
-	for file in `find . -type f`; do \
-		$(call install_copy, udev, 0, 0, 0644, \
-			$(UDEV_PKGDIR)/lib/udev/keymaps/$$file, \
-			/lib/udev/keymaps/$$file, n); \
+ifdef PTXCONF_UDEV_KEYMAPS
+	@cd $(UDEV_PKGDIR) && \
+	for keymap in `find lib/udev/keymaps/ -type f`; do \
+		$(call install_copy, udev, 0, 0, 0644, -, /$$keymap); \
 	done
 endif
 
 ifdef PTXCONF_UDEV_CUST_RULES
 	@if [ -d $(PTXDIST_WORKSPACE)/projectroot/lib/udev/rules.d/ ]; then \
-		$(call install_tree, udev, 0, 0, $(PTXDIST_WORKSPACE)/projectroot/lib/udev/rules.d, /lib/udev/rules.d); \
+		$(call install_tree, udev, 0, 0, \
+			$(PTXDIST_WORKSPACE)/projectroot/lib/udev/rules.d, \
+			/lib/udev/rules.d); \
 	else \
 		echo "UDEV_CUST_RULES is enabled but Directory containing" \
 			"customized udev rules is missing!"; \
@@ -235,9 +176,23 @@ ifdef PTXCONF_UDEV_CUST_RULES
 	fi
 endif
 
-#	#
-#	# startup script
-#	#
+	@$(foreach helper, $(UDEV_HELPER-y), \
+		$(call install_copy, udev, 0, 0, 0644, -, \
+			/lib/udev//$(helper));)
+
+ifdef PTXCONF_UDEV_ACL
+	@$(call install_link, udev, ../../udev/udev-acl, \
+		/lib/ConsoleKit/run-seat.d/udev-acl.ck)
+endif
+
+ifdef PTXCONF_UDEV_LIBUDEV
+	@$(call install_lib, udev, 0, 0, 0644, libudev)
+endif
+
+ifdef PTXCONF_UDEV_LIBGUDEV
+	@$(call install_lib, udev, 0, 0, 0644, libgudev-1.0)
+endif
+
 ifdef PTXCONF_UDEV_STARTSCRIPT
 ifdef PTXCONF_INITMETHOD_BBINIT
 	@$(call install_alternative, udev, 0, 0, 0755, /etc/init.d/udev)
@@ -255,152 +210,6 @@ ifdef PTXCONF_INITMETHOD_UPSTART
 	@$(call install_alternative, udev, 0, 0, 0644, /etc/init/udev-finish.conf)
 endif
 endif
-
-
-#	#
-#	# Install a configuration on demand only
-#	#
-ifdef PTXCONF_UDEV_ETC_CONF
-	@$(call install_alternative, udev, 0, 0, 0644, /etc/udev/udev.conf)
-endif
-
-#	#
-#	# utilities from extra/
-#	#
-ifdef PTXCONF_UDEV_EXTRA_ATA_ID
-	@$(call install_copy, udev, 0, 0, 0755, -, /lib/udev/ata_id)
-endif
-
-ifdef PTXCONF_UDEV_EXTRA_CDROM_ID
-	@$(call install_copy, udev, 0, 0, 0755, -, /lib/udev/cdrom_id)
-	@$(call install_copy, udev, 0, 0, 0644, -, \
-		/lib/udev/rules.d/60-cdrom_id.rules,n)
-	@$(call install_copy, udev, 0, 0, 0644, -, \
-		/lib/udev/rules.d/75-cd-aliases-generator.rules,n)
-endif
-
-ifdef PTXCONF_UDEV_EXTRA_COLLECT
-	@$(call install_copy, udev, 0, 0, 0755, -, /lib/udev/collect)
-endif
-
-ifdef PTXCONF_UDEV_EXTRA_EDD_ID
-	@$(call install_copy, udev, 0, 0, 0755, -, /lib/udev/edd_id)
-	@$(call install_copy, udev, 0, 0, 0644, -, \
-		/lib/udev/rules.d/61-persistent-storage-edd.rules,n)
-endif
-
-ifdef PTXCONF_UDEV_EXTRA_FINDKEYBOARDS
-	@$(call install_copy, udev, 0, 0, 0755, -, /lib/udev/findkeyboards)
-endif
-
-ifdef PTXCONF_UDEV_EXTRA_FIRMWARE
-	@$(call install_copy, udev, 0, 0, 0755, -, /lib/udev/firmware)
-	@$(call install_copy, udev, 0, 0, 0644, -, \
-		/lib/udev/rules.d/50-firmware.rules,n)
-endif
-
-ifdef PTXCONF_UDEV_EXTRA_FLOPPY
-	@$(call install_copy, udev, 0, 0, 0755, -, \
-		/lib/udev/create_floppy_devices)
-	@$(call install_copy, udev, 0, 0, 0644, -, \
-		/lib/udev/rules.d/60-floppy.rules)
-endif
-
-ifdef PTXCONF_UDEV_EXTRA_FSTAB_IMPORT
-	@$(call install_copy, udev, 0, 0, 0755, -, /lib/udev/fstab_import)
-	@$(call install_copy, udev, 0, 0, 0644, -, \
-		/lib/udev/rules.d/79-fstab_import.rules)
-endif
-
-ifndef PTXCONF_ARCH_ARM
-ifdef PTXCONF_UDEV_EXTRA_HID2HCI
-	@$(call install_copy, udev, 0, 0, 0755, -, /lib/udev/hid2hci)
-	@$(call install_copy, udev, 0, 0, 0644, -, \
-		/lib/udev/rules.d/70-hid2hci.rules,n)
-endif
-endif
-
-ifdef PTXCONF_UDEV_EXTRA_INPUT_ID
-	@$(call install_copy, udev, 0, 0, 0755, -, /lib/udev/input_id)
-endif
-
-ifdef PTXCONF_UDEV_EXTRA_KEYBOARD_FORCE_RELEASE
-	@$(call install_copy, udev, 0, 0, 0755, -, /lib/udev/keyboard-force-release.sh, n)
-	@$(call install_copy, udev, 0, 0, 0644, -, \
-		/lib/udev/rules.d/95-keyboard-force-release.rules,n)
-endif
-
-ifdef PTXCONF_UDEV_EXTRA_KEYMAP
-	@$(call install_copy, udev, 0, 0, 0755, -, /lib/udev/keymap)
-	@$(call install_copy, udev, 0, 0, 0644, -, \
-		/lib/udev/rules.d/95-keymap.rules,n)
-endif
-
-ifdef PTXCONF_UDEV_EXTRA_MOBILE_ACTION_MODESWITCH
-	@$(call install_copy, udev, 0, 0, 0755, -, /lib/udev/mobile-action-modeswitch)
-	@$(call install_copy, udev, 0, 0, 0644, -, \
-		/lib/udev/rules.d/61-mobile-action.rules)
-endif
-
-ifdef PTXCONF_UDEV_EXTRA_PATH_ID
-	@$(call install_copy, udev, 0, 0, 0755, -, /lib/udev/path_id)
-endif
-
-ifdef PTXCONF_UDEV_EXTRA_PCI_DB
-	@$(call install_copy, udev, 0, 0, 0755, -, /lib/udev/pci-db)
-endif
-
-ifdef PTXCONF_UDEV_EXTRA_RULE_GENERATOR
-	@$(call install_copy, udev, 0, 0, 0755, -, \
-		/lib/udev/rule_generator.functions)
-endif
-
-ifdef PTXCONF_UDEV_EXTRA_SCSI_ID
-	@$(call install_copy, udev, 0, 0, 0755, -, /lib/udev/scsi_id)
-endif
-
-ifdef PTXCONF_UDEV_EXTRA_UDEV_ACL
-	@$(call install_copy, udev, 0, 0, 0755, -, /lib/udev/udev-acl)
-	@$(call install_copy, udev, 0, 0, 0644, -, \
-		/lib/udev/rules.d/70-acl.rules,n)
-	@$(call install_link, udev, ../../udev/udev-acl, \
-		/lib/ConsoleKit/run-seat.d/udev-acl.ck)
-endif
-
-ifdef PTXCONF_UDEV_EXTRA_USB_DB
-	@$(call install_copy, udev, 0, 0, 0755, -, /lib/udev/usb-db)
-endif
-
-ifdef PTXCONF_UDEV_EXTRA_USB_ID
-	@$(call install_copy, udev, 0, 0, 0755, -, /lib/udev/usb_id)
-endif
-
-ifdef PTXCONF_UDEV_EXTRA_V4L_ID
-	@$(call install_copy, udev, 0, 0, 0755, -, /lib/udev/v4l_id)
-	@$(call install_copy, udev, 0, 0, 0644, -, \
-		/lib/udev/rules.d/60-persistent-v4l.rules,n)
-endif
-
-ifdef PTXCONF_UDEV_EXTRA_WRITE_CD_RULES
-	@$(call install_copy, udev, 0, 0, 0755, -, /lib/udev//write_cd_rules)
-endif
-
-ifdef PTXCONF_UDEV_EXTRA_WRITE_NET_RULES
-	@$(call install_copy, udev, 0, 0, 0755, -, /lib/udev//write_net_rules)
-	@$(call install_copy, udev, 0, 0, 0644, -, \
-		/lib/udev/rules.d/75-net-description.rules,n)
-	@$(call install_copy, udev, 0, 0, 0644, -, \
-		/lib/udev/rules.d/75-persistent-net-generator.rules,n)
-endif
-
-ifdef PTXCONF_UDEV_LIBUDEV
-	@$(call install_lib, udev, 0, 0, 0644, libudev)
-endif
-
-ifdef PTXCONF_UDEV_LIBGUDEV
-	@$(call install_lib, udev, 0, 0, 0644, libgudev-1.0)
-endif
-
 ifdef PTXCONF_UDEV_SYSTEMD
 	@$(call install_copy, udev, 0, 0, 0644, -, \
 		/lib/systemd/system/udev-kernel.socket)

patches/udev-162/autogen.sh
@@ -1,9 +1,0 @@
-#!/bin/bash
-
-autoreconf \
-	--force \
-	--install \
-	--warnings=syntax \
-	--warnings=obsolete \
-	--warnings=unsupported
-

patches/udev-162/remove-SOCK_CLOEXEC.diff
@@ -1,30 +1,0 @@
-From: Michael Olbrich <m.olbrich@pengutronix.de>
-Subject: HACK: remove SOCK_CLOEXEC usage
-
-SOCK_CLOEXEC is not supported by OSELAS.Toolchain-1.99.3
-
-Not for upstream.
-
-Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>
-
----
- udev/udevd.c |    3 ++-
- 1 file changed, 2 insertions(+), 1 deletion(-)
-
-Index: udev-162/udev/udevd.c
-===================================================================
---- udev-162.orig/udev/udevd.c
-+++ udev-162/udev/udevd.c
-@@ -982,10 +982,11 @@ static int init_notify(const char *state
- 		goto finish;
- 	}
- 
--	if ((fd = socket(AF_UNIX, SOCK_DGRAM|SOCK_CLOEXEC, 0)) < 0) {
-+	if ((fd = socket(AF_UNIX, SOCK_DGRAM, 0)) < 0) {
- 		r = -errno;
- 		goto finish;
- 	}
-+	util_set_fd_cloexec(fd);
- 
- 	memset(&sockaddr, 0, sizeof(sockaddr));
- 	sockaddr.sa.sa_family = AF_UNIX;

patches/udev-162/series
@@ -1,5 +1,0 @@
-udev-150-split-extras.diff
-udev-150-revert-SOCK_CLOEXEC.diff
-udev-150-revert-bsg_h-removal.diff
-udev-150-remove-le16toh.diff
-remove-SOCK_CLOEXEC.diff

patches/udev-162/udev-150-remove-le16toh.diff
@@ -1,30 +1,0 @@
-From: Michael Olbrich <m.olbrich@pengutronix.de>
-Subject: HACK: remove le16toh usage
-
-le16toh is not supported by OSELAS.Toolchain-1.99.3
-
-Not for upstream.
-
-Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>
-
----
- extras/ata_id/ata_id.c |    6 +++++-
- 1 file changed, 5 insertions(+), 1 deletion(-)
-
-Index: udev-162/extras/ata_id/ata_id.c
-===================================================================
---- udev-162.orig/extras/ata_id/ata_id.c
-+++ udev-162/extras/ata_id/ata_id.c
-@@ -167,7 +167,11 @@ static void disk_identify_fixup_uint16 (
- 	uint16_t *p;
- 
- 	p = (uint16_t *) identify;
--	p[offset_words] = le16toh (p[offset_words]);
-+#if __BYTE_ORDER == __LITTLE_ENDIAN
-+	p[offset_words] = p[offset_words];
-+#else
-+	p[offset_words] = __bswap_16 (p[offset_words]);
-+#endif
- }
- 
- /**

patches/udev-162/udev-150-revert-SOCK_CLOEXEC.diff
@@ -1,129 +1,0 @@
-From: Michael Olbrich <m.olbrich@pengutronix.de>
-Subject: HACK: revert 26347a4c5538008318188118872490128f43fcd3 (don't use SOCK_CLOEXEC)
-
-SOCK_CLOEXEC is not defined by the glibc in OSELAS.Toolchain-1.99.3
-
-Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>
-
----
- libudev/libudev-monitor.c |    6 ++++--
- libudev/libudev-private.h |    1 +
- libudev/libudev-util.c    |   12 ++++++++++++
- udev/udev-watch.c         |    6 ++++--
- udev/udevd.c              |    4 +++-
- 5 files changed, 24 insertions(+), 5 deletions(-)
-
-Index: udev-162/libudev/libudev-monitor.c
-===================================================================
---- udev-162.orig/libudev/libudev-monitor.c
-+++ udev-162/libudev/libudev-monitor.c
-@@ -144,12 +144,13 @@ struct udev_monitor *udev_monitor_new_fr
- 		util_strscpy(&udev_monitor->sun.sun_path[1], sizeof(udev_monitor->sun.sun_path)-1, socket_path);
- 		udev_monitor->addrlen = offsetof(struct sockaddr_un, sun_path) + strlen(socket_path)+1;
- 	}
--	udev_monitor->sock = socket(AF_LOCAL, SOCK_DGRAM|SOCK_CLOEXEC, 0);
-+	udev_monitor->sock = socket(AF_LOCAL, SOCK_DGRAM, 0);
- 	if (udev_monitor->sock == -1) {
- 		err(udev, "error getting socket: %m\n");
- 		free(udev_monitor);
- 		return NULL;
- 	}
-+	util_set_fd_cloexec(udev_monitor->sock);
- 
- 	dbg(udev, "monitor %p created with '%s'\n", udev_monitor, socket_path);
- 	return udev_monitor;
-@@ -201,12 +202,13 @@ struct udev_monitor *udev_monitor_new_fr
- 	if (udev_monitor == NULL)
- 		return NULL;
- 
--	udev_monitor->sock = socket(PF_NETLINK, SOCK_DGRAM|SOCK_CLOEXEC, NETLINK_KOBJECT_UEVENT);
-+	udev_monitor->sock = socket(PF_NETLINK, SOCK_DGRAM, NETLINK_KOBJECT_UEVENT);
- 	if (udev_monitor->sock == -1) {
- 		err(udev, "error getting socket: %m\n");
- 		free(udev_monitor);
- 		return NULL;
- 	}
-+	util_set_fd_cloexec(udev_monitor->sock);
- 
- 	udev_monitor->snl.nl_family = AF_NETLINK;
- 	udev_monitor->snl.nl_groups = group;
-Index: udev-162/libudev/libudev-private.h
-===================================================================
---- udev-162.orig/libudev/libudev-private.h
-+++ udev-162/libudev/libudev-private.h
-@@ -205,6 +205,7 @@ size_t util_strscpyl(char *dest, size_t
- int udev_util_replace_whitespace(const char *str, char *to, size_t len);
- int udev_util_replace_chars(char *str, const char *white);
- int udev_util_encode_string(const char *str, char *str_enc, size_t len);
-+void util_set_fd_cloexec(int fd);
- unsigned int util_string_hash32(const char *key);
- uint64_t util_string_bloom64(const char *str);
- 
-Index: udev-162/libudev/libudev-util.c
-===================================================================
---- udev-162.orig/libudev/libudev-util.c
-+++ udev-162/libudev/libudev-util.c
-@@ -535,6 +535,18 @@ static unsigned int murmur_hash2(const c
- 	return h;
- }
- 
-+void util_set_fd_cloexec(int fd)
-+{
-+	int flags;
-+
-+	flags = fcntl(fd, F_GETFD);
-+	if (flags < 0)
-+		flags = FD_CLOEXEC;
-+	else
-+		flags |= FD_CLOEXEC;
-+	fcntl(fd, F_SETFD, flags);
-+}
-+
- unsigned int util_string_hash32(const char *str)
- {
- 	return murmur_hash2(str, strlen(str), 0);
-Index: udev-162/udev/udev-watch.c
-===================================================================
---- udev-162.orig/udev/udev-watch.c
-+++ udev-162/udev/udev-watch.c
-@@ -38,8 +38,10 @@ static int inotify_fd = -1;
-  */
- int udev_watch_init(struct udev *udev)
- {
--	inotify_fd = inotify_init1(IN_CLOEXEC);
--	if (inotify_fd < 0)
-+	inotify_fd = inotify_init();
-+	if (inotify_fd >= 0)
-+		util_set_fd_cloexec(inotify_fd);
-+	else
- 		err(udev, "inotify_init failed: %m\n");
- 	return inotify_fd;
- }
-Index: udev-162/udev/udevd.c
-===================================================================
---- udev-162.orig/udev/udevd.c
-+++ udev-162/udev/udevd.c
-@@ -225,6 +225,7 @@ static void worker_new(struct event *eve
- 	/* allow the main daemon netlink address to send devices to the worker */
- 	udev_monitor_allow_unicast_sender(worker_monitor, monitor);
- 	udev_monitor_enable_receiving(worker_monitor);
-+	util_set_fd_cloexec(udev_monitor_get_fd(worker_monitor));
- 
- 	worker = calloc(1, sizeof(struct worker));
- 	if (worker == NULL) {
-@@ -1227,13 +1228,14 @@ int main(int argc, char *argv[])
- 	}
- 
- 	/* unnamed socket from workers to the main daemon */
--	if (socketpair(AF_LOCAL, SOCK_DGRAM|SOCK_CLOEXEC, 0, worker_watch) < 0) {
-+	if (socketpair(AF_LOCAL, SOCK_DGRAM, 0, worker_watch) < 0) {
- 		fprintf(stderr, "error getting socketpair\n");
- 		err(udev, "error getting socketpair\n");
- 		rc = 6;
- 		goto exit;
- 	}
- 	pfd[FD_WORKER].fd = worker_watch[READ_END];
-+	util_set_fd_cloexec(worker_watch[WRITE_END]);
- 
- 	rules = udev_rules_new(udev, resolve_names);
- 	if (rules == NULL) {

patches/udev-162/udev-150-revert-bsg_h-removal.diff
@@ -1,130 +1,0 @@
-From: Michael Olbrich <m.olbrich@pengutronix.de>
-Subject: HACK: revert 24355313dc9686a14a618fcd9bbdedc29c57b651 (readd bsg.h)
-
-Readd bsg.h which is not part of the kernel headers in OSELAS.Toolchain-1.99.3
-
-Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>
-
----
- Makefile.am                  |    3 +
- extras/scsi_id/bsg.h         |   84 +++++++++++++++++++++++++++++++++++++++++++
- extras/scsi_id/scsi_serial.c |    3 +
- 3 files changed, 88 insertions(+), 2 deletions(-)
-
-Index: udev-162/Makefile.am
-===================================================================
---- udev-162.orig/Makefile.am
-+++ udev-162/Makefile.am
-@@ -287,7 +287,8 @@ extras_scsi_id_scsi_id_SOURCES =\
- 	extras/scsi_id/scsi_id.c \
- 	extras/scsi_id/scsi_serial.c \
- 	extras/scsi_id/scsi.h \
--	extras/scsi_id/scsi_id.h
-+	extras/scsi_id/scsi_id.h \
-+	extras/scsi_id/bsg.h
- extras_scsi_id_scsi_id_LDADD = libudev/libudev-private.la
- libexec_PROGRAMS += extras/scsi_id/scsi_id
- dist_man_MANS += extras/scsi_id/scsi_id.8
-Index: udev-162/extras/scsi_id/bsg.h
-===================================================================
---- /dev/null
-+++ udev-162/extras/scsi_id/bsg.h
-@@ -0,0 +1,84 @@
-+#ifndef BSG_H
-+#define BSG_H
-+
-+#define BSG_PROTOCOL_SCSI		0
-+
-+#define BSG_SUB_PROTOCOL_SCSI_CMD	0
-+#define BSG_SUB_PROTOCOL_SCSI_TMF	1
-+#define BSG_SUB_PROTOCOL_SCSI_TRANSPORT	2
-+
-+struct sg_io_v4 {
-+	__s32 guard;		/* [i] 'Q' to differentiate from v3 */
-+	__u32 protocol;		/* [i] 0 -> SCSI , .... */
-+	__u32 subprotocol;	/* [i] 0 -> SCSI command, 1 -> SCSI task
-+				   management function, .... */
-+
-+	__u32 request_len;	/* [i] in bytes */
-+	__u64 request;		/* [i], [*i] {SCSI: cdb} */
-+	__u64 request_tag;	/* [i] {SCSI: task tag (only if flagged)} */
-+	__u32 request_attr;	/* [i] {SCSI: task attribute} */
-+	__u32 request_priority;	/* [i] {SCSI: task priority} */
-+	__u32 request_extra;	/* [i] {spare, for padding} */
-+	__u32 max_response_len;	/* [i] in bytes */
-+	__u64 response;		/* [i], [*o] {SCSI: (auto)sense data} */
-+
-+        /* "dout_": data out (to device); "din_": data in (from device) */
-+	__u32 dout_iovec_count;	/* [i] 0 -> "flat" dout transfer else
-+				   dout_xfer points to array of iovec */
-+	__u32 dout_xfer_len;	/* [i] bytes to be transferred to device */
-+	__u32 din_iovec_count;	/* [i] 0 -> "flat" din transfer */
-+	__u32 din_xfer_len;	/* [i] bytes to be transferred from device */
-+	__u64 dout_xferp;	/* [i], [*i] */
-+	__u64 din_xferp;	/* [i], [*o] */
-+
-+	__u32 timeout;		/* [i] units: millisecond */
-+	__u32 flags;		/* [i] bit mask */
-+	__u64 usr_ptr;		/* [i->o] unused internally */
-+	__u32 spare_in;		/* [i] */
-+
-+	__u32 driver_status;	/* [o] 0 -> ok */
-+	__u32 transport_status;	/* [o] 0 -> ok */
-+	__u32 device_status;	/* [o] {SCSI: command completion status} */
-+	__u32 retry_delay;	/* [o] {SCSI: status auxiliary information} */
-+	__u32 info;		/* [o] additional information */
-+	__u32 duration;		/* [o] time to complete, in milliseconds */
-+	__u32 response_len;	/* [o] bytes of response actually written */
-+	__s32 din_resid;	/* [o] din_xfer_len - actual_din_xfer_len */
-+	__s32 dout_resid;	/* [o] dout_xfer_len - actual_dout_xfer_len */
-+	__u64 generated_tag;	/* [o] {SCSI: transport generated task tag} */
-+	__u32 spare_out;	/* [o] */
-+
-+	__u32 padding;
-+};
-+
-+#ifdef __KERNEL__
-+
-+#if defined(CONFIG_BLK_DEV_BSG)
-+struct bsg_class_device {
-+	struct device *class_dev;
-+	struct device *parent;
-+	int minor;
-+	struct request_queue *queue;
-+	struct kref ref;
-+	void (*release)(struct device *);
-+};
-+
-+extern int bsg_register_queue(struct request_queue *q,
-+			      struct device *parent, const char *name,
-+			      void (*release)(struct device *));
-+extern void bsg_unregister_queue(struct request_queue *);
-+#else
-+static inline int bsg_register_queue(struct request_queue *q,
-+				     struct device *parent, const char *name,
-+				     void (*release)(struct device *))
-+{
-+	return 0;
-+}
-+static inline void bsg_unregister_queue(struct request_queue *q)
-+{
-+}
-+#endif
-+
-+#endif /* __KERNEL__ */
-+
-+#endif
-Index: udev-162/extras/scsi_id/scsi_serial.c
-===================================================================
---- udev-162.orig/extras/scsi_id/scsi_serial.c
-+++ udev-162/extras/scsi_id/scsi_serial.c
-@@ -32,7 +32,8 @@
- #include <scsi/scsi.h>
- #include <scsi/sg.h>
- #include <linux/types.h>
--#include <linux/bsg.h>
-+/* #include <linux/bsg.h> */
-+#include "bsg.h"
- 
- #include "libudev.h"
- #include "libudev-private.h"

commit f13646f72473fc45056aed7150e3c984c702bc25

udev: add rootprefix and rootlibdir to conf args
Recent udev versions (at least 181 and newer) are searching for the firmware
under ROOTPREFIX, which defaults to /usr. Usually firmware was installed in
/lib/firmware. This patch sets the rootprefix, so that the firmware can be
found.

This problem was introduced in:
"e6ad184 udev: version bump 162/172 -> 172/181".

Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>

rules/udev.make
@@ -73,6 +73,8 @@ else
 UDEV_CONF_OPT += \
 	--disable-manpages \
 	--libexecdir=/lib \
+	--with-rootprefix= \
+	--with-rootlibdir=/lib \
 	--$(call ptx/endis,PTXCONF_UDEV_MTD_PROBE)-mtd_probe
 endif
 