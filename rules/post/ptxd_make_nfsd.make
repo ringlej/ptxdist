5088c1283a382526a63a881b869ac3321d78211e
Michael Olbrich <m.olbrich@pengutronix.de>
2012-09-12 09:59:44 +0200

    ptxdist: add command to export the nfsroot with a userspace nfsd
    Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

---
+++ rules/post/ptxd_make_nfsd.make
@@ -0,0 +1,15 @@
+# -*-makefile-*-
+#
+# Copyright (C) 2012 by Michael Olbrich <m.olbrich@pengutronix.de>
+#
+# See CREDITS for details about who has contributed to this project.
+#
+# For further information about the PTXdist project and license conditions
+# see the README file.
+#
+
+ptxd_make_nfsd: $(STATEDIR)/host-unfs3.install.post $(STATEDIR)/world.targetinstall
+	@$(call image/env) \
+	ptxd_make_nfsd
+
+# vim: syntax=make
---
+++ scripts/lib/ptxd_make_nfsd.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#
+# Copyright (C) 2012 by Michael Olbrich <m.olbrich@pengutronix.de>
+#
+# See CREDITS for details about who has contributed to this project.
+#
+# For further information about the PTXdist project and license conditions
+# see the README file.
+#
+
+ptxd_make_nfsd_exec() {
+    local port
+    local root="/$(basename "${ptx_nfsroot}")"
+    local base="$(dirname "${ptx_nfsroot}")"
+
+    if ! port="$(ptxd_get_kconfig "${PTXDIST_BOARDSETUP}" "PTXCONF_BOARDSETUP_NFSPORT")"; then
+	port=2049
+    fi
+
+    echo
+    echo "Mount rootfs with nfsroot=${root},v3,tcp,port=${port},mountport=${port}"
+    echo
+
+    echo "${root} (rw,no_root_squash)" > "${PTXDIST_TEMPDIR}/exports" &&
+    UNFS_BASE="${base}" unfsd -e "${PTXDIST_TEMPDIR}/exports" -n ${port} -m ${port} -p -d
+}
+export -f ptxd_make_nfsd_exec
+
+ptxd_make_nfsd() {
+    ptxd_make_image_init &&
+    ptxd_get_ipkg_files ${image_pkgs_selected_target} &&
+
+    cd "${ptx_nfsroot}" &&
+    {
+	ptxd_dopermissions "${ptxd_reply_perm_files[@]}"
+	echo ptxd_make_nfsd_exec
+    } | fakeroot --
+}
+export -f ptxd_make_nfsd
+
