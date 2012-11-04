9ef5f4142288cb9049eebc9fd56b9f77bfbca3e9
Michael Olbrich <m.olbrich@pengutronix.de>
2012-11-04 23:02:05 +0100

    wrapper: optimize
     - use /bin/sh instead of /bin/bash
     - don't use grep
    
    This is a lot faster if /bin/sh is dash
    
    Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

--- scripts/wrapper/libwrapper.sh
+++ scripts/wrapper/libwrapper.sh
@@ -1,17 +1,17 @@
-#!/bin/bash
+#!/bin/sh
 
-LINKING=1
-FORTIFY=1
-STDLIB=1
-FPIE=1
-PIE=1
-OPTIMIZE=0
-declare -a ARG_LIST
+LINKING=true
+FORTIFY=true
+STDLIB=true
+FPIE=true
+PIE=true
+OPTIMIZE=false
+ARG_LIST=""
 
 . ${PTXDIST_PLATFORMCONFIG}
 
 wrapper_exec() {
-	if [[ "${PTXDIST_VERBOSE}" = 1 && -n "${PTXDIST_FD_LOGFILE}" ]]; then
+	if [ "${PTXDIST_VERBOSE}" = 1 -a -n "${PTXDIST_FD_LOGFILE}" ]; then
 		echo "wrapper: ${PTXDIST_CCACHE} ${0##*/} ${ARG_LIST} $*" >&${PTXDIST_FD_LOGFILE}
 	fi
 	exec ${PTXDIST_CCACHE} $0.real ${ARG_LIST} "$@"
@@ -21,56 +21,58 @@ cc_check_args() {
 	for ARG in "$@"; do
 		case "${ARG}" in
 			-c)
-				LINKING=0
-				PIE=0
+				LINKING=false
+				PIE=false
 				;;
 			-D_FORTIFY_SOURCE | -D_FORTIFY_SOURCE=*)
-				FORTIFY=0
+				FORTIFY=false
 				;;
 			-nostdlib | -ffreestanding)
-				STDLIB=0
-				FPIE=0
-				PIE=0
+				STDLIB=false
+				FPIE=false
+				PIE=false
 				;;
 			-fno-PIC | -fno-pic | -fno-PIE | -fno-pie | \
 			-nopie | -static | -shared | \
 			-D__KERNEL__ | -nostartfiles )
-				FPIE=0
-				PIE=0
+				FPIE=false
+				PIE=false
 				;;
 			-fPIC | -fpic )
-				FPIE=0
+				FPIE=false
 				;;
 			-O0)
 				;;
 			-O*)
-				OPTIMIZE=1
+				OPTIMIZE=true
 				;;
 		esac
 	done
 	# Used e.g. by the kernel to get the compiler version. Adding
 	# linker options confuses gcc because there is nothing to link.
-	if [[ "$*" = "-v" ]]; then
-		LINKING=0
+	if [ "$*" = "-v" ]; then
+		LINKING=false
 	fi
 }
 
 add_arg() {
 	for arg in "${@}"; do
-		ARG_LIST[${#ARG_LIST[@]}]="${arg}"
+		ARG_LIST="${ARG_LIST} ${arg}"
 	done
 }
 
 add_opt_arg() {
-	local blacklist="${pkg_wrapper_blacklist}"
 	local opt="${1}"
 	shift
 
-	if grep -q "\<${opt}\>" <<< "${blacklist}"; then
-		return
-	fi
+	for item in ${pkg_wrapper_blacklist}; do
+		if [ "${item}" = "${opt}" ]; then
+			return
+		fi
+	done
 	opt="PTXCONF_${opt}"
-	if [[ -z "${!opt}" ]]; then
+	eval "opt=\$${opt}"
+	if [ -z "${opt}" ]; then
 		return
 	fi
 	add_arg "${@}"
@@ -87,13 +89,13 @@ add_ld_args() {
 ld_add_ld_args() {
 	# ld warns about ignored keywords (-z <keyword>) when only asked for the
 	# version
-	if [[ "$*" != "-v" ]]; then
+	if [ "$*" != "-v" ]; then
 		add_ld_args
 	fi
 }
 
 cc_add_ld_args() {
-	if [[ "${LINKING}" = 1 ]]; then
+	if ${LINKING}; then
 		add_ld_args "-Wl,"
 		add_arg ${PTXDIST_CROSS_LDFLAGS}
 		add_opt_arg TARGET_EXTRA_LDFLAGS ${PTXCONF_TARGET_EXTRA_LDFLAGS}
@@ -104,22 +106,22 @@ cc_add_fortify() {
 	# fortify only works when optimizing is enabled. The warning
 	# generated when combining -D_FORTIFY_SOURCE with -O0 can confuse
 	# configure checks
-	if [[ "${FORTIFY}" = 1 && "${OPTIMIZE}" = 1 ]]; then
+	if ${FORTIFY} && ${OPTIMIZE}; then
 		add_opt_arg TARGET_HARDEN_FORTIFY "-D_FORTIFY_SOURCE=2"
 	fi
 }
 
 cc_add_stack() {
-	if [[ "${STDLIB}" = 1 ]]; then
+	if ${STDLIB}; then
 		add_opt_arg TARGET_HARDEN_STACK "-fstack-protector" "--param=ssp-buffer-size=4"
 	fi
 }
 
 cc_add_pie() {
-	if [[ "${FPIE}" = 1 ]]; then
+	if ${FPIE}; then
 		add_opt_arg TARGET_HARDEN_PIE "-fPIE"
 	fi
-	if [[ "${PIE}" = 1 ]]; then
+	if ${PIE}; then
 		add_opt_arg TARGET_HARDEN_PIE "-pie"
 	fi
 }
