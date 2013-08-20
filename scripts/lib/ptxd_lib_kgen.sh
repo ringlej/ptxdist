ringlej/ptxdist

commit 51c5786c16a9a3508af9b6cc69947a2f3b8bc7cf

platforms: generate menu for hosttools on the fly
Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>

scripts/lib/ptxd_lib_kgen.sh
@@ -90,8 +90,7 @@ EOF
     # transmogrify part into subdir
     local oldIFS="$IFS"
     case "${kgen_part}" in
-	ptx)	  IFS=: kgen_dirs=( ${PTXDIST_PATH_RULES} ) ;;
-	platform) IFS=: kgen_dirs=( ${PTXDIST_PATH_PLATFORMS} ) ;;
+	ptx|platform) IFS=: kgen_dirs=( ${PTXDIST_PATH_RULES}:${PTXDIST_PATH_PLATFORMS} ) ;;
 	board|user|collection) return 0 ;;
 	*) cat <<EOF
 

platforms/Kconfig
@@ -57,4 +57,4 @@ menu "new image creation options    "
 source "generated/image2.in"
 endmenu
 
-source "platforms/hosttools.in"
+source "generated/hosttools_platform.in"

platforms/hosttools.in
@@ -1,29 +1,0 @@
-source "rules/cross-nasm.in"
-source "rules/host-bzip2.in"
-source "rules/host-cdrkit.in"
-source "rules/host-cmake.in"
-source "rules/host-cramfs.in"
-source "rules/host-dosfstools.in"
-source "rules/host-e2fsprogs.in"
-source "rules/host-genext2fs.in"
-source "rules/host-genimage.in"
-source "rules/host-genpart.in"
-source "rules/host-gettext.in"
-source "rules/host-libblkid.in"
-source "rules/host-libcap.in"
-source "rules/host-libconfuse.in"
-source "rules/host-libiconv.in"
-source "rules/host-libkmod.in"
-source "rules/host-liblzo.in"
-source "rules/host-libuuid.in"
-source "rules/host-lzop.in"
-source "rules/host-mkelfImage.in"
-source "rules/host-mtd-utils.in"
-source "rules/host-mtools.in"
-source "rules/host-openssl.in"
-source "rules/host-squashfs-tools.in"
-source "rules/host-u-boot-tools.in"
-source "rules/host-util-linux-ng.in"
-source "rules/host-xl-tools.in"
-source "rules/host-xz.in"
-source "rules/host-zlib.in"

rules/cross-nasm.in
@@ -1,4 +1,5 @@
 ## SECTION=crosstools
+## SECTION=hosttools_platform
 
 config CROSS_NASM
 	bool

rules/host-bzip2.in
@@ -1,4 +1,5 @@
 ## SECTION=hosttools_noprompt
+## SECTION=hosttools_platform
 
 config HOST_BZIP2
 	tristate

rules/host-cdrkit.in
@@ -1,4 +1,5 @@
 ## SECTION=hosttools_noprompt
+## SECTION=hosttools_platform
 
 config HOST_CDRKIT
 	tristate

rules/host-cmake.in
@@ -1,4 +1,5 @@
 ## SECTION=hosttools_noprompt
+## SECTION=hosttools_platform
 
 config HOST_CMAKE
 	tristate

rules/host-cramfs.in
@@ -1,4 +1,5 @@
 ## SECTION=hosttools_noprompt
+## SECTION=hosttools_platform
 
 config HOST_CRAMFS
 	tristate

commit 3652c3736993d32448b82475aabeca10ff8fbc6c

Fix problem with finding platforms/*.in
ptxdist 2013.08 doesn't find my platforms/u-boot.in file. This is
because it looks for ":/path/to/bsp/platforms" instead of the variant
without colon.

Following shell script demonstrates that:

    #!/bin/bash

    oldifs="$IFS"
    IFS=: a="1:2:3:" b="4:5:6:"
    c=(${a}:${b})
    d=(${a} ${b})
    IFS="$oldifs"
    echo ${c[@]} # 1 2 3 :4 5 6
    echo ${d[@]} # 1 2 3 4 5 6

So only the variant without a ':' is correct.

The problem was introduced with 51c5786c16a9a3508af9b6cc69947a2f3b8bc7cf
on 20th June 2013. So I think it was also in 2013.07 although I didn't
test.(

Signed-off-by: Bernhard Walle <bernhard@bwalle.de>
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

scripts/lib/ptxd_lib_kgen.sh
@@ -90,7 +90,7 @@ EOF
     # transmogrify part into subdir
     local oldIFS="$IFS"
     case "${kgen_part}" in
-	ptx|platform) IFS=: kgen_dirs=( ${PTXDIST_PATH_RULES}:${PTXDIST_PATH_PLATFORMS} ) ;;
+	ptx|platform) IFS=: kgen_dirs=( ${PTXDIST_PATH_RULES} ${PTXDIST_PATH_PLATFORMS} ) ;;
 	board|user|collection) return 0 ;;
 	*) cat <<EOF
 