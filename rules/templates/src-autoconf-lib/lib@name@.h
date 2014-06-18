4fe2c90c7b42f9389bb3c472202137c2f1056791
Juergen Borleis <jbe@pengutronix.de>
2014-06-18 17:22:10 +0200

    lib autotools template: improvements of the library template
    Show the new user what kind of files are important in an autotoolized package
    and how to use them.
    
    Signed-off-by: Juergen Borleis <jbe@pengutronix.de>

---
+++ rules/templates/src-autoconf-lib/lib@name@.h
@@ -0,0 +1,7 @@
+/*
+ * (C) Copyright <year> <your name> <your mail address>
+ *
+ * <define the license of the file or refer to the main COPYING file>
+ */
+
+extern int @name@_library_globally_visible_function(void);
