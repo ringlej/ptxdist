a1e929d4cdd7e1ff42cd3593cc7b6da9c1b63cc3
Michael Olbrich <m.olbrich@pengutronix.de>
2014-06-18 20:38:49 +0200

    ptxdist: add support for icecc
    Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

--- scripts/wrapper/libwrapper.sh
+++ scripts/wrapper/libwrapper.sh
@@ -23,9 +23,9 @@ fi
 wrapper_exec() {
 	PATH="$(echo "${PATH}" | sed "s;${PTXDIST_PATH_SYSROOT_HOST}/lib/wrapper:;;")"
 	if [ "${PTXDIST_VERBOSE}" = 1 -a -n "${PTXDIST_FD_LOGFILE}" ]; then
-		echo "wrapper: ${PTXDIST_CCACHE} ${0##*/} ${ARG_LIST} $* ${LATE_ARG_LIST}" >&${PTXDIST_FD_LOGFILE}
+		echo "wrapper: ${PTXDIST_ICECC}${PTXDIST_CCACHE} ${0##*/} ${ARG_LIST} $* ${LATE_ARG_LIST}" >&${PTXDIST_FD_LOGFILE}
 	fi
-	exec ${PTXDIST_CCACHE} "${0%/*}/real/${0##*/}" ${ARG_LIST} "$@" ${LATE_ARG_LIST}
+	exec ${PTXDIST_ICECC}${PTXDIST_CCACHE} "${0%/*}/real/${0##*/}" ${ARG_LIST} "$@" ${LATE_ARG_LIST}
 }
 
 cc_check_args() {
