508ca3cda8e8204f0955fe0ebf007f66753d03a8
Michael Olbrich <m.olbrich@pengutronix.de>
2015-11-07 09:39:21 +0100

    ptxd_make_world_install_post: copy files with '--remove-destination'
    Otherwise overwriting broken symlinks with files will fail.
    This happens occasionally in sysroot target, e.g. with /bin/pidof from
    busybox and initng.
    
    Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

--- scripts/lib/ptxd_make_world_install.sh
+++ scripts/lib/ptxd_make_world_install.sh
@@ -218,7 +218,7 @@ ptxd_make_world_install_post() {
     # create directories first to avoid race contitions with -jeX
     find "${pkg_pkg_dir}" -type d -printf "%P\0" | \
 	xargs -0 -I{} mkdir -p "${pkg_sysroot_dir}/{}" &&
-    cp -dprf -- "${pkg_pkg_dir}"/* "${pkg_sysroot_dir}" &&
+    cp -dpr --remove-destination -- "${pkg_pkg_dir}"/* "${pkg_sysroot_dir}" &&
 
     # host and cross packages
     if [ "${pkg_type}" != "target" ]; then
