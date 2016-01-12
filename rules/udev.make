ringlej/ptxdist

commit 7732f7368455ea6c9cc2e1a244bfbfc671f56417

udev-legacy: provide regular overwrite for project specific rules
Use the regular search hierarchy for this kind of files as well.

Signed-off-by: Juergen Borleis <jbe@pengutronix.de>

rules/udev.make
@@ -214,15 +214,8 @@ ifdef PTXCONF_UDEV_KEYMAPS
 endif
 
 ifdef PTXCONF_UDEV_CUST_RULES
-	@if [ -d $(PTXDIST_WORKSPACE)/projectroot/lib/udev/rules.d/ ]; then \
-		$(call install_tree, udev, 0, 0, \
-			$(PTXDIST_WORKSPACE)/projectroot/lib/udev/rules.d, \
-			/lib/udev/rules.d); \
-	else \
-		echo "UDEV_CUST_RULES is enabled but Directory containing" \
-			"customized udev rules is missing!"; \
-		exit 1; \
-	fi
+	$(call install_tree_alternative, udev, 0, 0, /lib/udev/rules.d, \
+		/lib/udev/rules.d)
 endif
 
 	@$(foreach helper, $(UDEV_HELPER-y), \

commit 74285062b74725ce0b1cb274516f103d80921246

udev: fix creation of custom rules
This was broken since commit 7732f73 and custom rule files were not
copied.

Signed-off-by: Clemens Gruber <clemens.gruber@pqgruber.com>
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/udev.make
@@ -214,8 +214,7 @@ ifdef PTXCONF_UDEV_KEYMAPS
 endif
 
 ifdef PTXCONF_UDEV_CUST_RULES
-	$(call install_tree_alternative, udev, 0, 0, /lib/udev/rules.d, \
-		/lib/udev/rules.d)
+	$(call install_alternative_tree, udev, 0, 0, /lib/udev/rules.d)
 endif
 
 	@$(foreach helper, $(UDEV_HELPER-y), \