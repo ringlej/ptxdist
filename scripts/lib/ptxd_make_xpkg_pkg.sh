ringlej/ptxdist

commit 0d61da7cd13fa7a3c7d58f904bb9e69727d9d20c

use ^_ as separator in perms files
Change separator to allow ':' in filenames.

Signed-off-by: Ladislav Michl <ladis@linux-mips.org>
[mol: fix old perm file detection]
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

scripts/lib/ptxd_make_xpkg_pkg.sh
@@ -196,6 +196,7 @@ ptxd_install_setup_src() {
 export -f ptxd_install_setup_src
 
 ptxd_install_dir() {
+    local sep="\x1F"
     local dir="$1"
     local usr="$2"
     local grp="$3"
@@ -215,7 +216,7 @@ install directory:
     install -m "${mod_nfs}" -d "${ndirs[@]/%/${dir}}" &&
     install -m "${mod}" -o "${usr}" -g "${grp}" -d "${pdirs[@]/%/${dir}}" &&
 
-    echo "f:${dir}:${usr}:${grp}:${mod}" >> "${pkg_xpkg_perms}" ||
+    echo -e "f${sep}${dir}${sep}${usr}${sep}${grp}${sep}${mod}" >> "${pkg_xpkg_perms}" ||
     ptxd_install_error "install_dir failed!"
 }
 export -f ptxd_install_dir
@@ -290,6 +291,7 @@ export -f ptxd_install_file_strip
 
 
 ptxd_install_file_impl() {
+    local sep="\x1F"
     local src="$1"
     local dst="$2"
     local usr="$3"
@@ -355,7 +357,7 @@ Usually, just remove the 6th parameter and everything works fine.
     # now change to requested user and group
     chown "${usr}:${grp}" "${pdirs[@]/%/${dst}}" &&
 
-    echo "f:${dst}:${usr}:${grp}:${mod}" >> "${pkg_xpkg_perms}"
+    echo -e "f${sep}${dst}${sep}${usr}${sep}${grp}${sep}${mod}" >> "${pkg_xpkg_perms}"
 }
 export -f ptxd_install_file_impl
 
@@ -397,6 +399,7 @@ install link:
 export -f ptxd_install_ln
 
 ptxd_install_mknod() {
+    local sep="\x1F"
     local dst="$1"
     local usr="$2"
     local grp="$3"
@@ -426,7 +429,7 @@ install device node:
     done &&
     chown "${usr}:${grp}" "${pdirs[@]/%/${dst}}" &&
 
-    echo "n:${dst}:${usr}:${grp}:${mod}:${type}:${major}:${minor}" >> "${pkg_xpkg_perms}"
+    echo -e "n${sep}${dst}${sep}${usr}${sep}${grp}${sep}${mod}${sep}${type}${sep}${major}${sep}${minor}" >> "${pkg_xpkg_perms}"
 }
 export -f ptxd_install_mknod
 

rules/post/image_cpio.make
@@ -15,7 +15,7 @@ $(IMAGEDIR)/root.cpio: $(STATEDIR)/image_working_dir
 	@echo -n "Creating '$(notdir $(@))' from working dir..."
 	@cd $(image/work_dir) && \
 	( \
-		awk -F: $(DOPERMISSIONS) $(image/permissions) && \
+		awk $(DOPERMISSIONS) $(image/permissions) && \
 		echo "find . | cpio --quiet -H newc -o > '$(@)'" \
 	) | $(FAKEROOT) --
 	@echo "done."

rules/post/image_ext2.make
@@ -15,7 +15,7 @@ ifdef PTXCONF_IMAGE_EXT2
 $(IMAGEDIR)/root.ext2: $(STATEDIR)/image_working_dir
 	@echo -n "Creating root.ext2 from working dir..."
 	@cd $(image/work_dir);						\
-	(awk -F: $(DOPERMISSIONS) $(image/permissions) &&		\
+	(awk $(DOPERMISSIONS) $(image/permissions) &&			\
 	(								\
 		echo -n "$(PTXCONF_SYSROOT_HOST)/bin/genext2fs ";	\
 		echo -n "-b $(PTXCONF_IMAGE_EXT2_SIZE) ";		\

rules/post/image_jffs2.make
@@ -17,7 +17,7 @@ $(IMAGEDIR)/root.jffs2: $(STATEDIR)/image_working_dir $(STATEDIR)/host-mtd-utils
 	@echo -n "(--eraseblock=$(PTXCONF_IMAGE_JFFS2_BLOCKSIZE) "
 	@echo "$(call remove_quotes,$(PTXCONF_IMAGE_JFFS2_EXTRA_ARGS)))"
 	@cd $(image/work_dir);						\
-	(awk -F: $(DOPERMISSIONS) $(image/permissions) &&		\
+	(awk $(DOPERMISSIONS) $(image/permissions) &&			\
 	(								\
 		echo -n "$(PTXCONF_SYSROOT_HOST)/sbin/mkfs.jffs2 ";	\
 		echo -n "-d $(image/work_dir) ";			\

rules/post/image_squashfs.make
@@ -19,7 +19,7 @@ ifdef PTXCONF_IMAGE_SQUASHFS
 $(IMAGEDIR)/root.squashfs: $(STATEDIR)/image_working_dir $(STATEDIR)/host-squashfs-tools.install.post
 	@echo -n "Creating root.squashfs from working dir..."
 	@cd $(image/work_dir);							\
-	(awk -F: $(DOPERMISSIONS) $(image/permissions) &&		\
+	(awk $(DOPERMISSIONS) $(image/permissions) &&			\
 	(								\
 		echo -n "$(PTXCONF_SYSROOT_HOST)/sbin/mksquashfs ";	\
 		echo -n "$(image/work_dir) ";					\

rules/post/image_tgz.make
@@ -18,10 +18,10 @@ endif
 
 $(IMAGEDIR)/root.tgz: $(STATEDIR)/image_working_dir
 	@echo -n 'Creating root.tgz from working dir$(if $(IMAGE_TGZ_LABEL), with label "$(IMAGE_TGZ_LABEL)",)... '
-	@cd $(image/work_dir);							\
-	(awk -F: $(DOPERMISSIONS) $(image/permissions) &&		\
+	@cd $(image/work_dir);					\
+	(awk $(DOPERMISSIONS) $(image/permissions) &&		\
 	(	echo -n "tar ${IMAGE_TGZ_LABEL_ARGS} -zcf ";	\
-		echo -n "$@ ." )					\
+		echo -n "$@ ." )				\
 	) | $(FAKEROOT) --
 	@echo "done."
 endif

rules/post/image_ubi.make
@@ -22,7 +22,7 @@ $(IMAGEDIR)/root.ubifs: $(STATEDIR)/image_working_dir $(STATEDIR)/host-mtd-utils
 	@echo -n "-e $(PTXCONF_IMAGE_UBIFS_LEB_SIZE) -c $(PTXCONF_IMAGE_UBIFS_ROOT_MAX_LEB_COUNT) "
 	@echo -n "$(PTXCONF_IMAGE_UBIFS_EXTRA_ARGS))"
 	@cd $(image/work_dir);								\
-	(awk -F: $(DOPERMISSIONS) $(image/permissions) &&			\
+	(awk $(DOPERMISSIONS) $(image/permissions) &&				\
 	(									\
 		echo -n "$(PTXCONF_SYSROOT_HOST)/sbin/mkfs.ubifs ";		\
 		echo -n "-d $(image/work_dir) ";					\

rules/post/ptxd_make_image_common.make
@@ -8,7 +8,7 @@
 # see the README file.
 #
 
-DOPERMISSIONS := '{	\
+DOPERMISSIONS := '{ FS = "\x1F";	\
 	if ($$1 == "f")	\
 		printf("chmod %s \".%s\"; chown %s.%s \".%s\";\n", $$5, $$2, $$3, $$4, $$2);	\
 	if ($$1 == "n")	\

commit 65fcd73226574499de67ab56f5fab73ecc90e097

ptxd_make_xpkg_pkg: fix ascii representations in filenames
ASCII representations in filenames, e.g. \x2d ("-"), were
interpreted by 'echo -e'. This fix uses 'echo -e' only for
the seperator.

Fixes: 0d61da7cd13f ("use ^_ as separator in perms files")
Signed-off-by: Bastian Stender <bst@pengutronix.de>
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

scripts/lib/ptxd_make_xpkg_pkg.sh
@@ -196,7 +196,7 @@ ptxd_install_setup_src() {
 export -f ptxd_install_setup_src
 
 ptxd_install_dir() {
-    local sep="\x1F"
+    local sep="$(echo -e "\x1F")"
     local dir="$1"
     local usr="$2"
     local grp="$3"
@@ -216,7 +216,7 @@ install directory:
     install -m "${mod_nfs}" -d "${ndirs[@]/%/${dir}}" &&
     install -m "${mod}" -o "${usr}" -g "${grp}" -d "${pdirs[@]/%/${dir}}" &&
 
-    echo -e "f${sep}${dir}${sep}${usr}${sep}${grp}${sep}${mod}" >> "${pkg_xpkg_perms}" ||
+    echo "f${sep}${dir}${sep}${usr}${sep}${grp}${sep}${mod}" >> "${pkg_xpkg_perms}" ||
     ptxd_install_error "install_dir failed!"
 }
 export -f ptxd_install_dir
@@ -291,7 +291,7 @@ export -f ptxd_install_file_strip
 
 
 ptxd_install_file_impl() {
-    local sep="\x1F"
+    local sep="$(echo -e "\x1F")"
     local src="$1"
     local dst="$2"
     local usr="$3"
@@ -363,7 +363,7 @@ Usually, just remove the 6th parameter and everything works fine.
     # now change to requested user and group
     chown "${usr}:${grp}" "${pdirs[@]/%/${dst}}" &&
 
-    echo -e "f${sep}${dst}${sep}${usr}${sep}${grp}${sep}${mod}" >> "${pkg_xpkg_perms}"
+    echo "f${sep}${dst}${sep}${usr}${sep}${grp}${sep}${mod}" >> "${pkg_xpkg_perms}"
 }
 export -f ptxd_install_file_impl
 
@@ -405,7 +405,7 @@ install link:
 export -f ptxd_install_ln
 
 ptxd_install_mknod() {
-    local sep="\x1F"
+    local sep="$(echo -e "\x1F")"
     local dst="$1"
     local usr="$2"
     local grp="$3"
@@ -435,7 +435,7 @@ install device node:
     done &&
     chown "${usr}:${grp}" "${pdirs[@]/%/${dst}}" &&
 
-    echo -e "n${sep}${dst}${sep}${usr}${sep}${grp}${sep}${mod}${sep}${type}${sep}${major}${sep}${minor}" >> "${pkg_xpkg_perms}"
+    echo "n${sep}${dst}${sep}${usr}${sep}${grp}${sep}${mod}${sep}${type}${sep}${major}${sep}${minor}" >> "${pkg_xpkg_perms}"
 }
 export -f ptxd_install_mknod
 