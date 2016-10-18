ringlej/ptxdist

commit 333123a94302729884eb1ea36973da6db671a12c

iproute2: update version and minor improvements
Update to iproute2 4.7.0, which - thanks to Alexander Aring - includes a
patch to fix a xtables-related build failure.

In addition we have to set TC_CONFIG_XT to y. Otherwise, the
hand-crafted Configure script enables legacy iptables code, which in
turn leads to multiple build failures.
Enabling the iptables package in ptxdist is still not required.

Also pass the kernel headers include directory to make.

Replace the description of tc with a few modified lines from the
manpage.

Signed-off-by: Clemens Gruber <clemens.gruber@pqgruber.com>
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/iproute2.make
@@ -17,8 +17,8 @@ PACKAGES-$(PTXCONF_IPROUTE2) += iproute2
 #
 # Paths and names
 #
-IPROUTE2_VERSION	:= 4.4.0
-IPROUTE2_MD5		:= d762653ec3e1ab0d4a9689e169ca184f
+IPROUTE2_VERSION	:= 4.7.0
+IPROUTE2_MD5		:= d4b205830cdc2702f8a0cbd6232129cd
 IPROUTE2		:= iproute2-$(IPROUTE2_VERSION)
 IPROUTE2_SUFFIX		:= tar.xz
 IPROUTE2_URL		:= $(call ptx/mirror, KERNEL, utils/net/iproute2/$(IPROUTE2).$(IPROUTE2_SUFFIX))
@@ -38,7 +38,7 @@ $(STATEDIR)/iproute2.prepare:
 	@$(call world/prepare, IPROUTE2)
 # overwrite options we don't want, or may be misdetected
 	@echo 'TC_CONFIG_ATM:=n'	>> $(IPROUTE2_DIR)/Config
-	@echo 'TC_CONFIG_XT:=n'		>> $(IPROUTE2_DIR)/Config
+	@echo 'TC_CONFIG_XT:=y'		>> $(IPROUTE2_DIR)/Config
 	@echo 'IPT_LIB_DIR:=/usr/lib'	>> $(IPROUTE2_DIR)/Config
 	@echo 'TC_CONFIG_ELF:=n'	>> $(IPROUTE2_DIR)/Config
 ifndef PTXCONF_GLOBAL_SELINUX
@@ -53,7 +53,8 @@ endif
 IPROUTE2_MAKE_OPT := \
 	DESTDIR=$(SYSROOT) \
 	LDFLAGS='-rdynamic' \
-	WFLAGS="-Wall"
+	WFLAGS="-Wall" \
+	KERNEL_INCLUDE="$(KERNEL_HEADERS_INCLUDE_DIR)"
 
 # ----------------------------------------------------------------------------
 # Target-Install
@@ -93,18 +94,20 @@ $(STATEDIR)/iproute2.targetinstall:
 	done
 
 ifdef PTXCONF_IPROUTE2_TC
+	@$(call install_copy, iproute2, 0, 0, 0644, -, /usr/lib/tc/normal.dist)
 	@$(call install_copy, iproute2, 0, 0, 0644, -, /usr/lib/tc/pareto.dist)
 	@$(call install_copy, iproute2, 0, 0, 0644, -, /usr/lib/tc/paretonormal.dist)
 	@$(call install_copy, iproute2, 0, 0, 0644, -, /usr/lib/tc/experimental.dist)
 endif
 
 	@$(call install_alternative, iproute2, 0, 0, 0644, /etc/iproute2/ematch_map)
+	@$(call install_alternative, iproute2, 0, 0, 0644, /etc/iproute2/group)
+	@$(call install_alternative, iproute2, 0, 0, 0644, /etc/iproute2/nl_protos)
 	@$(call install_alternative, iproute2, 0, 0, 0644, /etc/iproute2/rt_dsfield)
 	@$(call install_alternative, iproute2, 0, 0, 0644, /etc/iproute2/rt_protos)
 	@$(call install_alternative, iproute2, 0, 0, 0644, /etc/iproute2/rt_realms)
 	@$(call install_alternative, iproute2, 0, 0, 0644, /etc/iproute2/rt_scopes)
 	@$(call install_alternative, iproute2, 0, 0, 0644, /etc/iproute2/rt_tables)
-	@$(call install_alternative, iproute2, 0, 0, 0644, /etc/iproute2/group)
 
 	@$(call install_finish, iproute2)
 

rules/iproute2.in
@@ -135,7 +135,8 @@ config IPROUTE2_TC
 	bool
 	prompt "tc"
 	help
-	  In Quality Of Service (QOS) and Class Of Service (COS)
-	  scenarios used for traffic controlling
+	  tc is used to configure Traffic Control in the Linux Kernel, which
+	  consists of Shaping, Scheduling, Policing and Dropping.
+	  Processing of traffic is controlled by qdiscs, classes and filters.
 
 endif

commit 761426aa7c6978ec6da0729ef2f4da7d730bd69b

iproute2: add patch to avoid iptables dependency
New patch, adding the missing build-dependency xtables-version.h.
Also set TC_CONFIG_XT=n in Config.

libxtables is not required, unless plugins that link to it are used.
Currently, none of these plugins are installed/installable in PTXdist.

Fixes: 333123a94302 ("iproute2: update version and minor improvements")

Signed-off-by: Clemens Gruber <clemens.gruber@pqgruber.com>
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

patches/iproute2-4.8.0/0001-add-xtables-version.h.patch
@@ -1,0 +1,22 @@
+From 4d90bfff717bd7be913832c7eaefd5b5f7408012 Mon Sep 17 00:00:00 2001
+From: Clemens Gruber <clemens.gruber@pqgruber.com>
+Date: Mon, 17 Oct 2016 17:06:20 +0200
+Subject: [PATCH] add xtables-version.h
+
+Signed-off-by: Clemens Gruber <clemens.gruber@pqgruber.com>
+---
+ include/xtables-version.h | 2 ++
+ 1 file changed, 2 insertions(+)
+ create mode 100644 include/xtables-version.h
+
+diff --git a/include/xtables-version.h b/include/xtables-version.h
+new file mode 100644
+index 000000000000..905e5fe070e7
+--- /dev/null
++++ b/include/xtables-version.h
+@@ -0,0 +1,2 @@
++#define XTABLES_VERSION "libxtables.so.11"
++#define XTABLES_VERSION_CODE 11
+-- 
+2.10.0
+

patches/iproute2-4.8.0/series
@@ -1,0 +1,1 @@
+0001-add-xtables-version.h.patch

rules/iproute2.make
@@ -38,7 +38,7 @@ $(STATEDIR)/iproute2.prepare:
 	@$(call world/prepare, IPROUTE2)
 # overwrite options we don't want, or may be misdetected
 	@echo 'TC_CONFIG_ATM:=n'	>> $(IPROUTE2_DIR)/Config
-	@echo 'TC_CONFIG_XT:=y'		>> $(IPROUTE2_DIR)/Config
+	@echo 'TC_CONFIG_XT:=n'		>> $(IPROUTE2_DIR)/Config
 	@echo 'IPT_LIB_DIR:=/usr/lib'	>> $(IPROUTE2_DIR)/Config
 	@echo 'TC_CONFIG_ELF:=n'	>> $(IPROUTE2_DIR)/Config
 ifndef PTXCONF_GLOBAL_SELINUX