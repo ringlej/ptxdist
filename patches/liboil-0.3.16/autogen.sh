508a7f443e83273457e1feee06ac294dc7ede05c
Michael Olbrich <m.olbrich@pengutronix.de>
2017-02-09 10:57:17 +0100

    liboil: make sure libtoolize works correctly
    The configure.ac / Makefile.am are broken, so without am_macro_dir=m4 some
    files are not copied and building can fail with:
    
    libtool: Version mismatch error.  This is libtool 2.4.6, but the
    libtool: definition of this LT_INIT comes from libtool 2.2.4.
    libtool: You should recreate aclocal.m4 with macros from libtool 2.4.6
    libtool: and run autoconf again.
    
    Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

--- patches/liboil-0.3.16/autogen.sh
+++ patches/liboil-0.3.16/autogen.sh
@@ -1,1 +1,19 @@
-../autogen.sh
+#!/bin/bash
+
+set -e
+
+aclocal $ACLOCAL_FLAGS
+
+am_macro_dir=m4 \
+libtoolize \
+	--force \
+	--copy
+
+autoreconf \
+	--force \
+	--install \
+	--warnings=cross \
+	--warnings=syntax \
+	--warnings=obsolete \
+	--warnings=unsupported
+
