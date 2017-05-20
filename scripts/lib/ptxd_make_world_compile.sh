ringlej/ptxdist

commit 30b9267e35eea1c2edb4da0231a428bfa25b6766

redirect command stderr to stdout
Redirect stderr to stdout for the actual autogen.sh, prepare, compile,
install and opkg-build command.
This preserves the chronological order of stdout vs. stderr when building
packages in parallel with output sync enabled.

As a side-effect, the stderr output of these commands won't show up on the
console for quiet builds. But most of that is not really useful by itself
anyways. And the resulting logfile is more readable now for the rest.

Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

scripts/lib/ptxd_make_world_compile.sh
@@ -28,7 +28,7 @@ ptxd_make_world_compile() {
 	    "${ptx_build_python}" \
 	    setup.py \
 	    "${pkg_make_opt}"
-	)
+	) 2>&1
 	;;
 	*)
 	ptxd_eval \
@@ -37,7 +37,7 @@ ptxd_make_world_compile() {
 	    "${pkg_make_env}" \
 	    "${MAKE}" -C "${pkg_build_dir}" \
 	    "${pkg_make_opt}" \
-	    "${pkg_make_par}"
+	    "${pkg_make_par}" 2>&1
 	;;
     esac
 }

scripts/lib/ptxd_make_opkg_finish.sh
@@ -16,7 +16,7 @@
 ptxd_make_opkg_finish_impl() {
     chown -R 0:0 "${pkg_xpkg_tmp}" &&
     ptxd_make_xpkg_pkg "${pkg_opkg_tmp}" "${pkg_xpkg_cmds}" "${pkg_xpkg_perms}" &&
-    opkg-build ${ptx_xpkg_extra_args} "${pkg_opkg_tmp}" "${ptx_pkg_dir}"
+    opkg-build ${ptx_xpkg_extra_args} "${pkg_opkg_tmp}" "${ptx_pkg_dir}" 2>&1
 }
 export -f ptxd_make_opkg_finish_impl
 

scripts/lib/ptxd_make_world_install.sh
@@ -102,7 +102,7 @@ ptxd_make_world_install() {
 
     "${echo:-echo}" \
 	"${cmd[@]}" \
-	| "${fakeroot:-fakeroot}" "${fakeargs[@]}" --
+	| "${fakeroot:-fakeroot}" "${fakeargs[@]}" -- 2>&1
     check_pipe_status
 }
 export -f ptxd_make_world_install

scripts/lib/ptxd_make_world_patchin.sh
@@ -411,7 +411,7 @@ ptxd_make_world_autogen() {
 
     # run autogen.sh if available
     if [ -n "${pkg_patch_autogen}" ]; then
-	"${pkg_patch_autogen}" || return
+	"${pkg_patch_autogen}" 2>&1 || return
 	echo -e "patchin: autogen: done\n"
     fi
 }

scripts/lib/ptxd_make_world_prepare.sh
@@ -140,7 +140,7 @@ ptxd_make_world_prepare() {
     case "${pkg_conf_tool}" in
 	autoconf|cmake|qmake|kconfig|perl)
 	    cd -- "${pkg_build_dir}" &&
-	    ptxd_make_world_prepare_"${pkg_conf_tool}" ;;
+	    ptxd_make_world_prepare_"${pkg_conf_tool}" 2>&1 ;;
 	python|python3)
 	    : ;; # nothing to do
 	"NO") echo "prepare stage disabled." ;;

commit 801f5c05b778af05bcd9feed585e3925f592252b

ptxd_make_world_compile.sh: conditionally redirect stderr>stdout
I discovered after updating to a new version of ptxdist that all of my
compile output including errors/warnings are now all going to stdout
instead of having the errors/warning going to stderr.

This is causing some problem when I use qtcreator as my IDE because
qtcreator will only filter stderr for errors/warnings to put into the
Issues tab, where one can simply click on the error/warning and be taken
right to the source line where the error/warning is found.

I found that this problem was introduced by commit
30b9267e35eea1c2edb4da0231a428bfa25b6766

This patch will conditionally redirect stderr>stdout on compile output
if ptxdist has output synchronization enabled.

This small program demonstrates the behavior with this patch applied:

$ cat local_src/testprog/testprog.c
#include <stdio.h>
#include <stdlib.h>

#error An error here
int main(int argc, char *argv[])
{
        printf("Hello World, I'm testprog!\n");

        exit(EXIT_SUCCESS);
}

$ ptxdist compile testprog >/dev/null
testprog.c:4:2: error: #error An error here
 #error An error here
  ^
make[1]: *** [testprog] Error 1
make: *** [/home/jringle-admin/code/gpec/ec1k-rootfs/platform-EC1K/state/testprog.compile] Error 2

$ ptxdist compile --output-sync testprog >/dev/null
make: *** [/home/jringle-admin/code/gpec/ec1k-rootfs/platform-EC1K/state/testprog.compile] Error 2

$ ptxdist compile --no-output-sync testprog >/dev/null
testprog.c:4:2: error: #error An error here
 #error An error here
  ^
make[1]: *** [testprog] Error 1
make: *** [/home/jringle-admin/code/gpec/ec1k-rootfs/platform-EC1K/state/testprog.compile] Error 2

Signed-off-by: Jon Ringle <jringle@gridpoint.com>

bin/ptxdist
@@ -2383,6 +2383,7 @@ setup_config() {
 #      PTXDIST_PARALLELMFLAGS_INTERN
 #      PTXDIST_PARALLELMFLAGS_EXTERN
 #      PTXDIST_OUTPUT_SYNC
+#      PTXDIST_STDERR_REDIRECT
 #
 setup_parallel() {
 	# default no parallel for now
@@ -2415,10 +2416,12 @@ setup_parallel() {
 	fi
 
 	PTXDIST_OUTPUT_SYNC="${PTXDIST_OUTPUT_SYNC:-${PTXDIST_QUIET}}"
+	PTXDIST_STDERR_REDIRECT=2
 	if [ "${PTXDIST_OUTPUT_SYNC}" == "1" ]; then
 		if "${PTXCONF_SETUP_HOST_MAKE}" -h | grep -q -- --output-sync; then
 			PTXDIST_OUTPUT_SYNC="--output-sync="
 		fi
+		PTXDIST_STDERR_REDIRECT=1
 	fi
 	if [[ "${PTXDIST_OUTPUT_SYNC}" =~ [01] ]]; then
 		unset PTXDIST_OUTPUT_SYNC
@@ -2720,7 +2723,8 @@ setup_export() {
 		PTXDIST_OUTPUT_SYNC \
 		PTXDIST_PEDANTIC \
 		PTXDIST_QUIET \
-		PTXDIST_VERBOSE
+		PTXDIST_VERBOSE \
+		PTXDIST_STDERR_REDIRECT
 }
 
 

scripts/lib/ptxd_make_world_compile.sh
@@ -11,6 +11,8 @@
 # call the compiler
 #
 ptxd_make_world_compile() {
+
+    ( exec 2>&${PTXDIST_STDERR_REDIRECT}
     ptxd_make_world_init &&
 
     if [ -z "${pkg_build_dir}" ]; then
@@ -28,7 +30,7 @@ ptxd_make_world_compile() {
 	    "${ptx_build_python}" \
 	    setup.py \
 	    "${pkg_make_opt}"
-	) 2>&1
+	)
 	;;
 	meson)
 	ptxd_eval \
@@ -37,7 +39,7 @@ ptxd_make_world_compile() {
 	    "${pkg_make_env}" \
 	    ninja -C "${pkg_build_dir}" \
 	    "${pkg_make_opt}" \
-	    "${pkg_make_par}" 2>&1
+	    "${pkg_make_par}"
 	;;
 	*)
 	ptxd_eval \
@@ -46,8 +48,8 @@ ptxd_make_world_compile() {
 	    "${pkg_make_env}" \
 	    "${MAKE}" -C "${pkg_build_dir}" \
 	    "${pkg_make_opt}" \
-	    "${pkg_make_par}" 2>&1
+	    "${pkg_make_par}"
 	;;
-    esac
+    esac )
 }
 export -f ptxd_make_world_compile