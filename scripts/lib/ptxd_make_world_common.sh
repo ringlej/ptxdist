b2ab2eb421f308c29961730037472e6e40ffa1fb
Michael Olbrich <m.olbrich@pengutronix.de>
2017-06-12 19:55:20 +0200

    pkg-config-wrapper: only allow packages that are selected
    Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

--- scripts/lib/ptxd_make_world_common.sh
+++ scripts/lib/ptxd_make_world_common.sh
@@ -329,6 +329,12 @@ ptxd_make_world_init() {
 	    ;;
 	*) ;;
     esac
+    local pkgconfig_whitelist
+    pkgconfig_whitelist="$(echo $(
+	for dep in ${pkg_build_deps}; do
+	    cat "${ptx_state_dir}/${dep}.pkgconfig" 2>/dev/null;
+	done))"
+    pkg_env="PKGCONFIG_WHITELIST='${pkgconfig_whitelist}' PKGCONFIG_WHITELIST_SRC='${pkg_label}' ${pkg_env}"
 
     # DESTDIR
     if [[ "${pkg_conf_tool}" =~ "python" ]]; then
--- scripts/lib/ptxd_make_world_install.sh
+++ scripts/lib/ptxd_make_world_install.sh
@@ -213,6 +213,13 @@ export -f ptxd_make_world_install_pack
 #
 ptxd_make_world_install_post() {
     ptxd_make_world_init &&
+    (
+	find "${pkg_pkg_dir}"/usr/{lib,share}/pkgconfig -name *.pc \
+	    -printf "%f\n" 2>/dev/null | sed 's/\.pc$//'
+	for dep in ${pkg_build_deps}; do
+	    cat "${ptx_state_dir}/${dep}.pkgconfig" 2>/dev/null;
+	done
+    ) | sort -u > "${ptx_state_dir}/${pkg_label}.pkgconfig"
     # do nothing if pkg_pkg_dir does not exist
     if [ \! -d "${pkg_pkg_dir}" ]; then
 	return
