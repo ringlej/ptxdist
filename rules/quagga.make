ringlej/ptxdist

commit 01a658fcec11fff13a712dc879209ff1675af6ae

quagga: this patch adds quagga support to ptxdist
Quagga is a routing software suite, providing implementations of
OSPFv2, OSPFv3, RIP v1 and v2, RIPng and BGP-4 for Unix platforms,
particularly FreeBSD, Linux, Solaris and NetBSD.

Signed-off-by: Oliver Graute <oliver.graute@neuhaus.de>
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/quagga.make
@@ -1,0 +1,136 @@
+# -*-makefile-*-
+#
+# Copyright (C) 2015 Dr. Neuhaus Telekommunikation GmbH, Hamburg Germany, Oliver Graute <oliver.graute@neuhaus.de>
+#
+# See CREDITS for details about who has contributed to this project.
+#
+# For further information about the PTXdist project and license conditions
+# see the README file.
+#
+
+#
+# We provide this package
+#
+PACKAGES-$(PTXCONF_QUAGGA) += quagga
+
+#
+# Paths and names
+#
+QUAGGA_VERSION	:= 0.99.23
+QUAGGA_MD5	:= d17145e62b6ea14f0f13bb63f59e5166
+QUAGGA		:= quagga-$(QUAGGA_VERSION)
+QUAGGA_SUFFIX	:= tar.gz
+QUAGGA_URL	:= http://download.savannah.gnu.org/releases/quagga/$(QUAGGA).$(QUAGGA_SUFFIX)
+QUAGGA_SOURCE	:= $(SRCDIR)/$(QUAGGA).$(QUAGGA_SUFFIX)
+QUAGGA_DIR	:= $(BUILDDIR)/$(QUAGGA)
+QUAGGA_LICENSE	:= GPLv2
+
+# ----------------------------------------------------------------------------
+# Prepare
+# ----------------------------------------------------------------------------
+
+#
+# autoconf
+#
+QUAGGA_CONF_TOOL	:= autoconf
+QUAGGA_CONF_OPT	:= \
+	$(CROSS_AUTOCONF_USR) \
+	$(GLOBAL_LARGE_FILE_OPTION) \
+	$(GLOBAL_IPV6_OPTION) \
+	--$(call ptx/endis, PTXCONF_VTYSH)-vtysh \
+	--$(call ptx/endis, PTXCONF_ZEBRA)-zebra \
+	--$(call ptx/endis, PTXCONF_BGPD)-bgpd \
+	--$(call ptx/endis, PTXCONF_RIPD)-ripd \
+	--$(call ptx/endis, PTXCONF_RIPNGD)-ripngd \
+	--$(call ptx/endis, PTXCONF_OSPFD)-ospfd \
+	--$(call ptx/endis, PTXCONF_OSPF6D)-ospf6d \
+	--$(call ptx/endis, PTXCONF_BABELD)-babeld \
+	--$(call ptx/endis, PTXCONF_WATCHQUAGGA)-watchquagga \
+	--disable-doc \
+	--enable-isisd \
+	--enable-solaris \
+	--disable-bgp-announce \
+	--enable-netlink \
+	--enable-snmp=agentx \
+	--enable-tcp-zebra \
+	--disable-opaque-lsa \
+	--disable-ospfapi \
+	--disable-ospfclient \
+	--disable-ospf-te \
+	--enable-multipath=1 \
+	--enable-user=user \
+	--enable-group=group \
+	--enable-vty-group=no \
+	--enable-configfile-mask=-600 \
+	--enable-logfile-mask=-600 \
+	--disable-rtadv \
+	--enable-irdp \
+	--enable-isis-topology \
+	--disable-capabilities \
+	--disable-rusage \
+	--enable-gcc-ultra-verbose \
+	--enable-linux24-tcp-md5 \
+	--enable-gcc-rdynamic \
+	--disable-backtrace \
+	--disable-time-check \
+	--enable-pcreposix \
+	--enable-fpm \
+	--disable-pie
+
+# ----------------------------------------------------------------------------
+# Target-Install
+# ----------------------------------------------------------------------------
+
+$(STATEDIR)/quagga.targetinstall:
+	@$(call targetinfo)
+
+	@$(call install_init, quagga)
+	@$(call install_fixup, quagga,PRIORITY,optional)
+	@$(call install_fixup, quagga,SECTION,base)
+	@$(call install_fixup, quagga,AUTHOR,"<oliver.graute@neuhaus.de>")
+	@$(call install_fixup, quagga,DESCRIPTION,missing)
+	@$(call install_lib, quagga, 0, 0, 0644, libzebra)
+
+ifdef PTXCONF_RIPD
+	@$(call install_copy, quagga, 0, 0, 0755, -, /usr/sbin/ripd)
+	@$(call install_alternative, quagga, 0, 0, 0644, /etc/ripd.conf)
+endif
+ifdef PTXCONF_RIPNGD
+	@$(call install_copy, quagga, 0, 0, 0755, -, /usr/sbin/ripngd)
+	@$(call install_alternative, quagga, 0, 0, 0644, /etc/ripngd.conf)
+endif
+ifdef PTXCONF_BABELD
+	@$(call install_copy, quagga, 0, 0, 0755, -, /usr/sbin/babeld)
+	@$(call install_alternative, quagga, 0, 0, 0644, /etc/babeld.conf)
+endif
+ifdef PTXCONF_BGPD
+	@$(call install_copy, quagga, 0, 0, 0755, -, /usr/sbin/bgpd)
+	@$(call install_alternative, quagga, 0, 0, 0644, /etc/bgpd.conf)
+endif
+ifdef PTXCONF_ZEBRA
+	@$(call install_copy, quagga, 0, 0, 0755, -, /usr/sbin/zebra)
+	@$(call install_alternative, quagga, 0, 0, 0644, /etc/zebra.conf)
+endif
+ifdef PTXCONF_OSPFD
+	@$(call install_copy, quagga, 0, 0, 0755, -, /usr/sbin/ospfd)
+	@$(call install_copy, quagga, 0, 0, 0755, -, /usr/sbin/ospfclient)
+	@$(call install_alternative, quagga, 0, 0, 0644, /etc/ospfd.conf)
+endif
+ifdef PTXCONF_OSPF6D
+	@$(call install_copy, quagga, 0, 0, 0755, -, /usr/sbin/ospf6d)
+	@$(call install_alternative, quagga, 0, 0, 0644, /etc/ospf6d.conf)
+endif
+ifdef PTXCONF_WATCHQUAGGA
+	@$(call install_copy, quagga, 0, 0, 0755, -, /usr/sbin/watchquagga)
+endif
+ifdef PTXCONF_VTYSH
+	@$(call install_copy, quagga, 0, 0, 0755, -, /usr/sbin/vtysh)
+	@$(call install_alternative, quagga, 0, 0, 0644, /etc/vtysh.conf)
+endif
+
+
+	@$(call install_finish, quagga)
+
+	@$(call touch)
+
+# vim: syntax=make

projectroot/etc/babeld.conf
@@ -1,0 +1,30 @@
+debug babel common
+!debug babel kernel
+!debug babel filter
+!debug babel timeout
+!debug babel interface
+!debug babel route
+!debug babel all
+
+router babel
+! network wlan0
+! network eth0
+! redistribute kernel
+! no redistribute static
+
+! The defaults are fine for a wireless interface
+
+!interface wlan0
+
+! A few optimisation tweaks are optional but recommended on a wired interface
+! Disable link quality estimation, enable split horizon processing, and
+! increase the hello and update intervals.
+
+!interface eth0
+! babel wired
+! babel split-horizon
+! babel hello-interval 12000
+! babel update-interval 36000
+
+! log file /var/log/quagga/babeld.log
+log stdout

projectroot/etc/bgpd.conf
@@ -1,0 +1,29 @@
+! -*- bgp -*-
+!
+! BGPd sample configuratin file
+!
+! $Id: bgpd.conf.sample,v 1.1 2002/12/13 20:15:29 paul Exp $
+!
+hostname bgpd
+password zebra
+!enable password please-set-at-here
+!
+!bgp mulitple-instance
+!
+router bgp 7675
+! bgp router-id 10.0.0.1
+! network 10.0.0.0/8
+! neighbor 10.0.0.2 remote-as 7675
+! neighbor 10.0.0.2 route-map set-nexthop out
+! neighbor 10.0.0.2 ebgp-multihop
+! neighbor 10.0.0.2 next-hop-self
+!
+! access-list all permit any
+!
+!route-map set-nexthop permit 10
+! match ip address all
+! set ip next-hop 10.0.0.1
+!
+!log file bgpd.log
+!
+log stdout

projectroot/etc/ospf6d.conf
@@ -1,0 +1,52 @@
+!
+! Zebra configuration saved from vty
+!   2003/11/28 00:49:49
+!
+hostname ospf6d@plant
+password zebra
+log stdout
+service advanced-vty
+!
+debug ospf6 neighbor state
+!
+interface fxp0
+ ipv6 ospf6 cost 1
+ ipv6 ospf6 hello-interval 10
+ ipv6 ospf6 dead-interval 40
+ ipv6 ospf6 retransmit-interval 5
+ ipv6 ospf6 priority 0
+ ipv6 ospf6 transmit-delay 1
+ ipv6 ospf6 instance-id 0
+!
+interface lo0
+ ipv6 ospf6 cost 1
+ ipv6 ospf6 hello-interval 10
+ ipv6 ospf6 dead-interval 40
+ ipv6 ospf6 retransmit-interval 5
+ ipv6 ospf6 priority 1
+ ipv6 ospf6 transmit-delay 1
+ ipv6 ospf6 instance-id 0
+!
+router ospf6
+ router-id 255.1.1.1
+ redistribute static route-map static-ospf6
+ interface fxp0 area 0.0.0.0
+!
+access-list access4 permit 127.0.0.1/32
+!
+ipv6 access-list access6 permit 3ffe:501::/32
+ipv6 access-list access6 permit 2001:200::/48
+ipv6 access-list access6 permit ::1/128
+!
+ipv6 prefix-list test-prefix seq 1000 deny any
+!
+route-map static-ospf6 permit 10
+ match ipv6 address prefix-list test-prefix
+ set metric-type type-2
+ set metric 2000
+!
+line vty
+ access-class access4
+ ipv6 access-class access6
+ exec-timeout 0 0
+!

projectroot/etc/ospfd.conf
@@ -1,0 +1,13 @@
+! -*- ospf -*-
+!
+! OSPFd sample configuration file
+!
+!
+hostname ospfd
+password zebra
+!enable password please-set-at-here
+!
+!router ospf
+!  network 192.168.1.0/24 area 0
+!
+log stdout

projectroot/etc/ripd.conf
@@ -1,0 +1,24 @@
+! -*- rip -*-
+!
+! RIPd sample configuration file
+!
+! $Id: ripd.conf.sample,v 1.1 2002/12/13 20:15:30 paul Exp $
+!
+hostname ripd
+password zebra
+!
+! debug rip events
+! debug rip packet
+!
+router rip
+! network 11.0.0.0/8
+! network eth0
+! route 10.0.0.0/8
+! distribute-list private-only in eth0
+!
+!access-list private-only permit 10.0.0.0/8
+!access-list private-only deny any
+! 
+!log file ripd.log
+!
+log stdout

projectroot/etc/ripngd.conf
@@ -1,0 +1,22 @@
+! -*- rip -*-
+!
+! RIPngd sample configuration file
+!
+! $Id: ripngd.conf.sample,v 1.1 2002/12/13 20:15:30 paul Exp $
+!
+hostname ripngd
+password zebra
+!
+! debug ripng events
+! debug ripng packet
+!
+!
+router ripng
+! network sit1
+! route 3ffe:506::0/32
+! distribute-list local-only out sit1
+!
+!ipv6 access-list local-only permit 3ffe:506::0/32
+!ipv6 access-list local-only deny any
+!
+log stdout

projectroot/etc/vtysh.conf
@@ -1,0 +1,7 @@
+!
+! Sample configuration file for vtysh.
+!
+!service integrated-vtysh-config
+!hostname quagga-router
+!username root nopassword
+!

commit f3de95cc272a61e0d83e20b66d4206acdf0a248b

quagga: fix configuration and install options
fixes: 01a658fcec11 quagga: this patch adds quagga support to ptxdist

Signed-off-by: Ladislav Michl <ladis@linux-mips.org>
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/quagga.make
@@ -37,15 +37,15 @@ QUAGGA_CONF_OPT	:= \
 	$(CROSS_AUTOCONF_USR) \
 	$(GLOBAL_LARGE_FILE_OPTION) \
 	$(GLOBAL_IPV6_OPTION) \
-	--$(call ptx/endis, PTXCONF_VTYSH)-vtysh \
-	--$(call ptx/endis, PTXCONF_ZEBRA)-zebra \
-	--$(call ptx/endis, PTXCONF_BGPD)-bgpd \
-	--$(call ptx/endis, PTXCONF_RIPD)-ripd \
-	--$(call ptx/endis, PTXCONF_RIPNGD)-ripngd \
-	--$(call ptx/endis, PTXCONF_OSPFD)-ospfd \
-	--$(call ptx/endis, PTXCONF_OSPF6D)-ospf6d \
-	--$(call ptx/endis, PTXCONF_BABELD)-babeld \
-	--$(call ptx/endis, PTXCONF_WATCHQUAGGA)-watchquagga \
+	--$(call ptx/endis, PTXCONF_QUAGGA_VTYSH)-vtysh \
+	--$(call ptx/endis, PTXCONF_QUAGGA_ZEBRA)-zebra \
+	--$(call ptx/endis, PTXCONF_QUAGGA_BGPD)-bgpd \
+	--$(call ptx/endis, PTXCONF_QUAGGA_RIPD)-ripd \
+	--$(call ptx/endis, PTXCONF_QUAGGA_RIPNGD)-ripngd \
+	--$(call ptx/endis, PTXCONF_QUAGGA_OSPFD)-ospfd \
+	--$(call ptx/endis, PTXCONF_QUAGGA_OSPF6D)-ospf6d \
+	--$(call ptx/endis, PTXCONF_QUAGGA_BABELD)-babeld \
+	--$(call ptx/endis, PTXCONF_QUAGGA_WATCHQUAGGA)-watchquagga \
 	--disable-doc \
 	--enable-isisd \
 	--enable-solaris \
@@ -91,40 +91,39 @@ $(STATEDIR)/quagga.targetinstall:
 	@$(call install_fixup, quagga,DESCRIPTION,missing)
 	@$(call install_lib, quagga, 0, 0, 0644, libzebra)
 
-ifdef PTXCONF_RIPD
+ifdef PTXCONF_QUAGGA_RIPD
 	@$(call install_copy, quagga, 0, 0, 0755, -, /usr/sbin/ripd)
 	@$(call install_alternative, quagga, 0, 0, 0644, /etc/ripd.conf)
 endif
-ifdef PTXCONF_RIPNGD
+ifdef PTXCONF_QUAGGA_RIPNGD
 	@$(call install_copy, quagga, 0, 0, 0755, -, /usr/sbin/ripngd)
 	@$(call install_alternative, quagga, 0, 0, 0644, /etc/ripngd.conf)
 endif
-ifdef PTXCONF_BABELD
+ifdef PTXCONF_QUAGGA_BABELD
 	@$(call install_copy, quagga, 0, 0, 0755, -, /usr/sbin/babeld)
 	@$(call install_alternative, quagga, 0, 0, 0644, /etc/babeld.conf)
 endif
-ifdef PTXCONF_BGPD
+ifdef PTXCONF_QUAGGA_BGPD
 	@$(call install_copy, quagga, 0, 0, 0755, -, /usr/sbin/bgpd)
 	@$(call install_alternative, quagga, 0, 0, 0644, /etc/bgpd.conf)
 endif
-ifdef PTXCONF_ZEBRA
+ifdef PTXCONF_QUAGGA_ZEBRA
 	@$(call install_copy, quagga, 0, 0, 0755, -, /usr/sbin/zebra)
 	@$(call install_alternative, quagga, 0, 0, 0644, /etc/zebra.conf)
 endif
-ifdef PTXCONF_OSPFD
+ifdef PTXCONF_QUAGGA_OSPFD
 	@$(call install_copy, quagga, 0, 0, 0755, -, /usr/sbin/ospfd)
-	@$(call install_copy, quagga, 0, 0, 0755, -, /usr/sbin/ospfclient)
 	@$(call install_alternative, quagga, 0, 0, 0644, /etc/ospfd.conf)
 endif
-ifdef PTXCONF_OSPF6D
+ifdef PTXCONF_QUAGGA_OSPF6D
 	@$(call install_copy, quagga, 0, 0, 0755, -, /usr/sbin/ospf6d)
 	@$(call install_alternative, quagga, 0, 0, 0644, /etc/ospf6d.conf)
 endif
-ifdef PTXCONF_WATCHQUAGGA
+ifdef PTXCONF_QUAGGA_WATCHQUAGGA
 	@$(call install_copy, quagga, 0, 0, 0755, -, /usr/sbin/watchquagga)
 endif
-ifdef PTXCONF_VTYSH
-	@$(call install_copy, quagga, 0, 0, 0755, -, /usr/sbin/vtysh)
+ifdef PTXCONF_QUAGGA_VTYSH
+	@$(call install_copy, quagga, 0, 0, 0755, -, /usr/bin/vtysh)
 	@$(call install_alternative, quagga, 0, 0, 0644, /etc/vtysh.conf)
 endif
 