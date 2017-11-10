e4264503789bf37e6687962c697949ff7a49f7ce
Juergen Borleis <jbe@pengutronix.de>
2017-11-10 12:17:02 +0100

    Templates/src-autoconf-prog: improve and unify template
    Unify and simplify the executable template by using the macros from the
    'ptx.m4' and Co. M4 macro collection.
    
    Signed-off-by: Juergen Borleis <jbe@pengutronix.de>

--- rules/templates/src-autoconf-prog/@name@.c
+++ rules/templates/src-autoconf-prog/@name@.c
@@ -1,4 +1,13 @@
+/*
+ * For copyright/license information refer COPYING in the main directory
+ */
+
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
+
 #include <stdio.h>
+#include "internal.h"
 
 int main(void)
 {
