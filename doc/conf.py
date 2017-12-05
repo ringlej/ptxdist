f2910b3eb7eed6a257847cd42ee8b943e33207c4
Juergen Borleis <jbe@pengutronix.de>
2017-12-05 14:30:47 +0100

    Doc: just be in time
    Signed-off-by: Juergen Borleis <jbe@pengutronix.de>

--- doc/conf.py
+++ doc/conf.py
@@ -47,7 +47,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'PTXdist'
-copyright = u'2015, The Pengutronix Development Team'
+copyright = u'2015-2017, The Pengutronix Development Team'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
