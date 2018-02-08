ringlej/ptxdist

commit 878707bd943a04fc8e3a55a151499359e81ce780

logrotate: version bump to 3.9.1
- compile using autotools instead of hand craftet build system
- add example config file
- add example systemd timer and service

Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>

rules/logrotate.make
@@ -1,6 +1,6 @@
 # -*-makefile-*-
 #
-# Copyright (C) 2006 by Marc Kleine-Budde <mkl@pengutronix.de>
+# Copyright (C) 2006, 2015 by Marc Kleine-Budde <mkl@pengutronix.de>
 #           (C) 2010 by Michael Olbrich <m.olbrich@pengutronix.de>
 #
 # See CREDITS for details about who has contributed to this project.
@@ -17,8 +17,8 @@ PACKAGES-$(PTXCONF_LOGROTATE) += logrotate
 #
 # Paths and names
 #
-LOGROTATE_VERSION	:= 3.8.7
-LOGROTATE_MD5		:= 99e08503ef24c3e2e3ff74cc5f3be213
+LOGROTATE_VERSION	:= 3.9.1
+LOGROTATE_MD5		:= 4492b145b6d542e4a2f41e77fa199ab0
 LOGROTATE		:= logrotate-$(LOGROTATE_VERSION)
 LOGROTATE_SUFFIX	:= tar.gz
 LOGROTATE_URL		:= https://fedorahosted.org/releases/l/o/logrotate/$(LOGROTATE).$(LOGROTATE_SUFFIX)
@@ -30,11 +30,12 @@ LOGROTATE_LICENSE	:= GPLv2
 # Compile
 # ----------------------------------------------------------------------------
 
-LOGROTATE_PATH		:= PATH=$(CROSS_PATH)
-LOGROTATE_MAKE_ENV	:= $(CROSS_ENV) RPM_OPT_FLAGS='$(strip $(CROSS_CPPFLAGS))'
-LOGROTATE_MAKE_OPT	:= OS_NAME=Linux LFS=-D_FILE_OFFSET_BITS=64
-
-LOGROTATE_INSTALL_OPT	:= PREFIX=$(LOGROTATE_PKGDIR) install
+LOGROTATE_CONF_TOOL := autoconf
+LOGROTATE_CONF_OPT := \
+	$(CROSS_AUTOCONF_USR) \
+	$(GLOBAL_LARGE_FILE_OPTION) \
+	--$(call ptx/wwo, PTXCONF_LOGROTATE_ACL)-acl \
+	--$(call ptx/wwo, PTXCONF_GLOBAL_SELINUX)-selinux
 
 # ----------------------------------------------------------------------------
 # Target-Install
@@ -50,6 +51,16 @@ $(STATEDIR)/logrotate.targetinstall:
 	@$(call install_fixup, logrotate,DESCRIPTION,missing)
 
 	@$(call install_copy, logrotate, 0, 0, 0755, -, /usr/sbin/logrotate)
+	@$(call install_alternative, logrotate, 0, 0, 0644, /etc/logrotate.conf)
+
+ifdef PTXCONF_LOGROTATE_SYSTEMD_UNIT
+	@$(call install_alternative, logrotate, 0, 0, 0644, \
+		/lib/systemd/system/logrotate.timer)
+	@$(call install_alternative, logrotate, 0, 0, 0644, \
+		/lib/systemd/system/logrotate.service)
+	@$(call install_link, logrotate, ../logrotate.timer, \
+		/lib/systemd/system/multi-user.target.wants/logrotate.timer)
+endif
 
 	@$(call install_finish, logrotate)
 

generic/etc/logrotate.conf
@@ -1,0 +1,13 @@
+/var/log/syslog {
+	missingok
+
+	# rotate after X bytes - ignore time
+	size 512k
+
+	# keep backlog of X
+	rotate 20
+
+	postrotate
+		systemctl restart rsyslog
+	endscript
+}

generic/lib/systemd/system/logrotate.service
@@ -1,0 +1,5 @@
+[Unit]
+Description=logrotate
+
+[Service]
+ExecStart=/usr/sbin/logrotate /etc/logrotate.conf

generic/lib/systemd/system/logrotate.timer
@@ -1,0 +1,11 @@
+[Unit]
+Description=logrotate timer
+
+[Timer]
+OnBootSec=5min
+OnUnitActiveSec=1h
+AccuracySec=10s
+Unit=logrotate.service
+
+[Install]
+WantedBy=multi-user.target

patches/logrotate-3.8.7/0004-mktime-718332.patch
@@ -1,28 +1,0 @@
-From: unknown author <unknown.author@example.com>
-Date: Fri, 24 Jan 2014 16:45:55 +0100
-Subject: [PATCH] mktime-718332
-
-===================================================================
----
- logrotate.c |    2 ++
- 1 file changed, 2 insertions(+)
-
-diff --git a/logrotate.c b/logrotate.c
-index 755f395..526750b 100644
---- a/logrotate.c
-+++ b/logrotate.c
-@@ -2115,12 +2115,14 @@ static int readState(char *stateFilename)
- 		return 1;
- 	}
- 
-+	memset(&st->lastRotated, 0, sizeof(st->lastRotated));
- 	st->lastRotated.tm_year = year;
- 	st->lastRotated.tm_mon = month;
- 	st->lastRotated.tm_mday = day;
- 	st->lastRotated.tm_hour = hour;
- 	st->lastRotated.tm_min = minute;
- 	st->lastRotated.tm_sec = second;
-+	st->lastRotated.tm_isdst = -1;
- 
- 	/* fill in the rest of the st->lastRotated fields */
- 	lr_time = mktime(&st->lastRotated);

patches/logrotate-3.8.7/series
@@ -1,7 +1,0 @@
-# generated by git-ptx-patches
-#tag:base --start-number 1
-0001-datehack.patch
-0002-cpp-crossbuild.patch
-0003-chown-484762.patch
-0004-mktime-718332.patch
-# 0370e7c37f32d0185d759e6b73540bc8  - git-ptx-patches magic

patches/logrotate-3.9.1/0001-deb-config-h.patch
@@ -1,0 +1,29 @@
+From: unknown author <unknown.author@example.com>
+Date: Thu, 11 Jun 2015 15:35:31 +0200
+Subject: [PATCH] deb-config-h
+
+Fix locations of STATEFILE and DEFAULT_MAIL_COMMAND for Debian.
+---
+ config.h | 4 ++--
+ 1 file changed, 2 insertions(+), 2 deletions(-)
+
+diff --git a/config.h b/config.h
+index d715c5a83e49..9a7df229d6cd 100644
+--- a/config.h
++++ b/config.h
+@@ -27,7 +27,7 @@
+  * Default settings for Linux - leave these last.
+  */
+ #ifndef DEFAULT_MAIL_COMMAND
+-#define DEFAULT_MAIL_COMMAND "/bin/mail"
++#define DEFAULT_MAIL_COMMAND "/usr/bin/mail"
+ #endif
+ 
+ #ifndef COMPRESS_COMMAND
+@@ -43,5 +43,5 @@
+ #endif
+ 
+ #ifndef STATEFILE
+-#define STATEFILE "/var/lib/logrotate.status"
++#define STATEFILE "/var/lib/logrotate/status"
+ #endif

patches/logrotate-3.9.1/0002-datehack.patch
@@ -1,18 +1,18 @@
 From: unknown author <unknown.author@example.com>
-Date: Fri, 24 Jan 2014 16:45:55 +0100
+Date: Thu, 11 Jun 2015 15:35:31 +0200
 Subject: [PATCH] datehack
 
 No longer consider state file dates before 1996 as fatal errors.
 (Closes: #65534)
 ---
- logrotate.c |    2 +-
+ logrotate.c | 2 +-
  1 file changed, 1 insertion(+), 1 deletion(-)
 
 diff --git a/logrotate.c b/logrotate.c
-index 174a26b..a153fb7 100644
+index d3deb6add62a..8ffcff6f6ac0 100644
 --- a/logrotate.c
 +++ b/logrotate.c
-@@ -2046,7 +2046,7 @@ static int readState(char *stateFilename)
+@@ -2334,7 +2334,7 @@ static int readState(char *stateFilename)
  	}
  
  	/* Hack to hide earlier bug */

commit a851d291af52b17d43619229f58f3712816ff596

logrotate: Create directory for state file
When upgrading from logrotate 3.8.7 to 3.9.1 a new patch
patches/logrotate-3.9.1/0001-deb-config-h.patch was introduced which
changed the paths to DEFAULT_MAIL_COMMAND and STATEFILE. The statefile
was created by logrotate in /var/lib before, now it expects
/var/lib/logrotate to be present. If the directory is missing, logrotate
fails with the following error:

    error: error creating state file /var/lib/logrotate/status: No such file or directory

So after upgrading to ptxdist 2015.07.0 (containing the above mentioned
upgrade) all calls to logrotate must have been failed. This can easily
be fixed by just creating the empty folder.

Note: users may have overridden the path to the state file by passing
another path together with the -s option of logrotate.

Fixes: 878707bd943a04fc8e3a55a151499359e81ce780
Signed-off-by: Alexander Dahl <ada@thorsis.com>
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/logrotate.make
@@ -54,6 +54,7 @@ $(STATEDIR)/logrotate.targetinstall:
 
 	@$(call install_copy, logrotate, 0, 0, 0755, -, /usr/sbin/logrotate)
 	@$(call install_alternative, logrotate, 0, 0, 0644, /etc/logrotate.conf)
+	@$(call install_copy, logrotate, 0, 0, 0755, /var/lib/logrotate)
 
 ifdef PTXCONF_LOGROTATE_SYSTEMD_UNIT
 	@$(call install_alternative, logrotate, 0, 0, 0644, \