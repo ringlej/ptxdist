ringlej/ptxdist

commit 74ffd1f57d0d68a89183ea2bf47c0cd67a989520

htop: fix configure to discover ncurses6
htop's configure tries to link ncurses test agains both supplied
library and the one returned from ncurses-config --libs which
obviously fails for certain combinations. While it deserves
proper fix in htop, let's make patch minimal here for sake
of simplicity.

Signed-off-by: Ladislav Michl <ladis@linux-mips.org>
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

patches/htop-2.1.0/0002-configure-check-for-ncurses-with-ncurses6-config.patch
@@ -1,0 +1,20 @@
+--- htop-2.1.0/configure.ac.orig	2018-02-27 11:23:08.615702344 +0100
++++ htop-2.1.0/configure.ac	2018-02-27 11:24:21.287425809 +0100
+@@ -202,11 +202,12 @@
+             [AC_CHECK_HEADERS([ncurses.h],[:],[missing_headers="$missing_headers $ac_header"])])])])
+ else
+    HTOP_CHECK_SCRIPT([ncurses6], [refresh], [HAVE_LIBNCURSES], "ncurses6-config",
+-    HTOP_CHECK_SCRIPT([ncurses], [refresh], [HAVE_LIBNCURSES], "ncurses5-config",
+-     HTOP_CHECK_LIB([ncurses6],  [refresh], [HAVE_LIBNCURSES],
+-      HTOP_CHECK_LIB([ncurses],  [refresh], [HAVE_LIBNCURSES],
+-      missing_libraries="$missing_libraries libncurses"
+-   ))))
++    HTOP_CHECK_SCRIPT([ncurses], [refresh], [HAVE_LIBNCURSES], "ncurses6-config",
++     HTOP_CHECK_SCRIPT([ncurses], [refresh], [HAVE_LIBNCURSES], "ncurses5-config",
++      HTOP_CHECK_LIB([ncurses6],  [refresh], [HAVE_LIBNCURSES],
++       HTOP_CHECK_LIB([ncurses],  [refresh], [HAVE_LIBNCURSES],
++       missing_libraries="$missing_libraries libncurses"
++   )))))
+    
+    AC_CHECK_HEADERS([curses.h],[:],
+       [AC_CHECK_HEADERS([ncurses/curses.h],[:],

patches/htop-2.1.0/autogen.sh
@@ -1,0 +1,1 @@
+../autogen.sh

patches/htop-2.1.0/series
@@ -1,4 +1,5 @@
 # generated by git-ptx-patches
 #tag:base --start-number 1
 0001-linux-LinuxProcessList-fix-reading-of-number-of-read.patch
+0002-configure-check-for-ncurses-with-ncurses6-config.patch
 # f87a037f960f427f6ab7ebcbb54e5e4e  - git-ptx-patches magic

commit 3fddf37417692ffe09de5a7ed42cb2c1a9f949ec

htop: version bump 2.1.0 -> 2.2.0
Removed all patches:
- 0001-linux-LinuxProcessList-fix-reading-of-number-of-read.patch:
  applied upstream in https://github.com/hishamhm/htop/commit/70ed51a303
- 0002-configure-check-for-ncurses-with-ncurses6-config.patch:
  replaced by setting HTOP_NCURSES_CONFIG_SCRIPT=ncurses6-config in
  HTOP_CONF_ENV

Fixes: 74ffd1f57d0d68a8918 ("htop: fix configure to discover ncurses6")
Cc: Ladislav Michl <ladis@linux-mips.org>
Signed-off-by: Roland Hieber <r.hieber@pengutronix.de>
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

patches/htop-2.1.0/0001-linux-LinuxProcessList-fix-reading-of-number-of-read.patch
@@ -1,28 +1,0 @@
-From: Marc Kleine-Budde <mkl@pengutronix.de>
-Date: Wed, 7 Feb 2018 23:16:37 +0100
-Subject: [PATCH] linux/LinuxProcessList: fix reading of number of read
- syscalls of process
-
-The "if" tests if the character at index "5" is 'r', as a first quick
-check. However at index "5" will always be a colon ":". This patch fixes
-the off-by-one error. htop now shows proper values in the RD_SYSC
-column.
-
-Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>
----
- linux/LinuxProcessList.c | 2 +-
- 1 file changed, 1 insertion(+), 1 deletion(-)
-
-diff --git a/linux/LinuxProcessList.c b/linux/LinuxProcessList.c
-index 6f2631afa12f..230770258084 100644
---- a/linux/LinuxProcessList.c
-+++ b/linux/LinuxProcessList.c
-@@ -436,7 +436,7 @@ static void LinuxProcessList_readIoFile(LinuxProcess* process, const char* dirna
-          }
-          break;
-       case 's':
--         if (line[5] == 'r' && strncmp(line+1, "yscr: ", 6) == 0) {
-+         if (line[4] == 'r' && strncmp(line+1, "yscr: ", 6) == 0) {
-             process->io_syscr = strtoull(line+7, NULL, 10);
-          } else if (strncmp(line+1, "yscw: ", 6) == 0) {
-             process->io_syscw = strtoull(line+7, NULL, 10);

patches/htop-2.1.0/0002-configure-check-for-ncurses-with-ncurses6-config.patch
@@ -1,20 +1,0 @@
---- htop-2.1.0/configure.ac.orig	2018-02-27 11:23:08.615702344 +0100
-+++ htop-2.1.0/configure.ac	2018-02-27 11:24:21.287425809 +0100
-@@ -202,11 +202,12 @@
-             [AC_CHECK_HEADERS([ncurses.h],[:],[missing_headers="$missing_headers $ac_header"])])])])
- else
-    HTOP_CHECK_SCRIPT([ncurses6], [refresh], [HAVE_LIBNCURSES], "ncurses6-config",
--    HTOP_CHECK_SCRIPT([ncurses], [refresh], [HAVE_LIBNCURSES], "ncurses5-config",
--     HTOP_CHECK_LIB([ncurses6],  [refresh], [HAVE_LIBNCURSES],
--      HTOP_CHECK_LIB([ncurses],  [refresh], [HAVE_LIBNCURSES],
--      missing_libraries="$missing_libraries libncurses"
--   ))))
-+    HTOP_CHECK_SCRIPT([ncurses], [refresh], [HAVE_LIBNCURSES], "ncurses6-config",
-+     HTOP_CHECK_SCRIPT([ncurses], [refresh], [HAVE_LIBNCURSES], "ncurses5-config",
-+      HTOP_CHECK_LIB([ncurses6],  [refresh], [HAVE_LIBNCURSES],
-+       HTOP_CHECK_LIB([ncurses],  [refresh], [HAVE_LIBNCURSES],
-+       missing_libraries="$missing_libraries libncurses"
-+   )))))
-    
-    AC_CHECK_HEADERS([curses.h],[:],
-       [AC_CHECK_HEADERS([ncurses/curses.h],[:],

patches/htop-2.1.0/autogen.sh
@@ -1,1 +1,0 @@
-../autogen.sh

patches/htop-2.1.0/series
@@ -1,5 +1,0 @@
-# generated by git-ptx-patches
-#tag:base --start-number 1
-0001-linux-LinuxProcessList-fix-reading-of-number-of-read.patch
-0002-configure-check-for-ncurses-with-ncurses6-config.patch
-# f87a037f960f427f6ab7ebcbb54e5e4e  - git-ptx-patches magic

rules/htop.make
@@ -17,8 +17,8 @@ PACKAGES-$(PTXCONF_HTOP) += htop
 #
 # Paths and names
 #
-HTOP_VERSION	:= 2.1.0
-HTOP_MD5	:= f262b66ad6c194782f4d3a80627e84c8
+HTOP_VERSION	:= 2.2.0
+HTOP_MD5	:= 0d816b6beed31edc75babcfbf863ffa8
 HTOP		:= htop-$(HTOP_VERSION)
 HTOP_SUFFIX	:= tar.gz
 HTOP_URL	:= http://hisham.hm/htop/releases/$(HTOP_VERSION)/$(HTOP).$(HTOP_SUFFIX)
@@ -30,6 +30,9 @@ HTOP_LICENSE	:= GPL-2.0-only
 # Prepare
 # ----------------------------------------------------------------------------
 
+HTOP_CONF_ENV	:= \
+	$(CROSS_ENV) \
+	HTOP_NCURSES_CONFIG_SCRIPT=ncurses6-config
 #
 # autoconf
 #