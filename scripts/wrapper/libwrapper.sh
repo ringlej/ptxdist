6882777ae7ee85d0a8131cefdd1aeb3895697372
Michael Olbrich <m.olbrich@pengutronix.de>
2018-05-15 13:47:06 +0200

    wrapper: don't use target LDFLAGS for host tools
    Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

--- scripts/wrapper/libwrapper.sh
+++ scripts/wrapper/libwrapper.sh
@@ -168,7 +168,7 @@ cc_add_target_ld_args() {
 cc_add_host_ld_args() {
 	if ${LINKING}; then
 		cc_check_args ${pkg_ldflags}
-		add_arg ${pkg_ldflags}
+		add_host_arg ${pkg_ldflags}
 		add_late_arg ${PTXDIST_HOST_LDFLAGS}
 	fi
 }
