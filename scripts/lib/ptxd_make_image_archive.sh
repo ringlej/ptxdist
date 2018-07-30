8e28b1c2d0368b83ad4d8ad0b6f9aa7dbbf2d261
Michael Olbrich <m.olbrich@pengutronix.de>
2018-07-30 09:59:34 +0200

    ptxd_make_image_archive: make sure the image does not exist when an error occurs
    'tar' may fail but leave the image file in an undefined state.
    
    Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

--- scripts/lib/ptxd_make_image_archive.sh
+++ scripts/lib/ptxd_make_image_archive.sh
@@ -21,8 +21,13 @@ ptxd_make_image_archive_impl() {
 	echo -n "with label \"${image_label}\" "
     fi
     echo -ne "...\n" &&
-    tar "${image_label_args[@]}" -zcf "${image_image}" . &&
+    tar "${image_label_args[@]}" -zcf "${image_image}" .
+    local ret=$?
+    if [ "${ret}" != 0 ]; then
+	rm -f "${image_image}"
+    fi
     rm -r "${pkg_dir}"
+    return "${ret}"
 }
 export -f ptxd_make_image_archive_impl
 
