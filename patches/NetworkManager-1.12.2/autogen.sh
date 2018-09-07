fd33b03481a411a2d511874e7bf68e1c91000ea5
Michael Olbrich <m.olbrich@pengutronix.de>
2018-09-07 08:47:54 +0200

    networkmanager: fix autogen.sh
    intltoolize is needed for autoreconf to work correctly.
    
    Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

--- patches/NetworkManager-1.12.2/autogen.sh
+++ patches/NetworkManager-1.12.2/autogen.sh
@@ -1,1 +1,19 @@
-../autogen.sh
+#!/bin/bash
+
+set -e
+
+aclocal $ACLOCAL_FLAGS
+
+libtoolize \
+	--force \
+	--copy
+
+AUTOPOINT='intltoolize --automake --copy' \
+autoreconf \
+	--force \
+	--install \
+	--warnings=cross \
+	--warnings=syntax \
+	--warnings=obsolete \
+	--warnings=unsupported
+
