ringlej/ptxdist

commit 2f49c9fbd60f97b32a38eb9bc715361695fc2fe0

lighttpd: version bump 1.4.45 -> 1.4.51
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/lighttpd.make
@@ -17,8 +17,8 @@ PACKAGES-$(PTXCONF_LIGHTTPD) += lighttpd
 #
 # Paths and names
 #
-LIGHTTPD_VERSION	:= 1.4.45
-LIGHTTPD_MD5		:= a128e1eda76899ce3fd115efae5fe631
+LIGHTTPD_VERSION	:= 1.4.51
+LIGHTTPD_MD5		:= 6e68c19601af332fa3c5f174245f59bf
 LIGHTTPD		:= lighttpd-$(LIGHTTPD_VERSION)
 LIGHTTPD_SUFFIX		:= tar.xz
 LIGHTTPD_URL		:= http://download.lighttpd.net/lighttpd/releases-1.4.x/$(LIGHTTPD).$(LIGHTTPD_SUFFIX)
@@ -44,6 +44,7 @@ LIGHTTPD_CONF_OPT	:= \
 	--without-libev \
 	--without-mysql \
 	--without-ldap \
+	--without-pam \
 	--$(call ptx/wwo, PTXCONF_LIGHTTPD_ATTR)-attr \
 	--without-valgrind \
 	--without-libunwind \

commit 90f00cacd617cac9543638bb89c1a55c79cfc6fe

lighttpd: After version bump cleanup
v1.4.51 contains new modules, new protocols, bug fixes, new features,
and security fixes. This sets the new configure options and has links to
the announcements for completeness.

* https://www.lighttpd.net/2017/10/21/1.4.46/
  * new modules: mod_openssl, mod_vhostdb, mod_wstunnel
  * new protocols: Upgrade: websocket, HAProxy PROXY, RFC7239 Forwarded
  * bug fixes
* https://www.lighttpd.net/2017/10/22/1.4.47/
  * bug fixes: fix two regressions in 1.4.46
* https://www.lighttpd.net/2017/11/11/1.4.48/
  * mod_authn_sasl (new), meson build system (new), revamp build systems
  * bug fixes
* https://www.lighttpd.net/2018/3/11/1.4.49/
  * mod_proxy: basic support for HTTP CONNECT
  * bug fixes
* https://www.lighttpd.net/2018/8/13/1.4.50/
  * security fixes
  * bug fixes
* https://www.lighttpd.net/2018/10/14/1.4.51/
  * security fixes
  * bug fixes
  * new module: mod_authn_pam
  * support for wolfSSL

New options are set to 'without' for now until anybody needs that.

Fixes: 2f49c9fbd60f97b32a38eb9bc715361695fc2fe0
Signed-off-by: Alexander Dahl <ada@thorsis.com>
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/lighttpd.make
@@ -44,6 +44,9 @@ LIGHTTPD_CONF_OPT	:= \
 	--enable-extra-warnings \
 	--without-libev \
 	--without-mysql \
+	--without-pgsql \
+	--without-dbi \
+	--without-sasl \
 	--without-ldap \
 	--without-pam \
 	--$(call ptx/wwo, PTXCONF_LIGHTTPD_ATTR)-attr \
@@ -51,6 +54,7 @@ LIGHTTPD_CONF_OPT	:= \
 	--without-libunwind \
 	--without-krb5 \
 	--$(call ptx/wwo, PTXCONF_LIGHTTPD_OPENSSL)-openssl \
+	--without-wolfssl \
 	--$(call ptx/wwo, PTXCONF_LIGHTTPD_PCRE)-pcre \
 	--$(call ptx/wwo, PTXCONF_LIGHTTPD_ZLIB)-zlib \
 	--$(call ptx/wwo, PTXCONF_LIGHTTPD_BZ2LIB)-bzip2 \