ringlej/ptxdist

commit 02aff14f0d6a824609cbcc1410e450ddc1d3c54f

[bluez] version bump 3.36 -> 4.69
upstream merged bluez-lib and bluez-utils, so did I.

Signed-off-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
[mkl: fix gstreamer deps, install gst plugins in targetinstall]
Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>

rules/bluez.make
@@ -1,0 +1,137 @@
+# -*-makefile-*-
+#
+# Copyright (C) 2010 by Uwe Kleine-Koenig <u.kleine-koenig@pengutronix.de>
+#
+# See CREDITS for details about who has contributed to this project.
+#
+# For further information about the PTXdist project and license conditions
+# see the README file.
+#
+
+#
+# We provide this package
+#
+PACKAGES-$(PTXCONF_BLUEZ) += bluez
+
+#
+# Paths and names
+#
+BLUEZ_VERSION	:= 4.69
+BLUEZ		:= bluez-$(BLUEZ_VERSION)
+BLUEZ_SUFFIX	:= tar.gz
+BLUEZ_URL	:= http://www.kernel.org/pub/linux/bluetooth/$(BLUEZ).$(BLUEZ_SUFFIX)
+BLUEZ_SOURCE	:= $(SRCDIR)/$(BLUEZ).$(BLUEZ_SUFFIX)
+BLUEZ_DIR	:= $(BUILDDIR)/$(BLUEZ)
+BLUEZ_LICENSE	:= GPLv2+ LGPLv2.1+
+
+# ----------------------------------------------------------------------------
+# Get
+# ----------------------------------------------------------------------------
+
+#
+# autoconf
+#
+BLUEZ_CONF_TOOL	:= autoconf
+BLUEZ_CONF_OPT	:= $(CROSS_AUTOCONF_USR) \
+	--localstatedir=/var \
+	--enable-audio \
+	--enable-bccmd \
+	--disable-capng \
+	--enable-configfiles \
+	--disable-cups \
+	--enable-debug \
+	--disable-dfutool \
+	--disable-dund \
+	--disable-fortify \
+	--disable-hid2hci \
+	--disable-hidd \
+	--enable-input \
+	--enable-libtool-lock \
+	--disable-maemo6 \
+	--disable-netlink \
+	--enable-network \
+	--enable-optimization \
+	--disable-pand \
+	--disable-pcmcia \
+	--disable-pie \
+	--disable-pnat \
+	--enable-serial \
+	--enable-service \
+	--enable-shared \
+	--disable-static \
+	--enable-test \
+	--disable-tracer \
+	--enable-tools \
+	--enable-udevrules \
+	--enable-usb
+
+ifdef PTXCONF_BLUEZ_ALSA
+BLUEZ_CONF_OPT += --enable-alsa
+else
+BLUEZ_CONF_OPT += --disable-alsa
+endif
+
+ifdef PTXCONF_BLUEZ_GSTREAMER
+BLUEZ_CONF_OPT += --enable-gstreamer
+else
+BLUEZ_CONF_OPT += --disable-gstreamer
+endif
+
+# ----------------------------------------------------------------------------
+# Target-Install
+# ----------------------------------------------------------------------------
+
+$(STATEDIR)/bluez.targetinstall:
+	@$(call targetinfo)
+
+	@$(call install_init,  bluez)
+	@$(call install_fixup, bluez,PRIORITY,optional)
+	@$(call install_fixup, bluez,SECTION,base)
+	@$(call install_fixup, bluez,AUTHOR,"Uwe Kleine-Koenig <u.kleine-koenig@pengutronix.de>")
+	@$(call install_fixup, bluez,DESCRIPTION,missing)
+
+	@$(foreach lib,libbluetooth.so.3.9.0, \
+	$(call install_copy, bluez, 0, 0, 0644, -, /usr/lib/$(lib)); \
+	$(call install_link, bluez, $(lib), /usr/lib/$(basename $(lib))); \
+	$(call install_link, bluez, $(lib), /usr/lib/$(basename $(basename $(lib)))); \
+	)
+
+ifdef PTXCONF_BLUEZ_ALSA
+	@$(foreach alsamod,ctl_bluetooth pcm_bluetooth, \
+	$(call install_copy, bluez, 0, 0, 0644, -, /usr/lib/alsa-lib/libasound_module_$(alsamod).so); \
+	)
+endif
+
+ifdef PTXCONF_BLUEZ_GSTREAMER
+	@$(call install_copy, bluez, 0, 0, 0644, -, /usr/lib/gstreamer-0.10/libgstbluetooth.so)
+endif
+
+ifdef PTXCONF_BLUEZ_UTILS
+	@$(foreach binprogram,ciptool l2ping hcitool rctest rfcomm sdptool, \
+	$(call install_copy, bluez, 0, 0, 0755, -, /usr/bin/$(binprogram)); \
+	)
+
+	@$(foreach sbinprogram,bccmd hciattach hciconfig, \
+	$(call install_copy, bluez, 0, 0, 0755, -, /usr/sbin/$(sbinprogram)); \
+	)
+endif
+
+ifdef PTXCONF_BLUEZ_INSTALL_TESTSCRIPTS
+	@$(foreach testprog, simple-agent simple-service test-telephony test-adapter test-audio \
+		test-device test-discovery test-input test-manager test-network test-serial \
+		test-service test-telephony test-textfile monitor-bluetooth, \
+	$(call install_alternative, bluez, 0, 0, 0755, /test/$(testprog), n, /usr/share/doc/bluez/examples/$(testprog)); \
+	)
+endif
+
+	@$(call install_copy, bluez, 0, 0, 0755, -, /usr/sbin/bluetoothd)
+
+	@$(call install_copy, bluez, 0, 0, 0644, -, /etc/dbus-1/system.d/bluetooth.conf)
+	@$(foreach udevrule, $(notdir $(wildcard $(BLUEZ_PKGDIR)/lib/udev/rules.d/*.rules)), \
+	$(call install_copy, bluez, 0, 0, 0644, -, /lib/udev/rules.d/$(udevrule)); \
+	)
+
+	@$(call install_finish, bluez)
+	@$(call touch)
+
+# vim: syntax=make

rules/bluez-libs.in
@@ -1,11 +1,0 @@
-## SECTION=communication
-
-config BLUEZ_LIBS
-	tristate
-	prompt "bluez-libs"
-	help
-	  bluez libraries, needed to build bluez-utils. For details
-	  about this project see the homepage on
-
-		http://www.bluez.org
-

rules/bluez-libs.make
@@ -1,69 +1,0 @@
-# -*-makefile-*-
-#
-# Copyright (C) 2005 by Robert Schwebel
-#               2007, 2009 by Marc Kleine-Budde <mkl@pengutronix.de>
-#
-# See CREDITS for details about who has contributed to this project.
-#
-# For further information about the PTXdist project and license conditions
-# see the README file.
-#
-
-#
-# We provide this package
-#
-PACKAGES-$(PTXCONF_BLUEZ_LIBS) += bluez-libs
-
-#
-# Paths and names
-#
-BLUEZ_LIBS_VERSION	:= 3.36
-BLUEZ_LIBS		:= bluez-libs-$(BLUEZ_LIBS_VERSION)
-BLUEZ_LIBS_SUFFIX	:= tar.gz
-BLUEZ_LIBS_URL		:= http://bluez.sourceforge.net/download/$(BLUEZ_LIBS).$(BLUEZ_LIBS_SUFFIX)
-BLUEZ_LIBS_SOURCE	:= $(SRCDIR)/$(BLUEZ_LIBS).$(BLUEZ_LIBS_SUFFIX)
-BLUEZ_LIBS_DIR		:= $(BUILDDIR)/$(BLUEZ_LIBS)
-
-# ----------------------------------------------------------------------------
-# Get
-# ----------------------------------------------------------------------------
-
-$(BLUEZ_LIBS_SOURCE):
-	@$(call targetinfo)
-	@$(call get, BLUEZ_LIBS)
-
-# ----------------------------------------------------------------------------
-# Prepare
-# ----------------------------------------------------------------------------
-
-BLUEZ_LIBS_PATH	:= PATH=$(CROSS_PATH)
-BLUEZ_LIBS_ENV 	:= $(CROSS_ENV)
-
-#
-# autoconf
-#
-BLUEZ_LIBS_AUTOCONF := $(CROSS_AUTOCONF_USR)
-
-# ----------------------------------------------------------------------------
-# Target-Install
-# ----------------------------------------------------------------------------
-
-$(STATEDIR)/bluez-libs.targetinstall:
-	@$(call targetinfo)
-
-	@$(call install_init, bluez-libs)
-	@$(call install_fixup, bluez-libs,PRIORITY,optional)
-	@$(call install_fixup, bluez-libs,SECTION,base)
-	@$(call install_fixup, bluez-libs,AUTHOR,"Robert Schwebel <r.schwebel@pengutronix.de>")
-	@$(call install_fixup, bluez-libs,DESCRIPTION,missing)
-
-	@$(call install_copy, bluez-libs, 0, 0, 0644, - ,\
-		/usr/lib/libbluetooth.so.2.11.2)
-	@$(call install_link, bluez-libs, libbluetooth.so.2.11.2, /usr/lib/libbluetooth.so.2)
-	@$(call install_link, bluez-libs, libbluetooth.so.2.11.2, /usr/lib/libbluetooth.so)
-
-	@$(call install_finish, bluez-libs)
-
-	@$(call touch)
-
-# vim: syntax=make

rules/bluez-utils.in
@@ -1,12 +1,0 @@
-## SECTION=communication
-
-config BLUEZ_UTILS
-	tristate
-	prompt "bluez-utils"
-	select BLUEZ_LIBS
-	select DBUS
-	help
-	  For details about this project see the homepage on
-
-		http://www.bluez.org
-

rules/bluez-utils.make
@@ -1,72 +1,0 @@
-# -*-makefile-*-
-#
-# Copyright (C) 2005 by Robert Schwebel
-#               2007 by Marc Kleine-Budde <mkl@pengutronix.de>
-#
-# See CREDITS for details about who has contributed to this project.
-#
-# For further information about the PTXdist project and license conditions
-# see the README file.
-#
-
-#
-# We provide this package
-#
-PACKAGES-$(PTXCONF_BLUEZ_UTILS) += bluez-utils
-
-#
-# Paths and names
-#
-BLUEZ_UTILS_VERSION	:= 3.36
-BLUEZ_UTILS		:= bluez-utils-$(BLUEZ_UTILS_VERSION)
-BLUEZ_UTILS_SUFFIX	:= tar.gz
-BLUEZ_UTILS_URL		:= http://bluez.sourceforge.net/download/$(BLUEZ_UTILS).$(BLUEZ_UTILS_SUFFIX)
-BLUEZ_UTILS_SOURCE	:= $(SRCDIR)/$(BLUEZ_UTILS).$(BLUEZ_UTILS_SUFFIX)
-BLUEZ_UTILS_DIR		:= $(BUILDDIR)/$(BLUEZ_UTILS)
-
-# ----------------------------------------------------------------------------
-# Get
-# ----------------------------------------------------------------------------
-
-$(BLUEZ_UTILS_SOURCE):
-	@$(call targetinfo)
-	@$(call get, BLUEZ_UTILS)
-
-# ----------------------------------------------------------------------------
-# Prepare
-# ----------------------------------------------------------------------------
-
-BLUEZ_UTILS_PATH	:= PATH=$(CROSS_PATH)
-BLUEZ_UTILS_ENV 	:= $(CROSS_ENV)
-
-#
-# autoconf
-#
-BLUEZ_UTILS_AUTOCONF := \
-	$(CROSS_AUTOCONF_USR) \
-	--without-alsa \
-	--without-fuse \
-	--without-openobex \
-	--without-usb
-
-# ----------------------------------------------------------------------------
-# Target-Install
-# ----------------------------------------------------------------------------
-
-$(STATEDIR)/bluez-utils.targetinstall:
-	@$(call targetinfo)
-
-	@$(call install_init, bluez-utils)
-	@$(call install_fixup, bluez-utils,PRIORITY,optional)
-	@$(call install_fixup, bluez-utils,SECTION,base)
-	@$(call install_fixup, bluez-utils,AUTHOR,"Robert Schwebel <r.schwebel@pengutronix.de>")
-	@$(call install_fixup, bluez-utils,DESCRIPTION,missing)
-
-	# FIXME: wait for patch from Sandro Noel
-#	@$(call install_copy, bluez-utils, 0, 0, 0755, $(BLUEZ_UTILS_DIR)/foobar, /dev/null)
-
-	@$(call install_finish, bluez-utils)
-
-	@$(call touch)
-
-# vim: syntax=make

rules/bluez.in
@@ -1,0 +1,36 @@
+## SECTION=communication
+
+menuconfig BLUEZ
+	tristate "bluez                         "
+	select DBUS
+	select ALSA_LIB		if BLUEZ_ALSA
+	select GSTREAMER	if BLUEZ_GSTREAMER
+	select GST_PLUGINS_BASE	if BLUEZ_GSTREAMER
+	select PYGOBJECT	if BLUEZ_INSTALL_TESTSCRIPTS
+	select DBUS_PYTHON	if BLUEZ_INSTALL_TESTSCRIPTS
+	help
+	  official Linux Bluetooth protocol stack
+
+if BLUEZ
+
+config BLUEZ_ALSA
+	bool "alsa support"
+	help
+	  install bluez-alsa modules
+
+config BLUEZ_GSTREAMER
+	bool "gstreamer support"
+	help
+	  install libgstbluetooth
+	  
+config BLUEZ_UTILS
+	bool "install bluez tools"
+	help
+	  install ciptool l2ping hcitool rctest rfcomm sdptool
+
+config BLUEZ_INSTALL_TESTSCRIPTS
+	bool "install testscripts"
+	help
+	  install test scripts that allow to set up bluetooth connections
+
+endif

rules/libpcap.in
@@ -3,7 +3,7 @@
 menuconfig LIBPCAP
 	tristate
 	prompt "libpcap                       "
-	select BLUEZ_LIBS	if LIBPCAP_BLUETOOTH
+	select BLUEZ	if LIBPCAP_BLUETOOTH
 	help
 	  libpcap is a system-independent interface for user-level packet capture.
 	  libpcap provides a portable framework for low-level network monitoring.

scripts/migrate/migrate_ptx
@@ -130,3 +130,9 @@ s/^\(\(# \)\?PTXCONF_\)INITMETHOD_BBINIT_LINK_TELNETD\(.*\)$/\1UTELNETD_BBINIT_L
 s/^\(\(# \)\?PTXCONF_\)INITMETHOD_BBINIT_LINK_\(CROND\|HWCLOCK\|TELNETD\)/\1BUSYBOX_\3_BBINIT_LINK/
 s/^\(\(# \)\?PTXCONF_\)INITMETHOD_BBINIT_LINK_\(ALSA_UTILS\|APACHE2\|AT\|CHRONY\|CONNMAN\|CVS\|DBUS\|DIBBLER_[A-Z]*\|DNSMASQ\|DROPBEAR\|FAKE_OVERLAYFS\|LIGHTTPD\|LVM2\|MYSQL\|NTPCLIENT\|OPENSSH\|PORTMAP\|PPPD\|PROFTPD\|PUREFTPD\|RC_ONCE\|SAMBA\|SPLASHUTILS\|THTTPD\|UDEV\)/\1\3_BBINIT_LINK/
 
+#
+# symbol : PTXCONF_BLUEZ_LIBS -> PTXCONF_BLUEZ
+# reason : upstream merged bluez-libs + bluez-utils -> bluez
+#          PTXCONF_BLUEZ_UTILS is unchanged
+#
+s/^\(\(# \)\?PTXCONF_BLUEZ\)_LIBS/\1/

commit 754a9e6a057ba06ed408ccd0b5db37fa4d15a8d3

bluez: fix license string
Fixes: 02aff14f0d6a824609cbcc1410e45 "[bluez] version bump 3.36 -> 4.69"
Signed-off-by: Roland Hieber <r.hieber@pengutronix.de>
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/bluez.make
@@ -23,7 +23,7 @@ BLUEZ_SUFFIX	:= tar.gz
 BLUEZ_URL	:= $(call ptx/mirror, KERNEL, bluetooth/$(BLUEZ).$(BLUEZ_SUFFIX))
 BLUEZ_SOURCE	:= $(SRCDIR)/$(BLUEZ).$(BLUEZ_SUFFIX)
 BLUEZ_DIR	:= $(BUILDDIR)/$(BLUEZ)
-BLUEZ_LICENSE	:= GPL-2.0-or-later LGPL-2.1-or-later
+BLUEZ_LICENSE	:= GPL-2.0-or-later AND LGPL-2.1-or-later
 ifdef PTXCONF_BLUEZ_INSTALL_TESTSCRIPTS
 BLUEZ_DEVPKG	:= NO
 endif