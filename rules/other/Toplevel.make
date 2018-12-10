ringlej/ptxdist

commit a0b8075391cfeb324f60e42482b1c34f95d1de71

ptxdist: add --progress option
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/other/Toplevel.make
@@ -3,8 +3,14 @@
 # Copyright (C) 2002-2009 by The PTXdist Team - See CREDITS for Details
 #
 
+ifneq ($(filter n,$(MAKEFLAGS)),)
+# make sure recursive calls do nothing for --dry-run
+MAKE=true
+SHELL=true
+else
 # make sure bash is used to execute commands from makefiles
 SHELL=$(realpath $(PTXDIST_TOPDIR)/bin/bash)
+endif
 export SHELL
 
 unexport MAKEFLAGS

bin/ptxdist
@@ -1000,6 +1000,8 @@ Options:
   --output-sync			Improve output readability for parallel building.
 				Disabled by default except for quiet builds.
   --no-output-sync		Disable output-sync if enabled by default.
+  --progress			Show progress information. This is only available
+				in quiet mode.
 
   --j-intern=<n>, -ji<n>	set number of parallel builds in packages
 				(default = 2*CPUs)
@@ -1304,6 +1306,9 @@ parse_first()
 			;;
 		--no-output-sync)
 			PTXDIST_OUTPUT_SYNC=0
+			;;
+		--progress)
+			PTXDIST_PROGRESS=1
 			;;
 		-q|--quiet)
 			PTXDIST_QUIET=1

doc/ref_parameter.inc
@@ -287,6 +287,12 @@ Options
   completed. As a result, there will be long periods of time with no
   visible progress.
 
+``--progress``
+  show some progress information in the form of completed/total build
+  stages. This is only shown if quiet is enabled as well. Note, that this
+  adds some extra overhead at the beginning, so it will take some time
+  until the first build stage starts.
+
 ``--j-intern=<n>``, ``-ji<n>``
   set number of parallel jobs within packages. PTXdist will use this
   number for example when calling ``make`` during the compile stage.

rules/post/function-targetinfo.make
@@ -16,7 +16,7 @@
 #
 ifdef PTXDIST_QUIET
 ifdef PTXDIST_FD_STDOUT
-_targetinfo_opt_output := echo "started : $(PTX_COLOR_BLUE)$${target}$(PTX_COLOR_OFF)" >&$(PTXDIST_FD_STDOUT);
+_targetinfo_opt_output := echo "$$(ptxd_make_print_progress start $${target})started : $(PTX_COLOR_BLUE)$${target}$(PTX_COLOR_OFF)" >&$(PTXDIST_FD_STDOUT);
 endif
 endif
 ifdef PTXCONF_SETUP_GEN_DEP_TREE

rules/post/function-touch.make
@@ -14,7 +14,7 @@
 #
 ifdef PTXDIST_QUIET
 ifdef PTXDIST_FD_STDOUT
-_touch_opt_output := echo "finished: $(PTX_COLOR_GREEN)$${target}$(PTX_COLOR_OFF)" >&$(PTXDIST_FD_STDOUT);
+_touch_opt_output := echo "$$(ptxd_make_print_progress stop $${target})finished: $(PTX_COLOR_GREEN)$${target}$(PTX_COLOR_OFF)" >&$(PTXDIST_FD_STDOUT);
 endif
 endif
 

scripts/lib/ptxd_make_progress.sh
@@ -1,0 +1,43 @@
+#!/bin/bash
+#
+# Copyright (C) 2018 by Michael Olbrich <m.olbrich@pengutronix.de>
+#
+# See CREDITS for details about who has contributed to this project.
+#
+# For further information about the PTXdist project and license conditions
+# see the README file.
+#
+
+ptxd_make_setup_progress() {
+    if [ -n "${PTXDIST_PROGRESS}" ]; then
+	ptxd_make_target_count=$( \
+	    "${PTXCONF_SETUP_HOST_MAKE}" --dry-run \
+	    -f "${RULESDIR}/other/Toplevel.make" "${@}" 2>/dev/null | \
+		grep -E '^target=.*\.(get|extract|prepare|compile|install|targetinstall|report).* touch' | \
+		wc -l)
+	export ptxd_make_target_count
+	exec {ptxd_make_target_count_start}>> "${PTXDIST_TEMPDIR}/ptxd_make_target_count_start"
+	exec {ptxd_make_target_count_stop}>> "${PTXDIST_TEMPDIR}/ptxd_make_target_count_stop"
+	export ptxd_make_target_count_start ptxd_make_target_count_stop
+	:> "${PTXDIST_TEMPDIR}/ptxd_make_target_count"
+    fi
+}
+
+ptxd_make_print_progress() {
+    if [ -n "${ptxd_make_target_count}" ]; then
+	local start stop len
+	if [[ "${2}" =~ .*\.(get|extract|prepare|compile|install|targetinstall|report).* ]]; then
+	    local ptr="ptxd_make_target_count_${1}"
+	    echo -n + >&${!ptr}
+	    if [ "${2}" = "world.targetinstall" ]; then
+		# world.targetinstall has no 'targetinfo'
+		echo -n + >&${ptxd_make_target_count_start}
+	    fi
+	fi
+	start=$(wc -c < "${PTXDIST_TEMPDIR}/ptxd_make_target_count_start")
+	stop=$(wc -c < "${PTXDIST_TEMPDIR}/ptxd_make_target_count_stop")
+	len="$(wc -c <<<${ptxd_make_target_count})"
+	printf "[%$[len-1]d/%d] " "${stop}" "${ptxd_make_target_count}"
+    fi
+}
+export -f ptxd_make_print_progress

scripts/libptxdist.sh
@@ -166,6 +166,7 @@ ptxd_make() {
 			source "${lib}" || ptxd_bailout "failed to source lib: ${lib}"
 		done
 	done
+	ptxd_make_setup_progress "${@}" &&
 	${PTX_NICE:+nice -n ${PTX_NICE}} "${PTXCONF_SETUP_HOST_MAKE}" \
 	    "${PTX_MAKE_ARGS[@]}" ${PTXDIST_PARALLELMFLAGS_EXTERN} ${PTXDIST_LOADMFLAGS} \
 	    -f "${RULESDIR}/other/Toplevel.make" "${@}" || return

commit 8709d116de129ebdfb54ac5feb7c0cc84309fece

Toplevel: fix detection of -n / --dry-run option
$(filter n,$(MAKEFLAGS)) will be empty if make is called with any
additional parameters. For example, with `make -s -n`, MAKEFLAGS will
contain "sn", which will not match the pattern "n".

The following way is inspired by kernel commit 6f0fa58e459642b169, which
tried to do the same for the -s / --silent option (although support for
make 3.x is probably not important to us):

    kbuild: simplify silent build (-s) detection

    This allows to detect -s (--silent) option without checking GNU Make
    version.

    As commit e36aaea28972 ("kbuild: Fix silent builds with make-4")
    pointed out, GNU Make 4.x changed the way/order it presents the
    command line options into MAKEFLAGS.

    In Make 3.8x, 's' is always the first in a group of short options.
    The group may be prefixed with '-' in some cases.

    In Make 4.x, 's' is always the last in a group of short options.

    As commit e6ac89fabd03 ("kbuild: Correctly deal with make options
    which contain an 's'") addressed, we also need to deal with long
    options that contain 's', like --warn-undefined-variables.

    Test cases:

    [1] command line input:    make --silent
         -> MAKEFLAGS for Make 3.8x:    s
         -> MAKEFLAGS for Make 4.x :    s

    [2] command line input:    make -srR
         -> MAKEFLAGS for Make 3.8x:    sRr
         -> MAKEFLAGS for Make 4.x :    rRs

    [3] command line input:    make -s -rR --warn-undefined-variables
         -> MAKEFLAGS for Make 3.8x:    --warn-undefined-variables -sRr
         -> MAKEFLAGS for Make 4.x :    rRs --warn-undefined-variables

    My idea to cater to all the cases more easily is to filter out long
    options (--%), then search 's' with $(findstring ...).  This way will
    be more future-proof even if future versions of Make put 's' in the
    middle of the group.

    Signed-off-by: Masahiro Yamada <yamada.masahiro@socionext.com>

Fixes: a0b8075391cfeb324f60e42482b1c34f "ptxdist: add --progress option"

rules/other/Toplevel.make
@@ -3,7 +3,7 @@
 # Copyright (C) 2002-2009 by The PTXdist Team - See CREDITS for Details
 #
 
-ifneq ($(filter n,$(MAKEFLAGS)),)
+ifneq ($(findstring n,$(filter-out --%,$(MAKEFLAGS))),)
 # make sure recursive calls do nothing for --dry-run
 MAKE=true
 SHELL=true