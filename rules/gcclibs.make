ringlej/ptxdist

commit 60ae2f93bed3a1d5a7e629a17afbca4a6b422d9b

gcclibs: add more sanitizer
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/gcclibs.make
@@ -53,6 +53,14 @@ ifdef PTXCONF_GCCLIBS_LIBASAN
 	@$(call install_copy_toolchain_lib, gcclibs, libasan.so)
 endif
 
+ifdef PTXCONF_GCCLIBS_LIBLSAN
+	@$(call install_copy_toolchain_lib, gcclibs, liblsan.so)
+endif
+
+ifdef PTXCONF_GCCLIBS_LIBTSAN
+	@$(call install_copy_toolchain_lib, gcclibs, liblsan.so)
+endif
+
 ifdef PTXCONF_GCCLIBS_LIBUBSAN
 	@$(call install_copy_toolchain_lib, gcclibs, libubsan.so)
 endif

rules/gcclibs.in
@@ -45,6 +45,7 @@ config GCCLIBS_GCJ
 config GCCLIBS_LIBASAN
 	bool "libasan"
 	select GCCLIBS
+	select GCCLIBS_CXX
 	select GCCLIBS_GCC_S
 	select LIBC_DL
 	select LIBC_M
@@ -54,9 +55,36 @@ config GCCLIBS_LIBASAN
 	  Installs the AddressSanitizer runtime library
 	  libasan
 
+config GCCLIBS_LIBLSAN
+	bool "liblsan"
+	select GCCLIBS
+	select GCCLIBS_CXX
+	select GCCLIBS_GCC_S
+	select LIBC_DL
+	select LIBC_M
+	select LIBC_PTHREAD
+	select LIBC_RT
+	help
+	  Installs the LeakSanitizer runtime library
+	  liblsan
+
+config GCCLIBS_LIBTSAN
+	bool "libtsan"
+	select GCCLIBS
+	select GCCLIBS_CXX
+	select GCCLIBS_GCC_S
+	select LIBC_DL
+	select LIBC_M
+	select LIBC_PTHREAD
+	select LIBC_RT
+	help
+	  Installs the ThreadSanitizer runtime library
+	  libtsan
+
 config GCCLIBS_LIBUBSAN
 	bool "libubsan"
 	select GCCLIBS
+	select GCCLIBS_CXX
 	select GCCLIBS_GCC_S
 	select LIBC_DL
 	select LIBC_M

commit 6433eb09b3bf89e79a3075da89e014ebdac555c1

gcclibs: typo: don't install liblsan.so when GCCLIBS_LIBTSAN is enabled
As mentioned in gcclibs.in, the GCCLIBS_LIBTSAN symbol is meant to
install the ThreadSanitizer library, libtsan.so. The typo here,
"liblsan.so" was probably copied from the rule above it.

Reported-by: horizon on irc://chat.freenode.net/#ptxdist
Fixes: 60ae2f93bed3a1d5a7e6 ("gcclibs: add more sanitizer")
Signed-off-by: Roland Hieber <rohieb@rohieb.name>
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/gcclibs.make
@@ -58,7 +58,7 @@ ifdef PTXCONF_GCCLIBS_LIBLSAN
 endif
 
 ifdef PTXCONF_GCCLIBS_LIBTSAN
-	@$(call install_copy_toolchain_lib, gcclibs, liblsan.so)
+	@$(call install_copy_toolchain_lib, gcclibs, libtsan.so)
 endif
 
 ifdef PTXCONF_GCCLIBS_LIBUBSAN